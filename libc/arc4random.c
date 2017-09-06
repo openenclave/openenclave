36ae67c85fb9be04c440b5e56e985fe3eaa30678
Mike Brasher <mikbras@microsoft.com>
2017-09-06 01:45:20 -0500

    Reponses to review.

--- libc/arc4random.c
+++ libc/arc4random.c
@@ -8,11 +8,6 @@
 unsigned int arc4random(void)
 {
     unsigned long r;
-
-    __asm__ volatile("rdrand %%rax\n\t"
-        "mov %%rax, %0\n\t"
-        :
-        "=m"(r));
-
-    return ((r >> 32) ^ (r & 0x00000000FFFFFFFF));
+    __asm__ volatile("rdrand %%rax\n\t" "mov %%rax, %0\n\t" : "=m"(r));
+    return (unsigned int)r;
 }
