8239a709cbec0df8ce0ffb1413d3d9c55ea56c75
Mike Brasher <mikbras@microsoft.com>
2017-11-09 14:52:37 -0600

    libc and libcxx unit tests.

---
+++ tests/libcxx/enc/enc.cpp
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+#include <stdio.h>
+#include <openenclave/enclave.h>
+#include "../host/ocalls.h"
+#include "../host/args.h"
+
+extern const char* __test__;
+
+extern "C" int main(int argc, const char* argv[]);
+
+extern "C" void _exit(int status)
+{
+    OE_OCall(OCALL_EXIT, status, NULL);
+    abort();
+}
+
+extern "C" void _Exit(int status)
+{
+    _exit(status);
+    abort();
+}
+
+extern "C" void exit(int status)
+{
+    _exit(status);
+    abort();
+}
+
+typedef void (*Handler)(int signal);
+
+Handler signal(int signal, Handler)
+{
+    /* Ignore! */
+    return NULL;
+}
+
+extern "C" int close(int fd)
+{
+    assert("close() panic" == NULL);
+    return 0;
+}
+
+OE_ECALL void Test(Args* args)
+{
+    if (args)
+    {
+        printf("RUNNING: %s\n", __TEST__);
+        static const char* argv[] = { "test", NULL, };
+        static int argc = sizeof(argv) / sizeof(argv[0]);
+        args->ret = main(argc, argv);
+        args->test = OE_HostStrdup(__TEST__);
+    }
+}
---
+++ tests/libcxx/enc/fuzzing.cpp
@@ -0,0 +1,2 @@
+#include "warnings.h"
+#include "../../../3rdparty/libcxx/libcxx/fuzzing/fuzzing.cpp"
