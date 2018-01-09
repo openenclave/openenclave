44ea2f8b34b602d0c5e8acaf2fc7787ec74c0e87
Mike Brasher <mikbras@microsoft.com>
2018-01-09 15:14:17 -0600

    Removed unused register functions

--- include/openenclave/bits/registers.h
+++ include/openenclave/bits/registers.h
@@ -6,49 +6,6 @@
 #include <openenclave/defs.h>
 #include <openenclave/types.h>
 
-#define __oe_nop asm volatile("nop\n\t")
-#define __oe_ud2 asm volatile("ud2\n\t")
-
-OE_EXTERNC_BEGIN
-
-typedef struct _OE_Registers
-{
-    uint64_t rsp;
-    uint64_t rbp;
-    uint64_t rbx;
-    uint64_t r12;
-    uint64_t r13;
-    uint64_t r14;
-    uint64_t r15;
-}
-OE_Registers;
-
-OE_INLINE void OE_AssertRegisters(
-    const OE_Registers* lhs,
-    const OE_Registers* rhs)
-{
-    if (lhs->rsp != rhs->rsp)
-        abort();
-
-    if (lhs->rbp != rhs->rbp)
-        abort();
-
-    if (lhs->rbx != rhs->rbx)
-        abort();
-
-    if (lhs->r12 != rhs->r12)
-        abort();
-
-    if (lhs->r13 != rhs->r13)
-        abort();
-
-    if (lhs->r14 != rhs->r14)
-        abort();
-
-    if (lhs->r15 != rhs->r15)
-        abort();
-}
-
 int OE_SetGSRegisterBase(const void *ptr);
 
 int OE_GetGSRegisterBase(const void **ptr);
