3130b724018aa015fd6f2a68354fbe646b2d39a7
Mike Brasher <mikbras@microsoft.com>
2018-01-10 19:35:59 -0600

    Ported OE_AEP() assembly routine to Windows (MASM)

--- host/aep.asm
+++ host/aep.asm
@@ -1,6 +1,36 @@
-.code
-OE_AEP proc
+include ksamd64.inc
 
-OE_AEP endp
+ThreadBinding_tcs EQU 0h
+ENCLU_ERESUME EQU 3h
 
-end
+;;==============================================================================
+;;
+;; void OE_AEP(void)
+;;
+;;     Asynchronous Exception Pointer (AEP) function that handles exceptions 
+;;     and interrupts from an enclave. A pointer to this function is passed
+;;     to the EENTER instruction and this function. This implementation resumes
+;;     execution of the enclave (ERESUME).
+;; 
+;;     This function must not use or modify the stack, else it could overwrite
+;;     the host stack region used by enclave host stack allocaiton routines.
+;;
+;;==============================================================================
+
+
+NESTED_ENTRY OE_AEP, _TEXT$00
+    END_PROLOGUE
+
+aep:
+    mov rax, ENCLU_ERESUME
+    mov rbx, fs:[ThreadBinding_tcs]
+    lea rcx, aep
+    mov rdx, 0
+    ENCLU
+    ud2
+
+    BEGIN_EPILOGUE
+    ret
+NESTED_END OE_AEP, _TEXT$00
+
+END
