53d8d44631bac5c2ce156d6bb2e3c47f5b032246
Jork Loeser <jloeser@microsoft.com>
2018-03-05 13:06:24 -0800

    Fix #112 - race in static host mutex init

--- host/windows/hostthread.c
+++ host/windows/hostthread.c
@@ -64,8 +64,18 @@ int OE_H_MutexInit(OE_H_Mutex* Lock)
 
 int OE_H_MutexLock(OE_H_Mutex* Lock)
 {
-    if ((*Lock == OE_H_MUTEX_INITIALIZER) && OE_H_MutexInit(Lock))
-        return 1;
+    if (*Lock == OE_H_MUTEX_INITIALIZER)
+    {
+        if (OE_H_MutexInit(&newLock))
+            return 1;
+        if (InterlockedCompareExchangePointer(
+                Lock, newLock, OE_H_MUTEX_INITIALIZER)) !=
+            OE_H_MUTEX_INITIALIZER)
+            {
+                if (OE_H_MutexDestroy(newLock))
+                    return 1;
+            }
+    }
 
     return WaitForSingleObject(*Lock, INFINITE) != WAIT_OBJECT_0;
 }
