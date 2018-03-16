27b0ec32be2fe9d04080874784077432ceeeba17
Anand Krishnamoorthi <anakrish@microsoft.com>
2018-03-15 19:08:15 -0700

    Refactored new test into a separate file for condition variable testing.

---
+++ tests/thread/enc/cond_tests.cpp
@@ -0,0 +1,83 @@
+#include <assert.h>
+#include <openenclave/enclave.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../args.h"
+
+static OE_Mutex mutex = OE_MUTEX_INITIALIZER;
+static OE_Cond cond = OE_COND_INITIALIZER;
+
+// Number of waiting threads.
+static volatile int num_waiting = 0;
+
+// Number of woken up threads.
+static volatile int num_woken = 0;
+
+static volatile bool exit_thread = false;
+
+OE_ECALL void CBTestWaiterThreadImpl(void* args)
+{
+    while (!exit_thread)
+    {
+        OE_MutexLock(&mutex);
+
+        // Increment counter and wait.
+        ++num_waiting;
+        OE_CondWait(&cond, &mutex);
+
+        // After waking up, update counters.
+        --num_waiting;
+        ++num_woken;
+
+        OE_MutexUnlock(&mutex);
+    }
+}
+
+OE_ECALL void CBTestSignalThreadImpl(void* args)
+{
+    const size_t ITERS = 2000;
+
+    for (size_t i = 0; i < ITERS; ++i)
+    {
+        OE_MutexLock(&mutex);
+
+        // No thread should wake up until broadcast.
+        assert(num_woken == 0);
+
+        // Signal waiting threads to wake up.
+        // Stash the number of threads expected to wake up.
+        int num_expected = num_waiting;
+        OE_CondBroadcast(&cond);
+
+        OE_MutexUnlock(&mutex);
+
+        // There is no guarantee whether the woken up threads
+        // are scheduled for execution immediately.
+        // Therefore, wait until expected number of threads are woken up.
+        bool done = false;
+        while (!done)
+        {
+            OE_MutexLock(&mutex);
+
+            // No more than desired number of threads should be woken up.
+            assert(num_woken <= num_expected);
+
+            if (num_expected == num_woken)
+            {
+                // Test succeeded. Clear counter.
+                num_woken = 0;
+                done = true;
+            }
+
+            OE_MutexUnlock(&mutex);
+        }
+    }
+
+    // Signal waiter threads to exit.
+    OE_MutexLock(&mutex);
+
+    exit_thread = true;
+    OE_CondBroadcast(&cond);
+
+    OE_MutexUnlock(&mutex);
+}
