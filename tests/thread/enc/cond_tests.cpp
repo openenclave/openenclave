34b9650d2b948833c4cb100a58f1228b7adbc313
Anand Krishnamoorthi <anakrish@microsoft.com>
2018-03-15 19:50:18 -0700

    1. Added license 2. Fixed corner-case where the test could hang (thanks Lei!).    Waiter thread always checks for exit flag before waiting.

--- tests/thread/enc/cond_tests.cpp
+++ tests/thread/enc/cond_tests.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include <assert.h>
 #include <openenclave/enclave.h>
 #include <stdio.h>
@@ -17,24 +20,33 @@ static volatile bool exit_thread = false;
 
 OE_ECALL void CBTestWaiterThreadImpl(void* args)
 {
+    OE_MutexLock(&mutex);
+
     while (!exit_thread)
     {
-        OE_MutexLock(&mutex);
-
         // Increment counter and wait.
         ++num_waiting;
+
+        // Release mutex and wait.
         OE_CondWait(&cond, &mutex);
 
+        // This thread owns the mutex.
         // After waking up, update counters.
         --num_waiting;
         ++num_woken;
-
-        OE_MutexUnlock(&mutex);
     }
+
+    OE_MutexUnlock(&mutex);
 }
 
 OE_ECALL void CBTestSignalThreadImpl(void* args)
 {
+    // Iterate for enough number of times to
+    // detect any sporadic behavior.
+    // Note: The original issue that the accompanying fix
+    // addresses is a sporadic issue that can only be reliably
+    // reproduced by using large iterations (>= 2000) and trying out various
+    // numbers of waiter threads as this test does.
     const size_t ITERS = 2000;
 
     for (size_t i = 0; i < ITERS; ++i)
@@ -46,6 +58,9 @@ OE_ECALL void CBTestSignalThreadImpl(void* args)
 
         // Signal waiting threads to wake up.
         // Stash the number of threads expected to wake up.
+        // The number of waiting threads could be 0, 1 or max number of waiter
+        // threads. This is by design to allow for testing these various
+        // scenarios in a rapid manner.
         int num_expected = num_waiting;
         OE_CondBroadcast(&cond);
 
@@ -79,5 +94,10 @@ OE_ECALL void CBTestSignalThreadImpl(void* args)
     exit_thread = true;
     OE_CondBroadcast(&cond);
 
+    // Since the signal thread owns the mutex now, any running waiter thread
+    // would
+    // be currently in a OE_CondWait or in a OE_MutexLock.
+    // Once the signal thread releases the mutex, the waiter thread would return
+    // from either of the calls and then check the exit_thread flag and quit.
     OE_MutexUnlock(&mutex);
 }
