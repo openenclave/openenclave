0d35edf73ad3fe51386ac84868d2d9e2905dd970
Mike Brasher <mikbras@microsoft.com>
2018-03-20 18:34:45 -0500

    Added OE_HexDump() for enclaves

---
+++ enclave/hexdump.c
@@ -0,0 +1,38 @@
+#include <openenclave/enclave.h>
+
+/* Convert a nibble to an ASCII character: Example 0xF => 'F' */
+OE_INLINE char _NibbleToHexChar(uint8_t x)
+{
+    return (x < 10) ? ('0' + x) : ('A' + (x - 10));
+}
+
+/* Convert a byte to an ASCII hex string. Example: 0x3F => "3F" */
+static void _ByteToHexString(uint8_t byte, char buf[3])
+{
+    /* Convert high nibble to character */
+    buf[0] = _NibbleToHexChar(byte >> 4);
+
+    /* Convert low nibble to character */
+    buf[1] = _NibbleToHexChar(byte & 0x0F);
+
+    /* Zero-terminate the string */
+    buf[2] = '\0';
+}
+
+void OE_HexDump(const void* data, size_t size)
+{
+    size_t i;
+    const uint8_t* p = (const uint8_t*)data;
+
+    if (!data || !size)
+        return;
+
+    for (i = 0; i < size; i++)
+    {
+        char buf[3];
+        _ByteToHexString(p[i], buf);
+        OE_HostPrintf("%s", buf);
+    }
+
+    OE_HostPrintf("\n");
+}
