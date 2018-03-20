cd24bfed6a72d5b6872a52de1e6e17c4ece11cfc
vishnumohan1991 <vishnu.mohan@vvdntech.in>
2018-03-20 18:56:39 +0530

    Testing framework for test cases in mbedtls :\n\t 1.complete test usecases defined in selftest.

---
+++ tests/mbed_test/enc/enc.cpp
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+#include <stdio.h>
+#include <openenclave/enclave.h>
+#include <openenclave/bits/calls.h>
+#include "../host/ocalls.h"
+#include "../host/args.h"
+
+extern const char* __test__;
+
+extern "C" int main(int argc, const char* argv[]);
+
+extern "C" void _exit(int status)
+{
+    OE_OCall(OCALL_EXIT, status, NULL, 0);
+    abort();
+}
+
+extern "C" void _Exit(int status)
+{
+    _exit(status);
+    abort();
+}
+
+extern "C" void exit(int status)
+{
+    _exit(status);
+    abort();
+}
+
+typedef void (*Handler)(int signal);
+
+Handler signal(int signal, Handler)
+{
+    /* Ignore! */
+    return NULL;
+}
+
+extern "C" int close(int fd)
+{
+    assert("close() panic" == NULL);
+    return 0;
+}
+
+OE_ECALL void Test(Args* args)
+{
+    extern const char* __TEST__NAME;
+    if (args)
+    {
+        printf("RUNNING: %s\n", __TEST__NAME);
+        static const char* argv[] = { "test", "hello", };
+        static int argc = sizeof(argv) / sizeof(argv[0]);
+	printf("\n before main %d\n",argc);
+        args->ret = main(argc, argv);
+	printf("\n in main\n");
+        args->test = OE_HostStrdup(__TEST__NAME);
+    }
+}
