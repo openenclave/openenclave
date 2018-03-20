e69445266cc3b2c393c7c419e1fde0b0e4ae0542
Mike Brasher <mikbras@microsoft.com>
2018-03-20 18:35:18 -0500

    Added license header

--- enclave/hexdump.c
+++ enclave/hexdump.c
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include <openenclave/enclave.h>
 
 /* Convert a nibble to an ASCII character: Example 0xF => 'F' */
