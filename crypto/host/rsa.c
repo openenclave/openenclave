c97dc29e0f6f26f48b1208c1906febeb7a7bf4ad
Mike Brasher <mikbras@microsoft.com>
2018-03-26 19:39:21 -0500

    Removed unecessary PEM marshalling

--- crypto/host/rsa.c
+++ crypto/host/rsa.c
@@ -177,235 +177,6 @@ OE_CATCH:
     return result;
 }
 
-OE_Result OE_RSAFree(OE_RSA_KEY* key)
-{
-    OE_Result result = OE_UNEXPECTED;
-    OE_RSA_KEY_IMPL* impl = (OE_RSA_KEY_IMPL*)key;
-
-    /* Check the parameter */
-    if (!_ValidImpl(impl))
-        OE_THROW(OE_INVALID_PARAMETER);
-
-    /* Release the RSA object */
-    RSA_free(impl->rsa);
-
-    /* Clear the fields in the implementation */
-    _ClearImpl(impl);
-
-    OE_THROW(OE_OK);
-
-OE_CATCH:
-    return result;
-}
-
-OE_Result OE_RSASign(
-    const OE_RSA_KEY* privateKey,
-    OE_HashType hashType,
-    const void* hashData,
-    size_t hashSize,
-    uint8_t* signature,
-    size_t* signatureSize)
-{
-    OE_Result result = OE_UNEXPECTED;
-    const OE_RSA_KEY_IMPL* impl = (OE_RSA_KEY_IMPL*)privateKey;
-    int type = _MapHashType(hashType);
-
-    /* Check for null parameters */
-    if (!_ValidImpl(impl) || !hashData || !hashSize || !signatureSize)
-        OE_THROW(OE_INVALID_PARAMETER);
-
-    /* If signature buffer is null, then signature size must be zero */
-    if (!signature && *signatureSize != 0)
-        OE_THROW(OE_INVALID_PARAMETER);
-
-    /* Initialize OpenSSL */
-    OE_InitializeOpenSSL();
-
-    /* Determine the size of the signature; fail if buffer is too small */
-    {
-        size_t size = RSA_size(impl->rsa);
-
-        if (size > *signatureSize)
-        {
-            *signatureSize = size;
-            OE_THROW(OE_BUFFER_TOO_SMALL);
-        }
-
-        *signatureSize = size;
-    }
-
-    /* Verify that the data is signed by the given RSA private key */
-    unsigned int siglen;
-    if (!RSA_sign(type, hashData, hashSize, signature, &siglen, impl->rsa))
-        OE_THROW(OE_FAILURE);
-
-    /* This should never happen */
-    if (siglen != *signatureSize)
-        OE_THROW(OE_UNEXPECTED);
-
-    OE_THROW(OE_OK);
-
-OE_CATCH:
-
-    return result;
-}
-
-OE_Result OE_RSAVerify(
-    const OE_RSA_KEY* publicKey,
-    OE_HashType hashType,
-    const void* hashData,
-    size_t hashSize,
-    const uint8_t* signature,
-    size_t signatureSize)
-{
-    OE_Result result = OE_UNEXPECTED;
-    const OE_RSA_KEY_IMPL* impl = (OE_RSA_KEY_IMPL*)publicKey;
-    int type = _MapHashType(hashType);
-
-    /* Check for null parameters */
-    if (!_ValidImpl(impl) || !hashSize || !hashData || !signature ||
-        signatureSize == 0)
-    {
-        OE_THROW(OE_INVALID_PARAMETER);
-    }
-
-    /* Initialize OpenSSL */
-    OE_InitializeOpenSSL();
-
-    /* Verify that the data is signed by the given RSA private key */
-    if (!RSA_verify(
-            type, hashData, hashSize, signature, signatureSize, impl->rsa))
-    {
-        OE_THROW(OE_FAILURE);
-    }
-
-    OE_THROW(OE_OK);
-
-OE_CATCH:
-
-    return result;
-}
-
-OE_Result OE_RSAGenerate(
-    uint64_t bits,
-    uint64_t exponent,
-    OE_RSA_KEY* privateKey,
-    OE_RSA_KEY* publicKey)
-{
-    OE_Result result = OE_UNEXPECTED;
-    OE_RSA_KEY_IMPL* privateImpl = (OE_RSA_KEY_IMPL*)privateKey;
-    OE_RSA_KEY_IMPL* publicImpl = (OE_RSA_KEY_IMPL*)publicKey;
-    RSA* rsa = NULL;
-    BIO* bio = NULL;
-    EVP_PKEY* pkey = NULL;
-    const char nullTerminator = '\0';
-
-    _ClearImpl(privateImpl);
-    _ClearImpl(publicImpl);
-
-    /* Check parameters */
-    if (!privateImpl || !publicImpl)
-        OE_THROW(OE_INVALID_PARAMETER);
-
-    /* Check range of bits parameter */
-    if (bits > INT_MAX)
-        OE_THROW(OE_INVALID_PARAMETER);
-
-    /* Check range of exponent parameter */
-    if (exponent > ULONG_MAX)
-        OE_THROW(OE_INVALID_PARAMETER);
-
-    /* Initialize OpenSSL */
-    OE_InitializeOpenSSL();
-
-    /* Generate an RSA key pair */
-    if (!(rsa = RSA_generate_key(bits, exponent, 0, 0)))
-        OE_THROW(OE_FAILURE);
-
-    /* Create private key object */
-    {
-        BUF_MEM* mem;
-
-        if (!(bio = BIO_new(BIO_s_mem())))
-            OE_THROW(OE_FAILURE);
-
-        if (!PEM_write_bio_RSAPrivateKey(bio, rsa, NULL, NULL, 0, NULL, NULL))
-            OE_THROW(OE_FAILURE);
-
-        if (BIO_write(bio, &nullTerminator, sizeof(nullTerminator)) <= 0)
-            OE_THROW(OE_FAILURE);
-
-        if (!BIO_get_mem_ptr(bio, &mem))
-            OE_THROW(OE_FAILURE);
-
-        if (OE_RSAReadPrivateKeyPEM(
-                (uint8_t*)mem->data, mem->length, privateKey) != OE_OK)
-        {
-            OE_THROW(OE_FAILURE);
-        }
-
-        BIO_free(bio);
-        bio = NULL;
-    }
-
-    /* Create public key object */
-    {
-        BUF_MEM* mem;
-
-        if (!(bio = BIO_new(BIO_s_mem())))
-            OE_THROW(OE_FAILURE);
-
-        if (!(pkey = EVP_PKEY_new()))
-            OE_THROW(OE_FAILURE);
-
-        if (!(EVP_PKEY_assign_RSA(pkey, rsa)))
-            OE_THROW(OE_FAILURE);
-
-        rsa = NULL;
-
-        if (!PEM_write_bio_PUBKEY(bio, pkey))
-            OE_THROW(OE_FAILURE);
-
-        if (BIO_write(bio, &nullTerminator, sizeof(nullTerminator)) <= 0)
-            OE_THROW(OE_FAILURE);
-
-        BIO_get_mem_ptr(bio, &mem);
-
-        if (OE_RSAReadPublicKeyPEM(
-                (uint8_t*)mem->data, mem->length, publicKey) != OE_OK)
-        {
-            OE_THROW(OE_FAILURE);
-        }
-
-        BIO_free(bio);
-        bio = NULL;
-    }
-
-    OE_THROW(OE_OK);
-
-OE_CATCH:
-
-    if (rsa)
-        RSA_free(rsa);
-
-    if (bio)
-        BIO_free(bio);
-
-    if (pkey)
-        EVP_PKEY_free(pkey);
-
-    if (result != OE_OK)
-    {
-        if (_ValidImpl(privateImpl))
-            OE_RSAFree(privateKey);
-
-        if (_ValidImpl(publicImpl))
-            OE_RSAFree(publicKey);
-    }
-
-    return result;
-}
-
 OE_Result OE_RSAWritePrivateKeyPEM(
     const OE_RSA_KEY* key,
     uint8_t* data,
@@ -541,3 +312,189 @@ OE_CATCH:
 
     return result;
 }
+
+OE_Result OE_RSAFree(OE_RSA_KEY* key)
+{
+    OE_Result result = OE_UNEXPECTED;
+    OE_RSA_KEY_IMPL* impl = (OE_RSA_KEY_IMPL*)key;
+
+    /* Check the parameter */
+    if (!_ValidImpl(impl))
+        OE_THROW(OE_INVALID_PARAMETER);
+
+    /* Release the RSA object */
+    RSA_free(impl->rsa);
+
+    /* Clear the fields in the implementation */
+    _ClearImpl(impl);
+
+    OE_THROW(OE_OK);
+
+OE_CATCH:
+    return result;
+}
+
+OE_Result OE_RSASign(
+    const OE_RSA_KEY* privateKey,
+    OE_HashType hashType,
+    const void* hashData,
+    size_t hashSize,
+    uint8_t* signature,
+    size_t* signatureSize)
+{
+    OE_Result result = OE_UNEXPECTED;
+    const OE_RSA_KEY_IMPL* impl = (OE_RSA_KEY_IMPL*)privateKey;
+    int type = _MapHashType(hashType);
+
+    /* Check for null parameters */
+    if (!_ValidImpl(impl) || !hashData || !hashSize || !signatureSize)
+        OE_THROW(OE_INVALID_PARAMETER);
+
+    /* If signature buffer is null, then signature size must be zero */
+    if (!signature && *signatureSize != 0)
+        OE_THROW(OE_INVALID_PARAMETER);
+
+    /* Initialize OpenSSL */
+    OE_InitializeOpenSSL();
+
+    /* Determine the size of the signature; fail if buffer is too small */
+    {
+        size_t size = RSA_size(impl->rsa);
+
+        if (size > *signatureSize)
+        {
+            *signatureSize = size;
+            OE_THROW(OE_BUFFER_TOO_SMALL);
+        }
+
+        *signatureSize = size;
+    }
+
+    /* Verify that the data is signed by the given RSA private key */
+    unsigned int siglen;
+    if (!RSA_sign(type, hashData, hashSize, signature, &siglen, impl->rsa))
+        OE_THROW(OE_FAILURE);
+
+    /* This should never happen */
+    if (siglen != *signatureSize)
+        OE_THROW(OE_UNEXPECTED);
+
+    OE_THROW(OE_OK);
+
+OE_CATCH:
+
+    return result;
+}
+
+OE_Result OE_RSAVerify(
+    const OE_RSA_KEY* publicKey,
+    OE_HashType hashType,
+    const void* hashData,
+    size_t hashSize,
+    const uint8_t* signature,
+    size_t signatureSize)
+{
+    OE_Result result = OE_UNEXPECTED;
+    const OE_RSA_KEY_IMPL* impl = (OE_RSA_KEY_IMPL*)publicKey;
+    int type = _MapHashType(hashType);
+
+    /* Check for null parameters */
+    if (!_ValidImpl(impl) || !hashSize || !hashData || !signature ||
+        signatureSize == 0)
+    {
+        OE_THROW(OE_INVALID_PARAMETER);
+    }
+
+    /* Initialize OpenSSL */
+    OE_InitializeOpenSSL();
+
+    /* Verify that the data is signed by the given RSA private key */
+    if (!RSA_verify(
+            type, hashData, hashSize, signature, signatureSize, impl->rsa))
+    {
+        OE_THROW(OE_FAILURE);
+    }
+
+    OE_THROW(OE_OK);
+
+OE_CATCH:
+
+    return result;
+}
+
+OE_Result OE_RSAGenerate(
+    uint64_t bits,
+    uint64_t exponent,
+    OE_RSA_KEY* privateKey,
+    OE_RSA_KEY* publicKey)
+{
+    OE_Result result = OE_UNEXPECTED;
+    OE_RSA_KEY_IMPL* privateImpl = (OE_RSA_KEY_IMPL*)privateKey;
+    OE_RSA_KEY_IMPL* publicImpl = (OE_RSA_KEY_IMPL*)publicKey;
+    RSA* rsa = NULL;
+    BIO* bio = NULL;
+    EVP_PKEY* pkey = NULL;
+
+    _ClearImpl(privateImpl);
+    _ClearImpl(publicImpl);
+
+    /* Check parameters */
+    if (!privateImpl || !publicImpl)
+        OE_THROW(OE_INVALID_PARAMETER);
+
+    /* Check range of bits parameter */
+    if (bits > INT_MAX)
+        OE_THROW(OE_INVALID_PARAMETER);
+
+    /* Check range of exponent parameter */
+    if (exponent > ULONG_MAX)
+        OE_THROW(OE_INVALID_PARAMETER);
+
+    /* Initialize OpenSSL */
+    OE_InitializeOpenSSL();
+
+    /* Generate an RSA key pair */
+    if (!(rsa = RSA_generate_key(bits, exponent, 0, 0)))
+        OE_THROW(OE_FAILURE);
+
+    /* Copy the private key from the key-pair */
+    {
+        if (!(privateImpl->rsa = RSAPrivateKey_dup(rsa)))
+            OE_THROW(OE_FAILURE);
+
+        privateImpl->magic = OE_RSA_KEY_MAGIC;
+    }
+
+    /* Copy the public key from the key-pair */
+    {
+        if (!(publicImpl->rsa = RSAPublicKey_dup(rsa)))
+            OE_THROW(OE_FAILURE);
+
+        publicImpl->magic = OE_RSA_KEY_MAGIC;
+    }
+
+    OE_THROW(OE_OK);
+
+OE_CATCH:
+
+    if (rsa)
+        RSA_free(rsa);
+
+    if (bio)
+        BIO_free(bio);
+
+    if (pkey)
+        EVP_PKEY_free(pkey);
+
+    if (result != OE_OK)
+    {
+        if (_ValidImpl(privateImpl))
+            OE_RSAFree(privateKey);
+
+        if (_ValidImpl(publicImpl))
+            OE_RSAFree(publicKey);
+    }
+
+    return result;
+}
+
