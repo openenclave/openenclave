7c72c659a2f49d19dc81fd4f7801193d3c2c1a00
anakrish <35780660+anakrish@users.noreply.github.com>
2018-03-30 14:22:12 -0700

    1. Handle spurios wake. (#165)
    2. Use private flag since the mutex is not expected to be shared across process.

--- host/ocalls.c
+++ host/ocalls.c
@@ -91,7 +91,23 @@ void HandleThreadWait(OE_Enclave* enclave, uint64_t argIn)
 #if defined(__linux__)
 
     if (__sync_fetch_and_add(&event->value, -1) == 0)
-        syscall(__NR_futex, &event->value, FUTEX_WAIT, -1, NULL, NULL, 0);
+    {
+        do
+        {
+            syscall(
+                __NR_futex,
+                &event->value,
+                FUTEX_WAIT_PRIVATE,
+                -1,
+                NULL,
+                NULL,
+                0);
+            // If event->value is still -1, then this is a spurious-wake.
+            // Spurious-wakes are ignored by going back to FUTEX_WAIT.
+            // Since FUTEX_WAIT uses atomic instructions to load event->value,
+            // it is safe to use a non-atomic operation here.
+        } while (event->value == -1);
+    }
 
 #elif defined(_WIN32)
 
@@ -109,7 +125,8 @@ void HandleThreadWake(OE_Enclave* enclave, uint64_t argIn)
 #if defined(__linux__)
 
     if (__sync_fetch_and_add(&event->value, 1) != 0)
-        syscall(__NR_futex, &event->value, FUTEX_WAKE, 1, NULL, NULL, 0);
+        syscall(
+            __NR_futex, &event->value, FUTEX_WAKE_PRIVATE, 1, NULL, NULL, 0);
 
 #elif defined(_WIN32)
 
