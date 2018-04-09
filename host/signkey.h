0e80ded88e4a1d7edde171ba90d68cf0ea7c821c
Mike Brasher <mikbras@microsoft.com>
2018-04-09 17:57:05 -0500

    Moved sign key to own file

---
+++ host/signkey.h
@@ -0,0 +1,11 @@
+#ifndef _OE_HOST_SIGNKEY_H
+#define _OE_HOST_SIGNKEY_H
+
+#include <openenclave/defs.h>
+#include <openenclave/types.h>
+
+extern const char OE_DEBUG_SIGN_KEY[];
+
+extern size_t OE_DEBUG_SIGN_KEY_SIZE;
+
+#endif /* _OE_HOST_SIGNKEY_H */
