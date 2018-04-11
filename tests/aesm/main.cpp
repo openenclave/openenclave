e8f8c19362f300f2370e5870cc00a02c537dbf75
Simon Leet <simon.leet@microsoft.com>
2018-04-10 20:10:46 -0700

    Prototype libsgx integration
    * Add 3rdparty/libsgx to pull external package and publish cmake targets
    * Add USE_LIBSGX cmake option for compiling with libsgx dependency
    * Add use of libsgx interface to sgxload methods
    * Modify tests/aesm to smoke test quoting enclave loading by libsgx

--- tests/aesm/main.cpp
+++ tests/aesm/main.cpp
@@ -1,18 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <openenclave/bits/aesm.h>
 #include <openenclave/bits/tests.h>
 #include <openenclave/host.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#if defined(OE_USE_LIBSGX)
+#include <sgx_ql_oe_wrapper.h>
+#else
+#include <openenclave/bits/aesm.h>
+#endif
+
 #define SKIP_RETURN_CODE 2
 
 int main(int argc, const char* argv[])
 {
-    AESM* aesm;
-
     const uint32_t flags = OE_GetCreateFlags();
     if ((flags & OE_ENCLAVE_FLAG_SIMULATE) != 0)
     {
@@ -22,11 +25,22 @@ int main(int argc, const char* argv[])
         return SKIP_RETURN_CODE;
     }
 
+#if defined(OE_USE_LIBSGX)
+    quote3_error_t err;
+    sgx_target_info_t targetInfo = {};
+    if (SGX_QL_SUCCESS != (err = sgx_qe_get_target_info(&targetInfo)))
+    {
+        printf("FAILED: Call returned %x\n", err);
+        return -1;
+    }
+#else
+    AESM* aesm;
     if (!(aesm = AESMConnect()))
     {
         fprintf(stderr, "%s: failed to connect\n", argv[0]);
         exit(1);
     }
+#endif
 
     printf("=== passed all tests (%s)\n", argv[0]);
 
