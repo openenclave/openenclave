0ef23d676d385a28bfe65acc81c77d23c523c11d
Anita Govindarajan <anitagov@hotmail.com>
2018-04-12 10:40:00 -0700

    Moved stdbool.h include to OE section

--- 3rdparty/libunwind/libunwind/src/unwind/RaiseException.c
+++ 3rdparty/libunwind/libunwind/src/unwind/RaiseException.c
@@ -23,10 +23,10 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
-#include <stdbool.h>
 #include "unwind-internal.h"
 
 #ifdef OPEN_ENCLAVE
+#include <stdbool.h>
 extern bool OE_IsWithinEnclave(const void* ptr, size_t size);
 
 //
