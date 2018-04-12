fd10a9e6ad8a97c1785a49454bd658a92ec03d45
Anita Govindarajan <anitagov@hotmail.com>
2018-04-12 16:31:45 -0700

    Workaround for xrstor64 faulting with optimized builds  (#194)
    * Workaround for #144 xrstor64 fault
    
    * Minor change
    
    * xrstor64 fault wrkaround - xsave_area made static and left over formatting changes

--- libc/locale.c
+++ libc/locale.c
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <locale.h>
 #include <libc.h>
+#include <locale.h>
 #include <string.h>
 
 /* ATTN: should these assert? */
@@ -35,7 +35,7 @@ static struct lconv posix_lconv = {
     .int_n_sign_posn = -1,
 };
 
-static const struct __locale_struct c_locale = { 0 };
+static const struct __locale_struct c_locale = {0};
 
 locale_t uselocale(locale_t newloc)
 {
@@ -63,7 +63,8 @@ locale_t newlocale(int mask, const char* locale, locale_t loc)
     /* Currently we will support basic C/POSIX locale */
     builtin = (locale[0] == 'C' && !locale[1]) || !strcmp(locale, "POSIX");
 
-    if (builtin) {
+    if (builtin)
+    {
         /* if locale is already allocated, we need to modify it
            according to the mask value. Currently we are supporting
            builtin locales only. So locale modify has no effect. Just return
@@ -72,10 +73,14 @@ locale_t newlocale(int mask, const char* locale, locale_t loc)
         if (loc)
             return loc;
         else
-            /* if loc is NULL/0, then we will return a dummy C locale reference */
+            /* if loc is NULL/0, then we will return a dummy C locale reference
+             */
             return (locale_t)&c_locale;
-    } else {
-        /*  Enclave doesnt support any locales other than builtin for the time being */
+    }
+    else
+    {
+        /*  Enclave doesnt support any locales other than builtin for the time
+         * being */
         return 0;
     }
 }
