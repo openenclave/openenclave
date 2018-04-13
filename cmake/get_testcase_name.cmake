ff50a6a6a6bbc415edbfef9fc642d98512d668dd
vishnumohan1991 <vishnu.mohan@vvdntech.in>
2018-04-13 17:28:54 +0530

    adding iterated code for simon's comment

--- cmake/get_testcase_name.cmake
+++ cmake/get_testcase_name.cmake
@@ -12,9 +12,9 @@
 # filename - filename containing the test
 # namevar - variable to receive the testcase name
 #
-function(get_testcase_name FILENAME NAMEVAR)
+function(get_testcase_name FILENAME NAMEVAR PREFIX)
         string(REGEX REPLACE "\.c(pp)?$" "" n ${FILENAME})
-        string(REGEX REPLACE "^\.\./\.\./3rdparty/(mbedtls/mbedtls/programs/test/|libcxx/libcxx/test/|musl/libc-test/src/)" "" n ${n})
+        string(REGEX REPLACE ${PREFIX} "" n ${n})
         string(REGEX REPLACE "[/=]" "_" n ${n})
         string(REGEX REPLACE "[\!]" "-" n ${n})
 	set(${NAMEVAR} ${n} PARENT_SCOPE)
