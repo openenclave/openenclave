a007dbb8dc70b29c9daff4e6c513e11b56a51339
Anita Govindarajan <anitagov@hotmail.com>
2018-04-19 18:23:28 -0700

    Fixed issues with TestVectorException and TestGetsecInstruction

--- tests/VectorException/args.h
+++ tests/VectorException/args.h
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#ifndef _ARGS_H
-#define _ARGS_H
+#ifndef _vectorException_args_h
+#define _vectorException_args_h
 #include <openenclave/bits/cpuid.h>
 
 typedef struct _TestVectorExceptionArgs
@@ -14,6 +14,8 @@ typedef struct _TestSigillHandlingArgs
 {
     int ret;
     uint32_t cpuidTable[OE_CPUID_LEAF_COUNT][OE_CPUID_REG_COUNT];
+    uint32_t r1;
+    uint32_t r2;
 } TestSigillHandlingArgs;
 
 #endif /* _ARGS_H */
