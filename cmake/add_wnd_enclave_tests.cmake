d6dfb8e380344b114b4b9c98d518a06e703ad1ef
Rajesh Rathod <rajeshvrathod@gmail.com>
2018-04-26 16:58:48 +0530

    HOST/ENC RENAME CHANGES - All the host and signed so file names were changed to keep name consistency and can be used with add_wnd_enclave_tests function with single parameter only.

--- cmake/add_wnd_enclave_tests.cmake
+++ cmake/add_wnd_enclave_tests.cmake
@@ -1,21 +1,15 @@
-## ATTN: Instead of doing a bulk copy and then writing a custom action to 
-## enumerate all tests to add_test, create a custom task that you can use 
-## on a per test basis, for example:
+## This function is used on Windows only.
+## This function is called to copy linux signed.so file from linux to corresponding windows build location and will add tests for it.
+## TESTNAME : test name for which corresponding signed.so file should be copied from linux and this name is also used for add test.
 
-function(add_wnd_enclave_tests TESTNAME EXE_FILE SIGNED_BIN)
-
-	Message("add_wnd_enclave_tests called for .....................")
-	Message(${TESTNAME})
-	Message(${LINUX_BIN_DIR}/${TESTNAME}/enc/${SIGNED_BIN})
-	Message(${CMAKE_CURRENT_BINARY_DIR}/host/${SIGNED_BIN})
-
+function(add_wnd_enclave_tests TESTNAME)
 
 	# custom rule to copy binary from linux
 	# take a dependency on host binary to make sure it exists in addition to 
 	# enc binary in linux
 	add_custom_command(OUTPUT ${TESTNAME}_windows_include
-		COMMAND ${CMAKE_COMMAND} -E copy ${LINUX_BIN_DIR}/${TESTNAME}/enc/${SIGNED_BIN} ${CMAKE_CURRENT_BINARY_DIR}/host/${SIGNED_BIN}
-		DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/host/${EXE_FILE} ${LINUX_BIN_DIR}/${TESTNAME}/enc/${SIGNED_BIN}
+		COMMAND ${CMAKE_COMMAND} -E copy ${LINUX_BIN_DIR}/${TESTNAME}/enc/${TESTNAME}_enc.signed.so ${CMAKE_CURRENT_BINARY_DIR}/host/${TESTNAME}_enc.signed.so
+		DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/host/${TESTNAME}_host ${LINUX_BIN_DIR}/${TESTNAME}/enc/${TESTNAME}_enc.signed.so
 		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 		)
 	# add a custom target to ALL so that this step always needs to be run if 
@@ -26,6 +20,6 @@ function(add_wnd_enclave_tests TESTNAME EXE_FILE SIGNED_BIN)
 
 	# add the test (this assumes the syntax correctly copied the linux enc signed 
 	# binary into the host subdir in add_custom_command; tweak it as needed)
-	add_test(tests/${TESTNAME} ${CMAKE_CURRENT_BINARY_DIR}/host/${EXE_FILE} ${CMAKE_CURRENT_BINARY_DIR}/host/${SIGNED_BIN})
+	add_test(tests/${TESTNAME} ${CMAKE_CURRENT_BINARY_DIR}/host/${TESTNAME}_host ${CMAKE_CURRENT_BINARY_DIR}/host/${TESTNAME}_enc.signed.so)
 		
 endfunction(add_wnd_enclave_tests)
