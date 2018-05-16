780e13447d12778cb77b7dbddbeeda0fd3bf38d8
vishnumohan1991 <36887687+vishnumohan1991@users.noreply.github.com>
2018-05-16 13:07:36 +0530

    Adding dynamic configuration option for mbedtls Certs and mbedtls Debug (#230)
    * Adding support for CERT and DEBUG for mbedtls config when buildtype is debug default configuration is disabled
    
    * Bug fix for release build and reverting to default config.h
    
    * moving temp file location under build

--- 3rdparty/mbedtls/config.h
+++ 3rdparty/mbedtls/config.h
@@ -1776,8 +1776,7 @@
  *
  * This module is used for testing (ssl_client/server).
  */
-//Open Enclave TODO: disable in non-debug builds
-#define MBEDTLS_CERTS_C
+//#define MBEDTLS_CERTS_C
 
 /**
  * \def MBEDTLS_CIPHER_C
@@ -1831,8 +1830,7 @@
  *
  * This module provides debugging functions.
  */
- //Open Enclave TODO: disable in non-debug builds
-#define MBEDTLS_DEBUG_C
+//#define MBEDTLS_DEBUG_C
 
 /**
  * \def MBEDTLS_DES_C
