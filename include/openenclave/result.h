84143515134aae3f3d187ee781f2dcca1b4df6fd
Anita Govindarajan <anitagov@hotmail.com>
2018-05-23 16:55:45 -0700

    #216 - Fail enclave init if AESNI isn't present

--- include/openenclave/result.h
+++ include/openenclave/result.h
@@ -54,7 +54,8 @@ typedef enum _OE_Result {
     OE_INVALID_CPUSVN,
     OE_INVALID_ISVSVN,
     OE_INVALID_KEYNAME,
-    OE_DEBUG_DOWNGRADE
+    OE_DEBUG_DOWNGRADE,
+    OE_AESNI_FAILED
 } OE_Result;
 
 /**
