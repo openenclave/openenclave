a6ca322be68c75d91ac0a110ca8f75cf12adef80
Mike Brasher <mikbras@microsoft.com>
2018-06-04 17:04:32 -0500

    format-code

--- tests/pingpong-shared/host/main.cpp
+++ tests/pingpong-shared/host/main.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 extern int main_shared(int argc, const char* argv[]);
 
 int main(int argc, const char* argv[])
