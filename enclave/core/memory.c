b8a699e2e7005d267741479c46832156631c375f
Anita Govindarajan <anitagov@hotmail.com>
2018-06-07 23:18:16 -0700

    OE_IsOutsideEnclave fixes - added new case and wrapping tests

--- enclave/core/memory.c
+++ enclave/core/memory.c
@@ -32,15 +32,35 @@ bool OE_IsOutsideEnclave(const void* p, size_t n)
     const uint8_t* base = (const uint8_t*)__OE_GetEnclaveBase();
     uint64_t size = __OE_GetEnclaveSize();
 
+    // Postcondition tests to verify that wrapping didn't occur
+    if (end < start)
+    {
+        return false;
+    }
+
+    if (base + size < base)
+    {
+        return false;
+    }
+
+    // Case 1 - Start is within the enclave region
     if (!(start < base || start >= (base + size)))
         return false;
 
     if (n)
     {
         end--;
-
+        // Case 2 - Range end is within the enclave (at base or beyond but
+        // within the enclave)
         if (!(end < base || end >= (base + size)))
             return false;
+    }
+
+    // Case 3 - Range starts at/before the enclave starts and ends at/beyond the
+    // enclave
+    if ((start <= base) && end >= (base + size))
+    {
+        return false;
     }
 
     return true;
