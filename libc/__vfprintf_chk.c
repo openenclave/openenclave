421d8cc4e89aa56be81da5bb3e5f82d2171797b9
Mike Brasher <mikbras@microsoft.com>
2018-06-14 17:11:51 -0500

    Fixed typos

--- libc/__vfprintf_chk.c
+++ libc/__vfprintf_chk.c
@@ -2,9 +2,10 @@
 #include <stdarg.h>
 #include <assert.h>
 
-// GCC sometimes replaces vfpintf() calls with __vfprintf_chk() calls. In glibc 
-// this function sets the output stream's _IO_FLAGS2_FORTIFY flag, which causes // glibc to perform various checks on the output stream. Since MUSL has no 
-// equivalent flag, this implementation simply calls vfprintf().
+// GCC sometimes replaces vfprintf() calls with __vfprintf_chk() calls. In 
+// glibc this function sets the output stream's _IO_FLAGS2_FORTIFY flag, which 
+// causes glibc to perform various checks on the output stream. Since MUSL has 
+// no equivalent flag, this implementation simply calls vfprintf().
 int __vfprintf_chk(FILE *stream, const char *format, va_list ap)
 {
     return vfprintf(stream, format, ap);
