a99ab206673a9fe4acd2cde7334e1410c6c0de86
Mike Brasher <mikbras@microsoft.com>
2018-06-16 12:52:23 -0500

    Spelling

--- enclave/init.c
+++ enclave/init.c
@@ -1,7 +1,7 @@
 #include "report.h"
 
 // This function overrides the weak version defined in liboecore. When the
-// enclave appliation is linked with liboeenclave, then liboecore calls
+// enclave application is linked with liboeenclave, then liboecore calls
 // this function to initialize the liboeenclave library. Otherwise, liboecore
 // calls its own weak version.
 void __liboeenclave_init(void)
