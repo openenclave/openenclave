e0d21acb6e47b86e507bcc6646a82980638e9efc
Zehra Naz <zenaz@microsoft.com>
2018-06-20 14:31:57 -0700

    Make the input to cpuid, eax, =1

--- tests/VectorException/enc/init.cpp
+++ tests/VectorException/enc/init.cpp
@@ -20,9 +20,14 @@ int test_cpuid_instruction(unsigned int what)
 {
     if (!done)
     {
-        asm("movl  $1, %eax");
+        /*asm("movl  $1, %%eax   \n\t"
+            "cpuid             \n\t"
+            : "=c"(c)
+            :
+            : "eax", "ebx", "edx");*/
+        
         unsigned int a,b,d;
-        oe_get_cpuid(0, 0, &a, &b, &c, &d);
+        oe_get_cpuid(1, 0, &a, &b, &c, &d);
         // This should be executed only once.
         ++hits1;
         done = 1;
