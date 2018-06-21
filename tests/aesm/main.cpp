fb84e4f8b03e8fe46eba4cc699bc5e10a943a60d
Zehra Naz <zenaz@microsoft.com>
2018-06-20 18:24:39 -0700

    add utils test to aesm

--- tests/aesm/main.cpp
+++ tests/aesm/main.cpp
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//todo: remove this
+#include <openenclave/internal/cpuid.h>
+#include <openenclave/internal/tests.h>
+
 #if defined(OE_USE_LIBSGX)
 #include <sgx_ql_oe_wrapper.h>
 #else
@@ -13,6 +17,42 @@
 #endif
 
 #define SKIP_RETURN_CODE 2
+
+void asm_cpuid(unsigned int leaf, unsigned int subleaf, unsigned int* eax, unsigned int* ebx, unsigned int* ecx, unsigned int* edx)
+{
+    if (subleaf == NULL)
+    {
+        asm("cpuid\n\t"					
+	       : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)	
+	       : "0" (leaf));
+    }
+
+    asm("cpuid\n\t"					
+	   : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)	
+	   : "0" (leaf), "2" (subleaf));
+}
+
+void TestCpuidAgainstAssembly(unsigned int* leaf, unsigned int* subleaf)
+{
+    unsigned int a_asm = 0, b_asm = 0, c_asm = 0, d_asm = 0;
+    unsigned int a = 0, b = 0, c = 0, d = 0;
+
+    if (subleaf == NULL)
+    {
+        oe_get_cpuid(*leaf, 0, &a, &b, &c, &d);
+    }
+    else
+    {
+        oe_get_cpuid(*leaf, *subleaf, &a, &b, &c, &d);
+    }
+
+    asm_cpuid(*leaf, *subleaf, &a_asm, &b_asm, &c_asm, &d_asm);
+
+    OE_TEST(a == a_asm);
+    OE_TEST(b == b_asm);
+    OE_TEST(c == c_asm);
+    OE_TEST(d == d_asm);
+}
 
 int main(int argc, const char* argv[])
 {
@@ -41,6 +81,20 @@ int main(int argc, const char* argv[])
         exit(1);
     }
 #endif
+
+    // TODO REMOVE
+    unsigned int leaf = 0, subleaf = 0;
+    TestCpuidAgainstAssembly(&leaf, &subleaf);
+
+    // Since the subleaf is always required by our api, test out if 0 and not giving the field yield the same values.
+    TestCpuidAgainstAssembly(&leaf, NULL);
+
+    leaf = 0x80000000;
+    TestCpuidAgainstAssembly(&leaf, &subleaf);
+
+    leaf = 0x80000001;
+    TestCpuidAgainstAssembly(&leaf, &subleaf);
+    // REMOVE
 
     printf("=== passed all tests (%s)\n", argv[0]);
 
