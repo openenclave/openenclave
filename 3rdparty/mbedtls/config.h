98a0f251fdd3e592f1edc96e5d9976de1fc85704
Mike Brasher <mikbras@microsoft.com>
2018-06-29 14:44:53 -0500

    Enabled MBEDTLS threading

--- 3rdparty/mbedtls/config.h
+++ 3rdparty/mbedtls/config.h
@@ -1413,7 +1413,7 @@
  *
  * Uncomment this to enable pthread mutexes.
  */
-//#define MBEDTLS_THREADING_PTHREAD
+#define MBEDTLS_THREADING_PTHREAD
 
 /**
  * \def MBEDTLS_VERSION_FEATURES
@@ -2463,7 +2463,7 @@
  * Enable this layer to allow use of mutexes within mbed TLS
  */
 //Open Enclave TODO: enable and test threading in enclave context
-//#define MBEDTLS_THREADING_C
+#define MBEDTLS_THREADING_C
 
 /**
  * \def MBEDTLS_TIMING_C
