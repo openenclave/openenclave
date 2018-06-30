ff0656455897b9a409392c89c8e51c7c7ea8130f
Mike Brasher <mikbras@microsoft.com>
2018-06-30 13:57:02 -0500

    format-code

--- common/cert.c
+++ common/cert.c
@@ -4,9 +4,9 @@
 #include <openenclave/internal/asn1.h>
 #include <openenclave/internal/cert.h>
 #include <openenclave/internal/outbuf.h>
+#include <openenclave/internal/print.h>
 #include <openenclave/internal/raise.h>
 #include <openenclave/internal/utils.h>
-#include <openenclave/internal/print.h>
 
 #ifdef OE_BUILD_ENCLAVE
 #include <openenclave/internal/enclavelibc.h>
