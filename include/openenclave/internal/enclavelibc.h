7708722ea1ef7a3a5be90f2b43c3c62896d50fe8
Mike Brasher <mikbras@microsoft.com>
2018-06-30 17:29:01 -0500

    Restore enclavelic.h

---
+++ include/openenclave/internal/enclavelibc.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef _OE_ENCLAVELIBC_H
+#define _OE_ENCLAVELIBC_H
+
+#include "enclavelibc/errno.h"
+#include "enclavelibc/limits.h"
+#include "enclavelibc/pthread.h"
+#include "enclavelibc/sched.h"
+#include "enclavelibc/stdarg.h"
+#include "enclavelibc/stddef.h"
+#include "enclavelibc/stdint.h"
+#include "enclavelibc/stdio.h"
+#include "enclavelibc/stdlib.h"
+#include "enclavelibc/string.h"
+#include "enclavelibc/time.h"
+#include "enclavelibc/unistd.h"
+
+#endif /* _OE_ENCLAVELIBC_H */
