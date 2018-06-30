549f08128fa716ae37eaac5002524c5711230f30
Mike Brasher <mikbras@microsoft.com>
2018-06-30 18:47:51 -0500

    Comments

--- include/openenclave/internal/enclavelibc/setjmp.h
+++ include/openenclave/internal/enclavelibc/setjmp.h
@@ -10,11 +10,10 @@ OE_ENCLAVELIBC_EXTERNC_BEGIN
 
 typedef struct _oe_jmp_buf
 {
-    /* These are the registers that are preserved across function calls
-     * according to the 'System V AMD64 ABI' calling conventions:
-     * RBX, RSP, RBP, R12, R13, R14, R15. In addition, oe_setjmp() saves
-     * the RIP register (instruction pointer) to know where to jump back to).
-     */
+    // These are the registers that are preserved across function calls
+    // according to the 'System V AMD64 ABI' calling conventions:
+    // RBX, RSP, RBP, R12, R13, R14, R15. In addition, oe_setjmp() saves
+    // the RIP register (instruction pointer) to know where to jump back to).
     uint64_t rsp;
     uint64_t rbp;
     uint64_t rip;
