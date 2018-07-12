22fb13310881709d3457d5bcc3f23e9e552eb067
Mike Brasher <mikbras@microsoft.com>
2018-07-12 18:35:31 -0500

    Removed libunwind_stubs.c from libc (now in header)

--- 3rdparty/libunwind/stubs.h
+++ 3rdparty/libunwind/stubs.h
@@ -4,7 +4,7 @@
 #ifndef __OE_LIBUNWIND_STUBS_H
 #define __OE_LIBUNWIND_STUBS_H
 
-#ifndef __ASSEMBLER__
+#if !defined(__ASSEMBLER__)
 
 #include <pthread.h>
 #include <sys/types.h>
@@ -13,8 +13,34 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <signal.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
 
-void __libunwind_setbuf(FILE *stream, char *buf);
+// Disable use of adaptive mutexes, which are defined by GCC headers but not
+// supported by MUSL pthreads. Note that libunwind is compiled with GCC headers
+// but linked with MUSL libc.
+#ifdef PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP
+#undef PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP
+#define PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP PTHREAD_MUTEX_INITIALIZER
+#endif
+
+/* Redirect these calls to the stubs defined below */
+#define setbuf __libunwind_setbuf
+#define sigfillset __libunwind_sigfillset
+#define getpid __libunwind_getpid
+#define open __libunwind_open
+#define read __libunwind_read
+#define close __libunwind_close
+#define fstat __libunwind_fstat
+#define mmap __libunwind_mmap
+#define munmap __libunwind_munmap
+#define msync __libunwind_msync
+#define mincore __libunwind_mincore
+
+static __inline void __libunwind_setbuf(FILE* stream, char* buf)
+{
+}
 
 static __inline int __libunwind_sigfillset(sigset_t* set)
 {
@@ -26,7 +52,12 @@ static __inline pid_t __libunwind_getpid(void)
     return 0;
 }
 
-static __inline int __libunwind_open(const char *pathname, int flags)
+static __inline int __libunwind_open(const char* pathname, int flags)
+{
+    return -1;
+}
+
+static __inline ssize_t __libunwind_read(int fd, void* buf, size_t count)
 {
     return -1;
 }
@@ -36,48 +67,69 @@ static __inline int __libunwind_close(int fd)
     return -1;
 }
 
-static __inline ssize_t __libunwind_read(int fd, void *buf, size_t count)
+static __inline int __libunwind_fstat(int fd, struct stat* buf)
 {
     return -1;
 }
 
-static __inline int __libunwind_fstat(int fd, struct stat *buf)
+static __inline void* __libunwind_mmap(
+    void* addr,
+    size_t length,
+    int prot,
+    int flags,
+    int fd,
+    off_t offset)
 {
-    return -1;
+    void *memalign(size_t alignment, size_t size);
+    void* result = MAP_FAILED;
+
+    if (addr || fd != -1 || offset)
+        goto done;
+
+    if (prot != (PROT_READ | PROT_WRITE))
+        goto done;
+
+    if (flags != (MAP_PRIVATE | MAP_ANONYMOUS))
+        goto done;
+
+    result = memalign(4096, length);
+
+done:
+
+    return result;
 }
 
-#define mmap __libunwind_mmap
 
-#define munmap __libunwind_munmap
+static __inline int __libunwind_munmap(void* addr, size_t length)
+{
+    if (!addr)
+        return -1;
 
-#define msync __libunwind_msync
+    if (length)
+        free(addr);
 
-#define mincore __libunwind_mincore
+    return 0;
+}
 
-#define libunwind __libunwind_mincore
+static __inline int __libunwind_msync(void* addr, size_t length, int flags)
+{
+    return 0;
+}
 
-#define setbuf __libunwind_setbuf
+static __inline int __libunwind_mincore(
+    void* addr, 
+    size_t length, 
+    unsigned char* vec)
+{
+    if (!addr || !vec)
+        return -1;
 
-#define sigfillset __libunwind_sigfillset
+    size_t n = (length + getpagesize() - 1) / getpagesize();
+    memset(vec, 1, n);
 
-#define getpid __libunwind_getpid
+    return 0;
+}
 
-#define open __libunwind_open
-
-#define read __libunwind_read
-
-#define close __libunwind_close
-
-#define fstat __libunwind_fstat
-
-// Disable use of adaptive mutexes, which are defined by GCC headers but not
-// supported by MUSL pthreads. Note that libunwind is compiled with GCC headers
-// but linked with MUSL libc.
-#ifdef PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP
-#undef PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP
-#define PTHREAD_ADAPTIVE_MUTEX_INITIALIZER_NP PTHREAD_MUTEX_INITIALIZER
-#endif
-
-#endif /* !__ASSEMBLER__ */
+#endif /* !defined(__ASSEMBLER__) */
 
 #endif /* __OE_LIBUNWIND_STUBS_H */
