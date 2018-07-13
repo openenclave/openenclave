e8ae079c252b8a9582521e071eb17c200df34c9b
Mike Brasher <mikbras@microsoft.com>
2018-07-13 16:13:48 -0500

    New oe_backtrace test

---
+++ tests/backtrace/enc/enc.c
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <openenclave/enclave.h>
+#include <openenclave/internal/backtrace.h>
+#include <openenclave/internal/tests.h>
+#include <openenclave/internal/print.h>
+#include "../args.h"
+
+OE_NEVER_INLINE void GetBacktrace(Args* args)
+{
+    args->size = OE_COUNTOF(args->buffer);
+    args->size = oe_backtrace(args->buffer, args->size);
+}
+
+OE_ECALL void Test(void* args_)
+{
+    Args* args = (Args*)args_;
+
+    if (args)
+        GetBacktrace(args);
+}
