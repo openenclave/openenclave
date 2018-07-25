6f3f0327aed554b27875843ff19d0ce9f9a2e053
Mike Brasher <mikbras@microsoft.com>
2018-07-25 14:12:03 -0500

    fix build break

--- include/openenclave/internal/sgxtypes.h
+++ include/openenclave/internal/sgxtypes.h
@@ -867,7 +867,7 @@ typedef enum _oe_sgx_pckid {
     OE_SGX_PCK_ID_ENCRYPTED_PPID_3072 = 3,
     OE_SGX_PCK_ID_PCK_CERTIFICATE = 4,
     OE_SGX_PCK_ID_PCK_CERT_CHAIN = 5,
-    __OE_SGX_PCKID_MAX = OE_MAX_UINT
+    __OE_SGX_PCKID_MAX = OE_ENUM_MAX
 } oe_sgx_pckid_t;
 
 OE_STATIC_ASSERT(sizeof(oe_sgx_pckid_t) == sizeof(unsigned int));
