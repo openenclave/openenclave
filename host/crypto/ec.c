c1de41cefe69f90f32f56a43ee65d6913e8617aa
Mike Brasher <mikbras@microsoft.com>
2018-07-25 14:07:56 -0500

    format-code

--- host/crypto/ec.c
+++ host/crypto/ec.c
@@ -3,9 +3,9 @@
 
 #include "ec.h"
 #include <openenclave/internal/defs.h>
+#include <openenclave/internal/defs.h>
 #include <openenclave/internal/hexdump.h>
 #include <openenclave/internal/raise.h>
-#include <openenclave/internal/defs.h>
 #include <openssl/obj_mac.h>
 #include <openssl/pem.h>
 #include <string.h>
