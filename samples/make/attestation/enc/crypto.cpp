46e246417d1bc4f394f34eb53a3c81760aa370e6
Anita Govindarajan <v-angovi@microsoft.com>
2018-08-09 21:01:38 -0500

    Ran format-code

--- samples/make/attestation/enc/crypto.cpp
+++ samples/make/attestation/enc/crypto.cpp
@@ -100,7 +100,7 @@ static void InitializeMbedtls(void)
  */
 bool InitializeCrypto(void)
 {
-  /* Since samples is single-threaded, okay to not use synch primitives */
+    /* Since samples is single-threaded, okay to not use synch primitives */
     InitializeMbedtls();
     return g_Initialized;
 }
