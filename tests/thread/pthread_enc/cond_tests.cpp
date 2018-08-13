153e339dfa89cc247d2474e372506d879cf8ce02
Anita Govindarajan <anitagov@hotmail.com>
2018-08-13 11:43:20 -0700

    Disabling clang formatting for pthread_enc to prevent incorrect ordering

--- tests/thread/pthread_enc/cond_tests.cpp
+++ tests/thread/pthread_enc/cond_tests.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "../oethread_enc/cond_tests.cpp"
+// clang-format off
 #include "thread.h"
+#include "../oethread_enc/cond_tests.cpp"
+// clang-format on
--- tests/thread/pthread_enc/enc.cpp
+++ tests/thread/pthread_enc/enc.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "../oethread_enc/enc.cpp"
+// clang-format off
 #include "thread.h"
+#include "../oethread_enc/enc.cpp"
+// clang-format on
