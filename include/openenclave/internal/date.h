30f322f70ca48abbf6791ea555af0a5ada91ac51
Mike Brasher <mikbras@microsoft.com>
2018-08-13 17:53:31 -0500

    1-based month

--- include/openenclave/internal/date.h
+++ include/openenclave/internal/date.h
@@ -13,7 +13,7 @@ OE_EXTERNC_BEGIN
 typedef struct _oe_date
 {
     uint32_t year;    /* format: 1970, 2018, 2020 */
-    uint32_t month;   /* range: 0-11 */
+    uint32_t month;   /* range: 1-12 */
     uint32_t day;     /* range: 1-31 */
     uint32_t hours;   /* range: 0-23 */
     uint32_t minutes; /* range: 0-59 */
