75440675cde959d660fa6cd2f86aba1ee4b21b0e
radhikaj <radhikaj@live.com>
2018-08-20 15:03:17 -0700

    Move documentation from doc to docs. Remove refman md files and add html files for API documentation
     #Fix check license and one typo

---
+++ docs/refman/html/search/classes_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['_5foe_5fbasic_5fxstate',['_oe_basic_xstate',['../struct__oe__basic__xstate.html',1,'']]],
+  ['_5foe_5fcontext',['_oe_context',['../struct__oe__context.html',1,'']]],
+  ['_5foe_5fexception_5frecord',['_oe_exception_record',['../struct__oe__exception__record.html',1,'']]],
+  ['_5foe_5fidentity',['_oe_identity',['../struct__oe__identity.html',1,'']]],
+  ['_5foe_5freport',['_oe_report',['../struct__oe__report.html',1,'']]]
+];
