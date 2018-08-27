d9a21e57cb1d31a1c210912964452f9e60e0267d
Mike Brasher <mikbras@microsoft.com>
2018-08-26 23:25:57 -0500

    Supress deprecations in libcxxrt and mbedtsl

--- 3rdparty/musl/epilogue.h
+++ 3rdparty/musl/epilogue.h
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-OE_LIBC_DEPRECATED char* strcpy(char* dest, const char* src);
+typedef unsigned long size_t;
 
-OE_LIBC_DEPRECATED char* strcat(char* dest, const char* src);
+OE_LIBC_DEPRECATED
+char* strcpy(char* dest, const char* src);
 
+OE_LIBC_DEPRECATED
+char* strcat(char* dest, const char* src);
