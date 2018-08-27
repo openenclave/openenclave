7817d0c1e10a4a14a185de2c018fa9ad2dfc1cbb
Mike Brasher <mikbras@microsoft.com>
2018-08-27 12:11:29 -0500

    implement oe_get_enclave()

--- include/openenclave/internal/calls.h
+++ include/openenclave/internal/calls.h
@@ -303,6 +303,7 @@ typedef struct _oe_realloc_args
 **
 **     Runtime state to initialize enclave state with, includes
 **     - First 8 leaves of CPUID for enclave emulation
+**     - Enclave handle obtained by oe_create_enclave()
 **
 **==============================================================================
 */
@@ -310,6 +311,7 @@ typedef struct _oe_realloc_args
 typedef struct _oe_init_enclave_args
 {
     uint32_t cpuidTable[OE_CPUID_LEAF_COUNT][OE_CPUID_REG_COUNT];
+    oe_enclave_t* enclave;
 } oe_init_enclave_args_t;
 
 /*
