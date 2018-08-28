5c3edab4ebf0b4ce6f1cd6aedfc779e0f5f7a759
Mike Brasher <mikbras@microsoft.com>
2018-08-28 18:21:36 -0500

    revert to libc functions.

--- libc/libctime.c
+++ libc/libctime.c
@@ -13,11 +13,12 @@
 #include <sys/time.h>
 #include <time.h>
 
-// This definition is replicated from "musl/src/time/__tz.c" as this file has
+// These definitions are replicated from "musl/src/time/__tz.c" as this file has
 // some dependencies on other functions which are not developed for the enclave
 // environment so we are defining the variable here to resolve the dependency
 // of extern variable in gmtime_r.c.
 const char __gmt[] = "GMT";
+const char __utc[] = "UTC";
 
 static const uint64_t _SEC_TO_MSEC = 1000UL;
 static const uint64_t _MSEC_TO_USEC = 1000UL;
@@ -108,4 +109,21 @@ int oe_nanosleep(const struct timespec* req, struct timespec* rem)
 done:
 
     return ret;
+}
+
+size_t strftime(char* s, size_t max, const char* format, const struct tm* tm)
+{
+    oe_assert("strftime(): panic" == NULL);
+    return 0;
+}
+
+size_t strftime_l(
+    char* s,
+    size_t max,
+    const char* format,
+    const struct tm* tm,
+    locale_t loc)
+{
+    oe_assert("strftime_l(): panic" == NULL);
+    return 0;
 }
