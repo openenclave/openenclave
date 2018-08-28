5683c4323ee7e7dc5c33bb1b7de63636c382e617
Mike Brasher <mikbras@microsoft.com>
2018-08-28 18:06:04 -0500

    reverse to original __vfprintf_chk.c.

---
+++ libc/__vfprintf_chk.c
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <stdio.h>
+
+// GCC sometimes replaces vfprintf() calls with __vfprintf_chk() calls. In
+// glibc this function sets the output stream's _IO_FLAGS2_FORTIFY flag, which
+// causes glibc to perform various checks on the output stream. Since MUSL has
+// no equivalent flag, this implementation simply calls vfprintf().
+int __vfprintf_chk(FILE* stream, const char* format, va_list ap)
+{
+    return vfprintf(stream, format, ap);
+}
