176a32be5b73ea84ce8dfa158a5e18e1c533f922
Mike Brasher <mikbras@microsoft.com>
2018-08-28 19:16:54 -0500

    Remove unused file.

--- 3rdparty/libcxxrt/stubs.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#ifndef __OE_LIBCXXRT_STUBS_H
-#define __OE_LIBCXXRT_STUBS_H
-
-#endif /* __OE_LIBCXXRT_STUBS_H */
