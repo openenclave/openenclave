1f51eb0bc281acef03c942d29db5fd35ba8e26ef
Mike Brasher <mikbras@microsoft.com>
2018-08-31 13:48:31 -0500

    add syscall() libc function

--- libc/syscalls.c
+++ libc/syscalls.c
@@ -21,6 +21,7 @@
 #include <time.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 static const uint64_t _SEC_TO_MSEC = 1000UL;
 static const uint64_t _MSEC_TO_USEC = 1000UL;
@@ -236,4 +237,21 @@ long __syscall(long n, long x1, long x2, long x3, long x4, long x5, long x6)
 long __syscall_cp(long n, long x1, long x2, long x3, long x4, long x5, long x6)
 {
     return __syscall(n, x1, x2, x3, x4, x5, x6);
+}
+
+long syscall(long number, ...)
+{
+    va_list ap;
+
+    va_start(ap, number);
+    long x1 = va_arg(ap, long);
+    long x2 = va_arg(ap, long);
+    long x3 = va_arg(ap, long);
+    long x4 = va_arg(ap, long);
+    long x5 = va_arg(ap, long);
+    long x6 = va_arg(ap, long);
+    long ret = __syscall(number, x1, x2, x3, x4, x5, x6);
+    va_end(ap);
+
+    return ret;
 }
