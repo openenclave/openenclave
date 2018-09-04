232a1dc33d8a56c42742d71e99b46461849aa5d4
Mike Brasher <mikbras@microsoft.com>
2018-09-04 17:18:43 -0500

    review fixes.

---
+++ 3rdparty/musl/patches/exp2l.s
@@ -0,0 +1,4 @@
+// Rename expm1l to __unused_expm1l (use the C version from expm1l.c instead).
+// The exp21 assembly function from __exp2l.s is still needed.
+#define expm1l __unused_expm1l
+include "__exp2l.s"
