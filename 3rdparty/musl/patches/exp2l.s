b0c6e7863a6bb69ec291673f56173db9bd596939
Mike Brasher <mikbras@microsoft.com>
2018-09-06 16:34:36 -0500

    Use expm1l() define in C file rather than asm file.

--- 3rdparty/musl/patches/exp2l.s
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-// Rename expm1l to __unused_expm1l (use the C version from expm1l.c instead).
-// The exp21 assembly function from __exp2l.s is still needed.
-#define expm1l __unused_expm1l
-include "__exp2l.s"
