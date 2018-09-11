e6203a7fedcef03499c9f73d785e76d72140a692
Anita Govindarajan <v-angovi@microsoft.com>
2018-09-10 20:10:09 -0500

    Initial revision of libcxxthrd tests

---
+++ tests/libcxxthrd/enc/enc.cpp
@@ -0,0 +1,71 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <openenclave/enclave.h>
+#include <openenclave/internal/calls.h>
+#include <openenclave/internal/enclavelibc.h>
+#include <openenclave/internal/tests.h>
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include "../host/args.h"
+#include "../host/ocalls.h"
+
+extern const char* __test__;
+
+extern "C" int main(int argc, const char* argv[]);
+
+extern "C" void _exit(int status)
+{
+    oe_call_host("ocall_exit", (void*)(long)status);
+    abort();
+}
+
+extern "C" void _Exit(int status)
+{
+    _exit(status);
+    abort();
+}
+
+extern "C" void exit(int status)
+{
+    _exit(status);
+    abort();
+}
+
+typedef void (*Handler)(int signal);
+
+Handler signal(int signal, Handler)
+{
+    /* Ignore! */
+    return NULL;
+}
+
+extern "C" int close(int fd)
+{
+    OE_TEST("close() panic" == NULL);
+    return 0;
+}
+
+OE_ECALL void Test(Args* args)
+{
+    extern const char* __TEST__NAME;
+    if (args)
+    {
+        printf("RUNNING: %s\n", __TEST__NAME);
+        static const char* argv[] = {
+            "test", NULL,
+        };
+        static int argc = sizeof(argv) / sizeof(argv[0]);
+        args->ret = main(argc, argv);
+        args->test = oe_host_strndup(__TEST__NAME, OE_SIZE_MAX);
+    }
+}
+
+OE_SET_ENCLAVE_SGX(
+    1,    /* ProductID */
+    1,    /* SecurityVersion */
+    true, /* AllowDebug */
+    8192, /* HeapPageCount */
+    1024, /* StackPageCount */
+    2);   /* TCSCount */
