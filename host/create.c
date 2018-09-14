a84d08227d03575dac1b32ae50c26463df743c1b
Anand Krishnamoorthi <anakrish@microsoft.com>
2018-09-14 14:11:04 -0500

    Keep openssl pinned in memory. Maybe oe_sgx_ functions load/unload openssl multiple times?

--- host/create.c
+++ host/create.c
@@ -31,6 +31,7 @@
 #include "enclave.h"
 #include "memalign.h"
 #include "sgxload.h"
+#include "sgxquoteprovider.h"
 
 static oe_once_type _enclave_init_once = OE_H_ONCE_INITIALIZER;
 
@@ -1406,6 +1407,9 @@ oe_result_t oe_create_enclave(
     oe_sgx_load_context_t context;
 
     _InitializeEnclaveHost();
+
+    // Experimental
+    oe_initialize_quote_provider();
 
     if (enclaveOut)
         *enclaveOut = NULL;
