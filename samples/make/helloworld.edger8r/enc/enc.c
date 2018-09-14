3e32188e3c20a74a360dfaf34d366d5416cb2ac5
Paul Allen <paul.c.allen@microsoft.com>
2018-09-14 17:45:16 -0500

    helloworld.edger8r makefile updates
    now generate edl proxies and headers within enc and host directly

--- samples/make/helloworld.edger8r/enc/enc.c
+++ samples/make/helloworld.edger8r/enc/enc.c
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <openenclave/enclave.h>
 #include <stdio.h>
 
 // Include the trusted helloworld header that is generated
