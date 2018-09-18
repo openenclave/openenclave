178d61eecd134b3e7ee0444dad2408d18c4f6825
Paul Allen <paul.c.allen@microsoft.com>
2018-09-17 19:47:33 -0500

    Ported enclaveparam to use edger8r

--- tests/enclaveparam/enc/enc.c
+++ tests/enclaveparam/enc/enc.c
@@ -1,18 +1,34 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <string.h>
 #include <openenclave/enclave.h>
 #include <openenclave/internal/enclavelibc.h>
 #include <openenclave/internal/hostalloc.h>
 #include <openenclave/internal/print.h>
 #include <openenclave/internal/tests.h>
+#include "enclaveparam_t.h"
 
-OE_ECALL void test_ocall_enclave_param(void* args)
+ void test_ocall_enclave_param(char* func)
 {
-    const char* func = (const char*)args;
+    oe_result_t result = OE_INVALID_PARAMETER;
+    oe_enclave_t* enclave = oe_get_enclave();
+
     OE_TEST(func != NULL);
 
-    oe_result_t result = oe_call_host(func, args);
+    if (strcmp(func, "callback_1") == 0)
+    {
+        result = callback_1(enclave);
+    }
+    else if (strcmp(func, "callback_2") == 0)
+    {
+        result = callback_2(enclave);
+    }
+    else if (strcmp(func, "callback_3") == 0)
+    {
+        result = callback_3(enclave);
+    }
+
     OE_TEST(result == OE_OK);
 }
 
