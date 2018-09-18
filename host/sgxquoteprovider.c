c9805a4b4ba443f3330709c02a0f2d91bbeb176d
Simon Leet <simon.leet@microsoft.com>
2018-09-18 17:54:43 -0500

    Update prereqs Makefiles
    * Update DCAP prereqs Makefile to use public Intel release
    
    * Add optional Makefile for installing Azquotprov package

--- host/sgxquoteprovider.c
+++ host/sgxquoteprovider.c
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// Uncomment this line to enable tracing.
 #include <dlfcn.h>
 #include <openenclave/internal/hexdump.h>
 #include <openenclave/internal/raise.h>
