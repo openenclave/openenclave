wintersteiger/openenclave-tmp

Fixed integer overflow bugs in malloc calls.

enclave/revocationinfo.c
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "../common/revocation.h"
+#include <openenclave/bits/safemath.h>
 #include <openenclave/enclave.h>
 #include <openenclave/internal/calls.h>
 #include <openenclave/internal/enclavelibc.h>
@@ -39,12 +40,12 @@ done:
 oe_result_t oe_get_revocation_info(oe_get_revocation_info_args_t* args)
 {
     oe_result_t result = OE_FAILURE;
-    uint32_t host_args_buffer_size = sizeof(*args);
+    size_t host_args_buffer_size = sizeof(*args);
     uint8_t* host_args_buffer = NULL;
     oe_get_revocation_info_args_t* host_args = NULL;
     oe_get_revocation_info_args_t tmp_args = {0};
     uint8_t* p = NULL;
-    uint32_t crlUrlSizes[2] = {0};
+    size_t crlUrlSizes[2] = {0};
 
     if (args == NULL || args->num_crl_urls != 2 || args->crl_urls[0] == NULL ||
         args->crl_urls[1] == NULL)
@@ -54,8 +55,16 @@ oe_result_t oe_get_revocation_info(oe_get_revocation_info_args_t* args)
     // arguments.
     for (uint32_t i = 0; i < args->num_crl_urls; ++i)
     {
-        crlUrlSizes[i] = oe_strlen(args->crl_urls[i]) + 1;
-        host_args_buffer_size += crlUrlSizes[i];
+        result =
+            oe_safe_add_sizet(oe_strlen(args->crl_urls[i]), 1, &crlUrlSizes[i]);
+        if (result != OE_OK)
+            goto done;
+
+        result = oe_safe_add_sizet(
+            host_args_buffer_size, crlUrlSizes[i], &host_args_buffer_size);
+
+        if (result != OE_OK)
+            goto done;
     }
 
     host_args_buffer = oe_host_malloc(host_args_buffer_size);

host/files.c
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <openenclave/bits/safemath.h>
 #include <openenclave/host.h>
 #include <openenclave/internal/files.h>
 #include <openenclave/internal/trace.h>
@@ -47,8 +48,12 @@ oe_result_t __oe_load_file(
         *size = st.st_size;
     }
 
+    /* Check for integer overflow */
+    size_t totalSize;
+    OE_TRY(oe_safe_add_sizet(*size, extraBytes, &totalSize));
+
     /* Allocate memory */
-    if (!(*data = malloc(*size + extraBytes)))
+    if (!(*data = malloc(totalSize)))
         OE_THROW(OE_OUT_OF_MEMORY);
 
     /* Open the file */

openenclave/openenclave

enclave/revocationinfo.c
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "../common/revocation.h"
+#include <openenclave/bits/safemath.h>
 #include <openenclave/enclave.h>
 #include <openenclave/internal/calls.h>
 #include <openenclave/internal/enclavelibc.h>
@@ -39,12 +40,12 @@ done:
 oe_result_t oe_get_revocation_info(oe_get_revocation_info_args_t* args)
 {
     oe_result_t result = OE_FAILURE;
-    uint32_t host_args_buffer_size = sizeof(*args);
+    size_t host_args_buffer_size = sizeof(*args);
     uint8_t* host_args_buffer = NULL;
     oe_get_revocation_info_args_t* host_args = NULL;
     oe_get_revocation_info_args_t tmp_args = {0};
     uint8_t* p = NULL;
-    uint32_t crl_url_sizes[2] = {0};
+    size_t crl_url_sizes[2] = {0};
 
     if (args == NULL || args->num_crl_urls != 2 || args->crl_urls[0] == NULL ||
         args->crl_urls[1] == NULL)
@@ -54,8 +55,16 @@ oe_result_t oe_get_revocation_info(oe_get_revocation_info_args_t* args)
     // arguments.
     for (uint32_t i = 0; i < args->num_crl_urls; ++i)
     {
-        crl_url_sizes[i] = oe_strlen(args->crl_urls[i]) + 1;
-        host_args_buffer_size += crl_url_sizes[i];
+        result =
+            oe_safe_add_sizet(oe_strlen(args->crl_urls[i]), 1, &crl_url_sizes[i]);
+        if (result != OE_OK)
+            goto done;
+
+        result = oe_safe_add_sizet(
+            host_args_buffer_size, crl_url_sizes[i], &host_args_buffer_size);
+
+        if (result != OE_OK)
+            goto done;
     }
 
     host_args_buffer = oe_host_malloc(host_args_buffer_size);

host/files.c
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <openenclave/bits/safemath.h>
 #include <openenclave/host.h>
 #include <openenclave/internal/files.h>
 #include <openenclave/internal/trace.h>
@@ -47,8 +48,12 @@ oe_result_t __oe_load_file(
         *size = st.st_size;
     }
 
+    /* Check for integer overflow */
+    size_t total_size;
+    OE_TRY(oe_safe_add_sizet(*size, extra_bytes, &total_size));
+
     /* Allocate memory */
-    if (!(*data = malloc(*size + extra_bytes)))
+    if (!(*data = malloc(total_size)))
         OE_THROW(OE_OUT_OF_MEMORY);
 
     /* Open the file */