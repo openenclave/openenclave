a26fd2968a2c8ae305810fc36979d84e322b6b63
Mike Brasher <mikbras@microsoft.com>
2018-09-19 17:02:05 -0500

    pkg-config rework.

--- samples/test-samples.sh
+++ samples/test-samples.sh
@@ -38,6 +38,10 @@ fi || printandexit
 
 TEST_MAKE_DIR="$INSTALL_DIR/share/openenclave/samples"
 
+# Set the PKG_CONFIG_PATH environment variable so that the samples will use
+# the OE pkg-config scripts located in the build directory.
+export PKG_CONFIG_PATH="$BIN_DIR/output/share/pkgconfig"
+
 # build and run the make samples
 # The only exception is to not run them in simulation mode on SGX1-FLC platforms
 if [ $OE_SIMULATION ] && [ $USE_LIBSGX ]; then
