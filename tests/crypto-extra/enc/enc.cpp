33e68c5448e68179539b726d752bc6724ab6b1d4
Anand Krishnamoorthi <anakrish@microsoft.com>
2018-09-20 18:38:18 -0500

    Extra tests for certificate chains and certificate revocation lists.

---
+++ tests/crypto-extra/enc/enc.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+#include <openenclave/enclave.h>
+#include "../common/args.h"
+#include "../common/tests.cpp"
+
+OE_ECALL void ecall_test_cert_chain_positive(void* args_)
+{
+    test_cert_chain_args_t* args = (test_cert_chain_args_t*)args_;
+    test_cert_chain_positive(
+        args->root, args->intermediate, args->leaf, args->leaf2);
+}
+
+OE_ECALL void ecall_test_cert_chain_negative(void* args_)
+{
+    test_cert_chain_args_t* args = (test_cert_chain_args_t*)args_;
+    test_cert_chain_negative(
+        args->root, args->intermediate, args->leaf, args->leaf2);
+}
+
+OE_ECALL void ecall_test_crls(void* args_)
+{
+    test_crl_args_t* args = (test_crl_args_t*)args_;
+    test_crls(
+        args->root,
+        args->intermediate,
+        args->leaf1,
+        args->leaf2,
+        args->root_crl1,
+        args->root_crl1_size,
+        args->root_crl2,
+        args->root_crl2_size,
+        args->intermediate_crl1,
+        args->intermediate_crl1_size,
+        args->intermediate_crl2,
+        args->intermediate_crl2_size);
+}
+
+OE_SET_ENCLAVE_SGX(
+    1,    /* ProductID */
+    1,    /* SecurityVersion */
+    true, /* AllowDebug */
+    1024, /* HeapPageCount */
+    1024, /* StackPageCount */
+    2);   /* TCSCount */
