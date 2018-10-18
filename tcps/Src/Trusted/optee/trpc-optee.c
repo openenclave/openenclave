fee4fc736853343b0fb2fc9d95593df15649638f
Dave Thaler <dthaler@microsoft.com>
2018-10-18 08:54:56 -0700

    Add workaround to fix tests (#7)
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- tcps/Src/Trusted/optee/trpc-optee.c
+++ tcps/Src/Trusted/optee/trpc-optee.c
@@ -232,7 +232,12 @@ int sgx_is_within_enclave(const void* addr, size_t size)
 
 int sgx_is_outside_enclave(const void* addr, size_t size)
 {
+#ifdef SIMULATE_TEE
+    /* Temporary workaround. */
+    return TRUE;
+#else
     return !sgx_is_within_enclave(addr, size);
+#endif
 }
 
 sgx_status_t
