07d17fedd429feef80bd73727782c0dac659a2d5
Anita Govindarajan <v-angovi@microsoft.com>
2018-10-24 23:00:39 +0000

    Removed libcxxthrd directory after merge to libcxx

--- tests/libcxx/enc/enc.cpp
+++ tests/libcxx/enc/enc.cpp
@@ -262,11 +262,11 @@ OE_ECALL void Test(Args* args)
 }
 
 OE_SET_ENCLAVE_SGX(
-    1,    /* ProductID */
-    1,    /* SecurityVersion */
-    true, /* AllowDebug */
+    1,     /* ProductID */
+    1,     /* SecurityVersion */
+    true,  /* AllowDebug */
     12288, /* HeapPageCount */
-    1024, /* StackPageCount */
-    8);   /* TCSCount */
+    1024,  /* StackPageCount */
+    8);    /* TCSCount */
 
 OE_DEFINE_EMPTY_ECALL_TABLE();
