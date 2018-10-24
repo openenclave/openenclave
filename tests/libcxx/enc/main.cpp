0f75e02b904262c4dcf92e338dac5dd5e64e6d67
Anita Govindarajan <v-angovi@microsoft.com>
2018-10-24 22:55:13 +0000

    Added useful comment in main.cpp indicating its purpose

--- tests/libcxx/enc/main.cpp
+++ tests/libcxx/enc/main.cpp
@@ -1,5 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// This file exists so that each test has its own copy of
+// `__TEST__NAME`, that way the enclave can print the name as it loads
+// and runs the test.
 const char* __TEST__NAME = __TEST__;
 #include __TEST__
