3d0a626a308aec0d7456cad835b722f88e976176
Hernan Gatta <gattahernan@gmail.com>
2018-10-31 15:37:56 -0700

    Fix Linux build.

--- include/openenclave/bits/timetypes.h
+++ include/openenclave/bits/timetypes.h
@@ -5,7 +5,7 @@
 
 typedef int64_t __time64_t;
 
-#ifndef _TIME_T_DEFINED_
+#if !defined(_TIME_T_DEFINED_) && !defined(__time_t_defined)
 # ifdef _USE_32BIT_TIME_T
 typedef int32_t __time32_t;
 typedef __time32_t time_t;
@@ -13,9 +13,10 @@ typedef __time32_t time_t;
 typedef __time64_t time_t;
 # endif
 # define _TIME_T_DEFINED_
+# define __time_t_defined
 #endif
 
-#if !defined(_WINSOCKAPI_) && !defined(__timeval_defined)
+#if !defined(_WINSOCKAPI_) && !(defined(__timeval_defined) || defined(_STRUCT_TIMEVAL))
 struct timeval {
     long tv_sec;
     long tv_usec;
