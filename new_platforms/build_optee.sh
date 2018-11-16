c172b2dae35c569ec697ab415b1151b03dbc5d50
Hernan Gatta <HernanGatta@users.noreply.github.com>
2018-11-15 22:53:11 -0800

    Add support for building against AARCH64 to build_optee.sh (#65)

--- new_platforms/build_optee.sh
+++ new_platforms/build_optee.sh
@@ -1,25 +1,72 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-echo Installing build essentials
+# -------------------------------------
+# Package Prerequisites
+# -------------------------------------
+
+echo Installing Prerequisites
 
 sudo apt-get update
-sudo apt-get -y install build-essential
+sudo apt-get -y install android-tools-adb android-tools-fastboot autoconf \
+    automake bc bison build-essential cscope curl device-tree-compiler \
+    doxygen flex ftp-upload gdisk iasl libattr1-dev libcap-dev libfdt-dev \
+    libftdi-dev libglib2.0-dev libhidapi-dev libncurses5-dev libpixman-1-dev \
+    libssl-dev libtool make mtools netcat python-crypto python-serial \
+    python-wand unzip uuid-dev xdg-utils xterm xz-utils zlib1g-dev \
+    gcc-arm-linux-gnueabi graphviz gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 
-echo Installing arm toolchain
+# -------------------------------------
+# Download ARM Toolchain
+# -------------------------------------
 
-GCCPREFIX=gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf
+echo Installing ARM Toolchain
 
-if [ ! -d $GCCPREFIX ]; then
-    if [ ! -f $GCCPREFIX.tar.xz ]; then
-        wget https://releases.linaro.org/components/toolchain/binaries/6.2-2016.11/arm-linux-gnueabihf/$GCCPREFIX.tar.xz || exit 1
-    fi
-    tar xvf $GCCPREFIX.tar.xz || exit 1
+GCC_32=gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf
+GCC_64=gcc-linaro-6.2.1-2016.11-x86_64_aarch64-linux-gnu
+
+if [ -z "$ARCH" ]; then
+    export ARCH=aarch32
 fi
 
-echo Installing prerequisites
+if [ "$ARCH" = "aarch32" ]; then
+    if [ ! -d $GCC_32 ]; then
+        if [ ! -f $GCC_32.tar.xz ]; then
+            wget https://releases.linaro.org/components/toolchain/binaries/6.2-2016.11/arm-linux-gnueabihf/$GCC_32.tar.xz || exit 1
+        fi
+        tar xvf $GCC_32.tar.xz || exit 1
+    fi
+elif [ "$ARCH" = "aarch64" ]; then
+    if [ ! -d $GCC_64 ]; then
+        if [ ! -f $GCC_64.tar.xz ]; then
+            wget https://releases.linaro.org/components/toolchain/binaries/6.2-2016.11/aarch64-linux-gnu/$GCC_64.tar.xz || exit 1
+        fi
+        tar xvf $GCC_64.tar.xz || exit 1
+    fi
+fi
 
-sudo apt-get -y install android-tools-adb android-tools-fastboot autoconf automake bc bison build-essential cscope curl device-tree-compiler doxygen flex ftp-upload gdisk iasl libattr1-dev libcap-dev libfdt-dev libftdi-dev libglib2.0-dev libhidapi-dev libncurses5-dev libpixman-1-dev libssl-dev libtool make mtools netcat python-crypto python-serial python-wand unzip uuid-dev xdg-utils xterm xz-utils zlib1g-dev gcc-arm-linux-gnueabi graphviz
+# -------------------------------------
+# Configure ARM Toolchain
+# -------------------------------------
+
+if [ "$ARCH" = "aarch32" ]; then
+    TA_DEV_KIT_BITS=32
+
+    TA_CROSS_COMPILE=/usr/bin/arm-linux-gnueabi-
+
+    export CROSS_COMPILE=$PWD/$GCC_32/bin/arm-linux-gnueabihf-
+elif [ "$ARCH" = "aarch64" ]; then
+    TA_DEV_KIT_BITS=64
+
+    TA_CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-
+    TA_CROSS_COMPILE_32=/usr/bin/arm-linux-gnueabi-
+
+    export CROSS_COMPILE=$PWD/$GCC_64/bin/aarch64-linux-gnu-
+fi
+
+# -------------------------------------
+# Configure Intel SGX SDK
+# -------------------------------------
 
 echo Checking for Intel SGX SDK
 
@@ -39,16 +86,16 @@ if [ -z "$edgepath" ]; then
 fi
 if [ -z "$edgepath" ]; then
     if [ ! -e ../3rdparty/SGXSDK ]; then
-       if [ -n "${IntelSGXSDKInstallerURI}" ]; then
-           wget ${IntelSGXSDKInstallerURI} || exit 1
-       fi
-       if [ -e SGXSDK.zip ]; then
-           unzip SGXSDK.zip -d ../3rdparty || exit 1
-       fi
+        if [ -n "${IntelSGXSDKInstallerURI}" ]; then
+            wget ${IntelSGXSDKInstallerURI} || exit 1
+        fi
+        if [ -e SGXSDK.zip ]; then
+            unzip SGXSDK.zip -d ../3rdparty || exit 1
+        fi
     fi
     if [ -e ../3rdparty/SGXSDK ]; then
         export PATH="$PATH:$PWD/../3rdparty/SGXSDK/bin/x64"
-	chmod 755 $PWD/../3rdparty/SGXSDK/bin/x64/sgx_edger8r
+        chmod 755 $PWD/../3rdparty/SGXSDK/bin/x64/sgx_edger8r
     fi
 fi
 edgepath=`which sgx_edger8r`
@@ -76,7 +123,10 @@ else
 fi
 sgxSdkPath=$(dirname "${binPath}")
 
-# Next, install oeedger8r
+# -------------------------------------
+# Configure OE Edger8r
+# -------------------------------------
+
 oeedgepath=`which oeedger8r`
 if [ ! -z "$oeedgepath" ]; then
     export OEEDGER8R=$oeedgepath
@@ -112,38 +162,63 @@ export SGX_RELATIVE_PATH=../3rdparty/SGXSDK/
 export SGX_PATH=$PWD${SGX_RELATIVE_PATH}
 export SGX_SDK=$PWD/../3rdparty/SGXSDK
 
-export CROSS_COMPILE=$PWD/$GCCPREFIX/bin/arm-linux-gnueabihf-
-export PROC_COUNT=$(eval "cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l")
+PROC_COUNT=$(eval "cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l")
 
-echo Building OP-TEE OS
+# -------------------------------------
+# Build OP-TEE
+# -------------------------------------
 
-pushd ../3rdparty/optee_os/
+if [ -z "$TA_DEV_KIT_DIR" ]; then
+    echo Building OP-TEE OS
 
-export ARCH=arm
+    pushd ../3rdparty/optee_os/
 
-# TODO: change imx-mx6qmbbedge to imx-mx6qgeneric below once we find a branch
-# that supports both it and PTA_CYREP_GET_CERT_CHAIN_SIZE
-make -j ${PROC_COUNT} PLATFORM=imx-mx6qhmbedge CFG_PSCI_ARM32=y CFG_RPMB_FS=y CFG_REE_FS=n \
-                      CFG_RPMB_TESTKEY=y CFG_RPMB_WRITE_KEY=n CFG_RPMB_RESET_FAT=n CFG_WITH_USER_TA=y \
-                      CFG_PAGED_USER_TA=n CFG_WITH_PAGER=n CFG_CRYPTO_SIZE_OPTIMIZATION=y \
-                      platform-cflags-optimization=-Os CFG_UNWIND=n CFG_TEE_CORE_DEBUG=y \
-                      CFG_BOOT_SECONDARY_REQUEST=y CFG_NS_ENTRY_ADDR=0x10820000 CFG_TEE_TA_LOG_LEVEL=4 \
-                      CFG_TEE_CORE_LOG_LEVEL=2 CFG_TZ_SPI_CONTROLLERS=0x2 CFG_TA_RPC=y \
-                      CFG_CONSOLE_UART=UART3_BASE || exit 1
+    OPTEE_FLAGS=platform-cflags-optimization=-Os \
+                CFG_CRYPTO_SIZE_OPTIMIZATION=y   \
+                CFG_PAGED_USER_TA=n              \
+                CFG_REE_FS=n                     \
+                CFG_RPMB_FS=y                    \
+                CFG_RPMB_TESTKEY=y               \
+                CFG_RPMB_WRITE_KEY=n             \
+                CFG_RPMB_RESET_FAT=n             \
+                CFG_TA_RPC=y                     \
+                CFG_TEE_CORE_DEBUG=y             \
+                CFG_TEE_CORE_LOG_LEVEL=2         \
+                CFG_TEE_TA_LOG_LEVEL=4           \
+                CFG_UNWIND=n                     \
+                CFG_WITH_PAGER=n                 \
+                CFG_WITH_USER_TA=y
 
-popd
+    if [ $ARCH = "aarch32" ]; then
+        ARCH=arm make -j $PROC_COUNT PLATFORM=vexpress-qemu_virt \
+                                    $OPTEE_FLAGS || exit 1
+    elif [ $ARCH = "aarch64" ]; then
+        ARCH=arm make -j $PROC_COUNT PLATFORM=vexpress-qemu_armv8a               \
+                                    $OPTEE_FLAGS                                \
+                                    CROSS_COMPILE=$CROSS_COMPILE                \
+                                    CROSS_COMPILE_core=$CROSS_COMPILE           \
+                                    CROSS_COMPILE_ta_arm64=$TA_CROSS_COMPILE    \
+                                    CROSS_COMPILE_ta_arm32=$TA_CROSS_COMPILE_32 \
+                                    CFG_ARM64_core=y || exit 1
+    fi
 
-echo Building TCPS-SDK
+    popd
+    
+    export TA_DEV_KIT_DIR=$PWD/../3rdparty/optee_os/out/arm-plat-vexpress/export-ta_arm$TA_DEV_KIT_BITS
+fi
 
-export TA_DEV_KIT_DIR=$PWD/../3rdparty/optee_os/out/arm-plat-imx/export-ta_arm32
-export ARCH=aarch32
+# -------------------------------------
+# Build TCPS SDK
+# -------------------------------------
+
+echo Building TCPS SDK
+
 
 make -j ${PROC_COUNT} || exit 1
 
-export CROSS_COMPILE=/usr/bin/arm-linux-gnueabi-
-export ARCH=arm
+export CROSS_COMPILE=$TA_CROSS_COMPILE
 
-make -j ${PROC_COUNT} -C samples/sockets/Trusted/optee -f linux_gcc.mak BUILD_TARGET=debug $* || exit 1
-make -j ${PROC_COUNT} -C samples/helloworld/HelloWorldEnc/optee -f linux_gcc.mak BUILD_TARGET=debug $* || exit 1
+ARCH=arm make -j ${PROC_COUNT} -C samples/sockets/Trusted/optee -f linux_gcc.mak BUILD_TARGET=debug $* || exit 1
+ARCH=arm make -j ${PROC_COUNT} -C samples/helloworld/HelloWorldEnc/optee -f linux_gcc.mak BUILD_TARGET=debug $* || exit 1
 
 doxygen Doxyfile || exit 1
