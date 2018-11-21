03069b187c3ab6ef614db352d1baad99f28e968e
Anita Govindarajan <v-angovi@microsoft.com>
2018-11-20 18:25:26 -0800

    Renamed one more _oe function

--- enclave/core/asmdefs.h
+++ enclave/core/asmdefs.h
@@ -56,7 +56,7 @@ void oe_exception_dispatcher(void* context);
 #endif
 
 #ifndef __ASSEMBLER__
-void _oe_notify_nested_exit_start(
+void oe_notify_nested_exit_start(
     uint64_t arg1,
     oe_ocall_context_t* ocall_context);
 #endif
