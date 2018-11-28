8ca73a48313b5ac72e967b762dd8950a1104066d
Hernan Gatta <hegatta@microsoft.com>
2018-11-28 03:10:39 -0800

    Install prereqs plus cleanup.

--- new_platforms/run_tests_aarch64.sh
+++ new_platforms/run_tests_aarch64.sh
@@ -1,13 +1,30 @@
 export ARCH=aarch64
 export MACHINE=virt
 
+echo [CI] Building Open Enclave
+
 chmod +x ./new_platforms/build_optee.sh
 ./new_platforms/build_optee.sh || exit 1
 
-wget https://tcpsbuild.blob.core.windows.net/tcsp-build/OE-CI-Ubuntu-16.04-AARCH64.tar.xz || exit 1
-tar xvf OE-CI-Ubuntu-16.04-AARCH64.tar.xz || exit 1
+echo [CI] Installing Prerequistes
 
-cd ci
+apt install sshpass -y
+
+echo [CI] Downloading Emulated Environment
+
+CI_DIR=ci
+CI_ENV=OE-CI-Ubuntu-16.04-AARCH64
+
+if [ ! -f $CI_DIR ]; then
+    if [ ! -f $CI_ENV.tar.xz ]; then
+        wget https://tcpsbuild.blob.core.windows.net/tcsp-build/$CI_ENV.tar.xz || exit 1
+    fi
+    tar xvf $CI_ENV.tar.xz --no-same-owner || exit 1
+fi
+
+echo [CI] Launching QEMU
+
+cd $CI_DIR
 nohup ./qemu-system-aarch64 \
         -nographic \
         -serial file:ree.log -serial file:tee.log \
@@ -24,9 +41,13 @@ nohup ./qemu-system-aarch64 \
         -netdev user,id=net0,hostfwd=tcp::5555-:22 -device virtio-net,netdev=net0 \
         -virtfs local,id=sh0,path=$PWD/..,security_model=passthrough,readonly,mount_tag=sh0 &
 disown
+
+echo [CI] Connecting to QEMU Guest
         
 mkdir $HOME/.ssh
 ssh-keygen -f $HOME/.ssh/known_hosts -R "[localhost]:5555"
 ssh-keyscan -p 5555 localhost >> $HOME/.ssh/known_hosts
+
+echo [CI] Running Test Suite in QEMU Guest
 
 sshpass -p test ssh test@localhost -p 5555 -vvv "su -c \"mkdir /mnt/oe && mount -t 9p -o trans=virtio sh0 /mnt/oe -oversion=9p2000.L && cp /mnt/oe/new_platforms/bin/optee/tests/3156152a-19d1-423c-96ea-5adf5675798f.ta /lib/optee_armtz && /mnt/oe/new_platforms/tests/oetests_host/oetests_host \""
