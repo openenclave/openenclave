76a403c9da0095967ff91977fbf05b7daa5df120
Hernan Gatta <hegatta@microsoft.com>
2018-12-12 05:06:13 -0800

    Aesthetic changes to libsocket.

--- new_platforms/libsocket/enc/optee/sub.mk
+++ new_platforms/libsocket/enc/optee/sub.mk
@@ -1,7 +1,10 @@
 ../socket_insecure_enc.c: $(O)/socket_t.h
 
 CFLAGS += $(EXTRA_CFLAGS)
+
 CFLAGS += -I$(O) -I$(OE_INC) -I$(NP_INC) 
+
+CFLAGS += -DOE_USE_OPTEE
 CFLAGS += -D_INC_STRING
 
 srcs-y += ../socket_insecure_enc.c
