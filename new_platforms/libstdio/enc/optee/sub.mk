022377da1c7e4d37fc5001dc2b3bb303e233a935
Hernan Gatta <hegatta@microsoft.com>
2018-12-12 05:05:50 -0800

    Add stdio to CMake.

--- new_platforms/libstdio/enc/optee/sub.mk
+++ new_platforms/libstdio/enc/optee/sub.mk
@@ -1,24 +1,12 @@
-include ../../../oe_sub.mk
-
-ROOT_RELATIVE_PATH = ../../../
-
-../stdio_t.h: $(OE_SDK_ROOT_PATH)include/openenclave/stdio.edl
-	$(OEEDGER8R) --trusted --search-path "../..$(OEPATHSEP)$(OE_SDK_ROOT_PATH)include" --trusted-dir ".."  $(OE_SDK_ROOT_PATH)include/openenclave/stdio.edl
-
 ../stdio_enc.c: ../stdio_t.h
-
 files_optee.c: ../stdio_t.h
+
+CFLAGS += $(EXTRA_CFLAGS)
+
+CFLAGS += -I$(O) -I$(OE_INC) -I$(NP_INC) 
 
 CFLAGS += -DOE_USE_OPTEE
 
-global-incdirs-y += $(ROOT_RELATIVE_PATH)include/optee/enclave
-global-incdirs-y += $(ROOT_RELATIVE_PATH)include/optee
-global-incdirs-y += $(ROOT_RELATIVE_PATH)include
-global-incdirs-y += $(OpteeDir)lib/libutee/include
-global-incdirs-y += $(RIoTDir)CyReP/cyrep
-global-incdirs-y += $(RIoTDir)External/tinycbor/src
-global-incdirs-y += $(ROOT_RELATIVE_PATH)../include
-global-incdirs-y += ..
-
 srcs-y += ../stdio_enc.c
 srcs-y += files_optee.c
+srcs-y += $(GEN)
