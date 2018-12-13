2943e0f72e7c4a32a4a37a4db1474ac92ead06e6
Akash Gupta <akagup@microsoft.com>
2018-12-13 15:01:21 -0800

    Fixed bors build break.

--- tests/crypto/utils.c
+++ tests/crypto/utils.c
@@ -31,7 +31,8 @@ void hex_to_buf(const char* str, uint8_t* buf, size_t bufsize)
 
     for (size_t i = 0; i < strsz; i += 2)
     {
-        unsigned char v = 16 * to_num(str[i]) + to_num(str[i + 1]);
+        unsigned char v =
+            (unsigned char)(16 * to_num(str[i]) + to_num(str[i + 1]));
         buf[i / 2] = v;
     }
 }
