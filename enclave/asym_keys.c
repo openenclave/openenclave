803689f71960cfc5fb5f120475cb13c05726ee9b
Akash Gupta <akagup@microsoft.com>
2018-12-13 16:26:22 -0800

    Fixed build break due to extra warnings.

--- enclave/asym_keys.c
+++ enclave/asym_keys.c
@@ -547,7 +547,7 @@ void oe_free_key(
 
     if (key_info)
     {
-        oe_secure_zero_fill(key_info, key_buffer_size);
+        oe_secure_zero_fill(key_info, key_info_size);
         oe_free(key_info);
     }
 }
