c0ba62ce6c409de627df8d1e9463851b666ff00b
Anita Govindarajan <v-angovi@microsoft.com>
2018-12-18 02:35:05 +0000

    Removed sealKey from Windows run and restored enclave sizes

--- tests/sealKey/enc/enc.cpp
+++ tests/sealKey/enc/enc.cpp
@@ -481,6 +481,6 @@ OE_SET_ENCLAVE_SGX(
     1,    /* ProductID */
     1,    /* SecurityVersion */
     true, /* AllowDebug */
-    512,  /* HeapPageCount */
-    32,   /* StackPageCount */
+    1024, /* HeapPageCount */
+    1024, /* StackPageCount */
     5);   /* TCSCount */
