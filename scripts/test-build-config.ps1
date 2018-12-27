776bb6c9f1afc51473243fc89c805cdec047d7c7
John Kordich <johnkord@microsoft.com>
2018-12-27 14:36:12 -0800

    Added -DBUILD_ENCLAVES=1 flag to Windows CI builds.

--- scripts/test-build-config.ps1
+++ scripts/test-build-config.ps1
@@ -21,6 +21,7 @@ Param
     # Valid BUILDTYPE values are Debug|Release
     [ValidateSet("Debug", "Release", IgnoreCase = $false)]
     [String]$BUILD_TYPE = "DEBUG",
+    [Switch]$BUILD_ENCLAVES,
     [Parameter(Mandatory = $true)][String]$LINUX_BIN_DIR
 )
 
@@ -31,6 +32,7 @@ if ($h -or $help) {
      echo "        -help to Display usage and exit"
      echo "        -add_windows_enclave_tests to add tests for windows enclave"
      echo "        -build_type Debug|Release"
+     echo "        -build_enclaves 1"
      echo "        -linux_bin_dir [directory] directory for linux binaries"
      echo " Default is to build for SGX1 platform, Debug Build type & test in"
      echo " simulator mode"
@@ -40,7 +42,8 @@ if ($h -or $help) {
 
 $ErrorActionPreference = "Stop"
 
-$VS_PATH = Join-Path ${env:ProgramFiles(x86)} "Microsoft Visual Studio\2017\BuildTools\Common7\Tools\LaunchDevCmd.bat"
+$VS_PATH = Join-Path ${env:ProgramFiles(x86)} "Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+$VCVARSPATH=(Join-Path (Get-Item -Path ".\").FullName "\vcvars.txt")
 
 echo "linux_bin_dir: $LINUX_BIN_DIR"
 
@@ -55,10 +58,37 @@ if (Test-Path "./build" -PathType Container) {
 mkdir build
 cd build
 
+# Invoke vcvars64.bat and write its entire environment to a text file.
+cmd.exe /c "call `"$VS_PATH`" && set > $VCVARSPATH"
+
+# Now 'source' this text file into powershell's environment
+Get-Content -Path "$VCVARSPATH" | Foreach-Object {
+  if ($_ -match "^(.*?)=(.*)$") {
+    Set-Content "env:\$($matches[1])" $matches[2]
+  }
+}
+
+# Add clang binaries to PATH
+$env:PATH += ";C:\Program Files\LLVM\bin"
+
+$BUILD_GENERATOR="Visual Studio 15 2017 Win64"
+$LINUX_BIN_FLAG="-DLINUX_BIN_DIR=`"$LINUX_BIN_DIR`""
+$BUILD_ENCLAVES_FLAG=""
+
+if ($BUILD_ENCLAVES) {
+    $BUILD_GENERATOR="NMake Makefiles"
+    $BUILD_ENCLAVES_FLAG="-DBUILD_ENCLAVES=1"
+    $LINUX_BIN_FLAG=""
+
+    # Currently disable Windows Enclave Tests for BUILD_ENCLAVE builds.
+    # This will be enabled in a later PR.
+    Remove-Variable ADD_WINDOWS_ENCLAVE_TESTS
+}
+
 if ($ADD_WINDOWS_ENCLAVE_TESTS) {
-    & cmake.exe -G "Visual Studio 15 2017 Win64" -DLINUX_BIN_DIR="$LINUX_BIN_DIR" -DADD_WINDOWS_ENCLAVE_TESTS=1 ..
+    & cmake.exe -G $BUILD_GENERATOR $LINUX_BIN_FLAG -DADD_WINDOWS_ENCLAVE_TESTS=1 $BUILD_ENCLAVES_FLAG ..
 } else {
-    & cmake.exe -G "Visual Studio 15 2017 Win64" -DLINUX_BIN_DIR="$LINUX_BIN_DIR" ..
+    & cmake.exe -G $BUILD_GENERATOR $LINUX_BIN_FLAG $BUILD_ENCLAVES_FLAG ..
 }
 if ($LASTEXITCODE) {
     echo ""
@@ -67,7 +97,6 @@ if ($LASTEXITCODE) {
     exit 1
 }
 
-& $VS_PATH
 if ($LASTEXITCODE) {
     echo ""
     echo "Visual Studio failed"
