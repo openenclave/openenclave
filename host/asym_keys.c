f78a5f3b39a0dd0a1440363205f6b0486e368c87
Akash Gupta <akagup@microsoft.com>
2018-12-31 18:07:19 -0800

    Fixed license on file.

--- host/asym_keys.c
+++ host/asym_keys.c
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License
+// Licensed under the MIT License.
 
 #include <openenclave/bits/result.h>
 #include <openenclave/host.h>
