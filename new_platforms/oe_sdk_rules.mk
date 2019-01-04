8ba0bc97b7b3dbf068263a87eea94cf30ca4aafa
Hernan Gatta <hegatta@microsoft.com>
2019-01-04 15:21:23 -0800

    Remove Makefiles and Visual Studio files.

--- new_platforms/oe_sdk_rules.mk
+++
@@ -1,9 +0,0 @@
-export NEW_PLATFORMS_OPTEE_BIN_PATH = $(NEW_PLATFORMS_PATH)/bin/optee
-export OE_TRUSTED_LIB_NAME          = oeenclave
-export OE_TRUSTED_OUTPUT_PATH       = $(NEW_PLATFORMS_OPTEE_BIN_PATH)/new_platforms
-export OE_TRUSTED_LIB_PATH          = $(OE_TRUSTED_OUTPUT_PATH)/lib$(OE_TRUSTED_LIB_NAME).a
-export CYREP_CYREP_PATH             = $(OE_SDK_ROOT_PATH)/3rdparty/RIoT/CyReP/cyrep
-
-# Workaround warning treated as error for generated file
-# *_t.h:53:1: error: function declaration isn't a prototype [-Werror=strict-prototypes]
-WARNS=0
