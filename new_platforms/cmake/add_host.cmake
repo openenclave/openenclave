c8e08f87c2f8ecd120b3eab403020a11b32e282d
Hernan Gatta <hegatta@microsoft.com>
2019-01-16 01:34:38 -0800

    Factor host configuration routines.

---
+++ new_platforms/cmake/add_host.cmake
@@ -0,0 +1,15 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+function(add_host TARGET SOURCES)
+    add_executable(${TARGET} ${SRCS})
+
+    target_include_directories(${TARGET} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
+    target_link_libraries(${TARGET} oehost oestdio_host oesocket_host)
+
+    if(UNIX)
+        target_link_libraries(${TARGET} teec)
+    elseif(WIN32 AND TZ AND SIM)
+        target_link_libraries(${TARGET} oehost_opteesim)
+    endif()
+endfunction()
