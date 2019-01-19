e47c534e7bfd26429ce7d573db1419a98389c9ae
Dave Thaler <dthaler@microsoft.com>
2019-01-18 17:31:58 -0800

    Add VS solution file
    To build, this requires that openenclave.0.2.0-ci-20190117-032919.nupkg
    be previously built and placed in the following directory:
    openenclave\new_platforms\vsextension\ProjectWizard\Packages
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ new_platforms/vsextension/ProjectWizard/host.c
@@ -0,0 +1,76 @@
+#include <openenclave/host.h>
+#include "$projectname$_u.h"
+
+oe_result_t create_$safeprojectname$_enclave(const char* enclave_name, oe_enclave_t** out_enclave)
+{
+    oe_enclave_t* enclave = NULL;
+    uint32_t enclave_flags = 0;
+    oe_result_t result;
+
+    *out_enclave = NULL;
+
+    // Create the enclave
+#ifdef _DEBUG
+    enclave_flags |= OE_ENCLAVE_FLAG_DEBUG;
+#endif
+    result = oe_create_$safeprojectname$_enclave(
+        enclave_name,
+        OE_ENCLAVE_TYPE_UNDEFINED,
+        enclave_flags,
+        NULL,
+        0,
+        &enclave);
+    if (result != OE_OK)
+    {
+        return result;
+    }
+
+    return OE_OK;
+}
+
+void sample_enclave_call(void)
+{
+    oe_enclave_t* enclave = NULL;
+    oe_result_t result = create_$safeprojectname$_enclave(
+        "$projectname$.signed.dll",
+        &enclave);
+    if (result != OE_OK)
+    {
+        fprintf(
+            stderr,
+            "oe_create_enclave(): result=%u (%s)\n",
+            result,
+            oe_result_str(result));
+        goto exit;
+    }
+
+    /* Make calls into the enclave... */
+    /*
+        result = ecall_DoWorkInEnclave(enclave);
+        if (result != OE_OK)
+        {
+            fprintf(
+                stderr,
+                "calling into ecall_DoWorkInEnclave failed: result=%u (%s)\n",
+                result,
+                oe_result_str(result));
+            goto exit;
+        }
+    */
+
+exit:
+    /* Clean up the enclave if we created one. */
+    if (enclave != NULL)
+    {
+        oe_terminate_enclave(enclave);
+    }
+}
+
+/* Add implementations of any OCALLs here. */
+/*
+void ocall_DoWorkInHost(void)
+{
+    ... Implement your OCALL here. ...
+}
+
+*/
