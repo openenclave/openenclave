d46b3a1f23112918c0994244c3f44ac021562a55
Hernan Gatta <HernanGatta@users.noreply.github.com>
2019-01-23 16:41:46 -0800

    Updates to NuGet Package (#1367)
    * Recognize 'Simulation' as a valid configuration and add rcprt4.lib as dependencies for OP-TEE.
    
    * Add libc++ to SGX include paths.
    
    * Add CyReS headers and TA Dev Kit include path.
    
    * Support RelWithDebInfo configuration.
    
    * Add release support to targets file and fix PS script for NuGet package.

--- new_platforms/scripts/Prepare-ForNugetPack.ps1
+++ new_platforms/scripts/Prepare-ForNugetPack.ps1
@@ -1,17 +1,25 @@
 # Data
 $Leaves = @(
-    "nuget\lib\native\v141\sgx\hw\x86\Debug",
-    "nuget\lib\native\v141\sgx\hw\x64\Debug",
+    "nuget\lib\native\v141\sgx\hw\x86\Debug",     # 00
+    "nuget\lib\native\v141\sgx\hw\x86\Release",   # 01
 
-    "nuget\lib\native\v141\sgx\sim\x86\Debug",
-    "nuget\lib\native\v141\sgx\sim\x64\Debug",
+    "nuget\lib\native\v141\sgx\hw\x64\Debug",     # 02
+    "nuget\lib\native\v141\sgx\hw\x64\Release",   # 03
 
-    "nuget\lib\native\v141\tz\hw\arm\Debug",
-    "nuget\lib\native\v141\tz\sim\x86\Debug",
+    "nuget\lib\native\v141\sgx\sim\x86\Debug",    # 04
+    "nuget\lib\native\v141\sgx\sim\x86\Release",  # 05
 
-    "nuget\tools",
+    "nuget\lib\native\v141\sgx\sim\x64\Debug",    # 06
+    "nuget\lib\native\v141\sgx\sim\x64\Release",  # 07
 
-    "nuget\build\native\include"
+    "nuget\lib\native\v141\tz\hw\arm\Debug",      # 08
+    "nuget\lib\native\v141\tz\hw\arm\Release",    # 09
+
+    "nuget\lib\native\v141\tz\sim\x86\Debug",     # 10
+
+    "nuget\tools",                                # 11
+
+    "nuget\build\native\include"                  # 12
 )
 
 $EnclaveLibraries = @(
@@ -31,19 +39,16 @@ $OPTEESimLibraries = @(
     "oehost_opteesim"
 )
 
-$Extensions = @(
-    "lib",
-    "pdb"
-)
-
 # Helper Functions
 Function Copy-LibsWorker($Libraries, $SourceLeafPath, $DestinationLeafPath)
 {
     ForEach ($Library in $Libraries) {
-        ForEach ($Extension in $Extensions) {
-            $SourceFilePath = Join-Path $SourceLeafPath "$Library.$Extension"
-            Copy-Item -Path $SourceFilePath -Destination $DestinationLeafPath
-        }
+        $SourceFilePath = Join-Path $SourceLeafPath "$Library.lib"
+        Copy-Item -Path $SourceFilePath -Destination $DestinationLeafPath
+
+        # TODO: CMake fails to place PDB files next to static libraries for RelWithDebInfo.
+        $SourceFilePath = Join-Path $SourceLeafPath "$Library.pdb"
+        Copy-Item -Path $SourceFilePath -Destination $DestinationLeafPath -ErrorAction SilentlyContinue
     }
 }
 
@@ -78,27 +83,36 @@ ForEach ($Leaf in $Leaves) {
 # Fetch the relevant build artifacts from the build output.
 
 # SGX Hardware
-Copy-Libs build\x86\sgx\out\lib\Debug    $Leaves[0] -WithEnclaveLibraries -WithHostLibraries
-Copy-Libs build\x64\sgx\out\lib\Debug    $Leaves[1] -WithEnclaveLibraries -WithHostLibraries
+Copy-Libs build\x86\sgx\out\lib\Debug             $Leaves[00] -WithEnclaveLibraries -WithHostLibraries
+Copy-Libs build\x86\sgx\out\lib\RelWithDebInfo    $Leaves[01] -WithEnclaveLibraries -WithHostLibraries
+
+Copy-Libs build\x64\sgx\out\lib\Debug             $Leaves[02] -WithEnclaveLibraries -WithHostLibraries
+Copy-Libs build\x64\sgx\out\lib\RelWithDebInfo    $Leaves[03] -WithEnclaveLibraries -WithHostLibraries
 
 # SGX Simulation
-Copy-Libs build\x86\sgxsim\out\lib\Debug $Leaves[2] -WithEnclaveLibraries -WithHostLibraries
-Copy-Libs build\x64\sgxsim\out\lib\Debug $Leaves[3] -WithEnclaveLibraries -WithHostLibraries
+Copy-Libs build\x86\sgxsim\out\lib\Debug          $Leaves[04] -WithEnclaveLibraries -WithHostLibraries
+Copy-Libs build\x86\sgxsim\out\lib\RelWithDebInfo $Leaves[05] -WithEnclaveLibraries -WithHostLibraries
+
+Copy-Libs build\x64\sgxsim\out\lib\Debug          $Leaves[06] -WithEnclaveLibraries -WithHostLibraries
+Copy-Libs build\x64\sgxsim\out\lib\RelWithDebInfo $Leaves[07] -WithEnclaveLibraries -WithHostLibraries
 
 # TrustZone Hardware
-Copy-Libs build\arm\tz\out\lib\Debug     $Leaves[4] -WithHostLibraries
+Copy-Libs build\arm\tz\out\lib\Debug              $Leaves[08] -WithHostLibraries
+Copy-Libs build\arm\tz\out\lib\RelWithDebInfo     $Leaves[09] -WithHostLibraries
 
 # TrustZone Simulation
-Copy-Libs build\x86\tzsim\out\lib\Debug  $Leaves[5] -WithEnclaveLibraries -WithHostLibraries -WithOPTEESimLibraries
+Copy-Libs build\x86\tzsim\out\lib\Debug           $Leaves[10] -WithEnclaveLibraries -WithHostLibraries -WithOPTEESimLibraries
 
 # oeedger8r Tool
-Copy-Item build\oeedger8r.exe            $Leaves[6]
+Copy-Item build\oeedger8r.exe                     $Leaves[11]
 
 # Copy the headers from the source tree.
 
 # Open Enclave
-Copy-Item -Recurse -Path $ENV:SOURCES_PATH\include\openenclave -Destination $Leaves[7]
+Copy-Item -Recurse -Path $ENV:SOURCES_PATH\include\openenclave       -Destination $Leaves[12]
 
 # New Platforms
-Copy-Item -Recurse -Path $ENV:SOURCES_PATH\new_platforms\include -Destination $Leaves[7]
-Rename-Item -Path "$($Leaves[7])\include" -NewName new_platforms
+Copy-Item -Recurse -Path $ENV:SOURCES_PATH\new_platforms\include     -Destination $Leaves[12]
+Copy-Item -Recurse -Path $ENV:SOURCES_PATH\3rdparty\RIoT\CyReP\cyrep -Destination $Leaves[12]
+
+Rename-Item -Path "$($Leaves[12])\include" -NewName new_platforms
