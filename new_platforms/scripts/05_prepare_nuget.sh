51fad259caf742716ef054be651612d392c952e4
Hernan Gatta <hegatta@microsoft.com>
2019-01-25 08:02:31 -0800

    Only include necessary libs for Linux.

--- new_platforms/scripts/05_prepare_nuget.sh
+++ new_platforms/scripts/05_prepare_nuget.sh
@@ -5,11 +5,40 @@
 
 TARGET=nuget/lib/native/gcc6/optee/v3.3.0
 
+declare -a LIBRARIES=(
+    "liboeenclave"
+    "liboehost"
+
+    "liboestdio_enc"
+    "liboestdio_host"
+
+    "liboesocket_enc"
+    "liboesocket_host"
+
+    "libmbedcrypto"
+    "libmbedx509"
+)
+
+declare -a PLATFORMS=(
+    "vexpress-qemu_virt"
+    "vexpress-qemu_armv8a"
+    "ls-ls1012grapeboard"
+)
+
 mkdir -p $TARGET || exit 1
 mkdir -p nuget/tools || exit 1
 
-cp -R build/vexpress-qemu_virt/out/lib $TARGET/vexpress-qemu_virt || exit 1
-cp -R build/vexpress-qemu_armv8a/out/lib $TARGET/vexpress-qemu_armv8a || exit 1
-cp -R build/ls-ls1012grapeboard/out/lib $TARGET/ls-ls1012grapeboard || exit 1
+for PLATFORM in "${PLATFORMS[@]}"
+do
+    mkdir -p $TARGET/$PLATFORM || exit 1
+done
+
+for LIBRARY in "${LIBRARIES[@]}"
+do
+    for PLATFORM in "${PLATFORMS[@]}"
+    do
+        cp -R build/$PLATFORM/out/lib/$LIBRARY.a $TARGET/$PLATFORM || exit 1
+    done
+done
 
 cp build/oeedger8r nuget/tools || exit 1
