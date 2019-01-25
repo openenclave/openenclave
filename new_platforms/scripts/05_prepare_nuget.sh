69768e0f46ccc44cecff399e430875aa73a0c19c
Hernan Gatta <hegatta@microsoft.com>
2019-01-25 05:52:29 -0800

    Add lib directory.

--- new_platforms/scripts/05_prepare_nuget.sh
+++ new_platforms/scripts/05_prepare_nuget.sh
@@ -3,11 +3,11 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-mkdir -p nuget/native/gcc6/optee/v3.0.0
+mkdir -p nuget/lib/native/gcc6/optee/v3.0.0
 mkdir -p nuget/tools
 
-cp -R build/vexpress-qemu_virt/out/lib nuget/native/gcc6/optee/v3.0.0/vexpress-qemu_virt
-cp -R build/vexpress-qemu_armv8a/out/lib nuget/native/gcc6/optee/v3.0.0/vexpress-qemu_armv8a
-cp -R build/ls-ls1012grapeboard/out/lib nuget/native/gcc6/optee/v3.0.0/ls-ls1012grapeboard
+cp -R build/vexpress-qemu_virt/out/lib nuget/lib/native/gcc6/optee/v3.0.0/vexpress-qemu_virt
+cp -R build/vexpress-qemu_armv8a/out/lib nuget/lib/native/gcc6/optee/v3.0.0/vexpress-qemu_armv8a
+cp -R build/ls-ls1012grapeboard/out/lib nuget/lib/native/gcc6/optee/v3.0.0/ls-ls1012grapeboard
 
 cp build/oeedger8r nuget/tools
