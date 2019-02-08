e25e6ca400b9b4686df3c3f1aa18661fcbfd6632
Simon Leet <simon.leet@microsoft.com>
2019-02-08 08:38:48 -0800

    Fixup time implementations in enclave core

--- enclave/core/__secs_to_tm.c
+++ enclave/core/__secs_to_tm.c
@@ -2,13 +2,5 @@
 // Licensed under the MIT License.
 
 /* Use OE STDC time.h & limits.h defs for MUSL __secs_to_tm.c */
-#if !defined(OE_NEED_STDC_NAMES)
 #define OE_NEED_STDC_NAMES
-#define __UNDEF_OE_NEED_STDC_NAMES
-#endif
 #include "../../3rdparty/musl/musl/src/time/__secs_to_tm.c"
-#include <openenclave/corelibc/time.h>
-#if defined(__UNDEF_OE_NEED_STDC_NAMES)
-#undef OE_NEED_STDC_NAMES
-#undef __UNDEF_OE_NEED_STDC_NAMES
-#endif
--- enclave/core/gmtime.c
+++ enclave/core/gmtime.c
@@ -2,19 +2,9 @@
 // Licensed under the MIT License.
 
 #include <openenclave/bits/types.h>
-
-/* Use OE STDC time.h definition for struct tm */
-#if !defined(OE_NEED_STDC_NAMES)
-#define OE_NEED_STDC_NAMES
-#define __UNDEF_OE_NEED_STDC_NAMES
-#endif
 #include <openenclave/corelibc/time.h>
-#if defined(__UNDEF_OE_NEED_STDC_NAMES)
-#undef OE_NEED_STDC_NAMES
-#undef __UNDEF_OE_NEED_STDC_NAMES
-#endif
 
-int __secs_to_tm(long long, struct tm*);
+int __secs_to_tm(long long, struct oe_tm*);
 
 struct oe_tm* oe_gmtime(const time_t* timep)
 {
@@ -24,7 +14,7 @@ struct oe_tm* oe_gmtime(const time_t* timep)
 
 struct oe_tm* oe_gmtime_r(const time_t* timep, struct oe_tm* result)
 {
-    if (!timep || !result || __secs_to_tm(*timep, (struct tm*)result) != 0)
+    if (!timep || !result || __secs_to_tm(*timep, (struct oe_tm*)result) != 0)
         return NULL;
 
     result->tm_isdst = 0;
