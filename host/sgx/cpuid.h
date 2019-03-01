aa4a5fa5bc60648b5ea975a95711f1d2207d185b
Dmitry Shmulevich <dmitsh@microsoft.com>
2019-03-01 13:25:56 -0800

    addressed comments

--- host/sgx/cpuid.h
+++ host/sgx/cpuid.h
@@ -6,23 +6,13 @@
 
 #if defined(__GNUC__)
 #include <cpuid.h>
-/* Same as __get_cpuid, but sub-leaf can be specified.
-   Need this function as cpuid level 4 needs the sub-leaf to be specified in ECX
-*/
-static inline void oe_get_cpuid(
-    unsigned int __leaf,
-    unsigned int __subleaf,
-    unsigned int* __eax,
-    unsigned int* __ebx,
-    unsigned int* __ecx,
-    unsigned int* __edx)
-{
-    __cpuid_count(__leaf, __subleaf, *__eax, *__ebx, *__ecx, *__edx);
-}
-/* endif defined(__GNUC__) */
 #elif defined(_MSC_VER)
 #include <intrin.h>
 #include <limits.h>
+#else
+#error "oe_get_cpuid(): no cpuid intrinsic mapping for this compiler"
+#endif
+
 /* Same as __get_cpuid, but sub-leaf can be specified.
    Need this function as cpuid level 4 needs the sub-leaf to be specified in ECX
 */
@@ -34,6 +24,9 @@ static inline void oe_get_cpuid(
     unsigned int* __ecx,
     unsigned int* __edx)
 {
+#if defined(__GNUC__)
+    __cpuid_count(__leaf, __subleaf, *__eax, *__ebx, *__ecx, *__edx);
+#elif defined(_MSC_VER)
     int registers[4] = {0};
 
     __cpuidex(registers, __leaf, __subleaf);
@@ -42,7 +35,6 @@ static inline void oe_get_cpuid(
     *__ebx = registers[1];
     *__ecx = registers[2];
     *__edx = registers[3];
+#endif
 }
-#endif /* defined(_MSC_VER) */
-
 #endif /* _OE_CPUIDCOUNT_H */
