8b975714c5052b45c3e8f7129dc9c4e8ea6eedd6
Mike Brasher <mikbras@microsoft.com>
2019-03-28 19:53:21 -0500

    rename file to remove underscore.

---
+++ enclave/core/sgx/hostresolver.c
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "../../../device/resolver/resolv_host/enc/host_resolver.c"
---
+++ host/hostresolver.c
@@ -0,0 +1,4 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "../device/resolver/resolv_host/host/linux/host_resolver.c"
