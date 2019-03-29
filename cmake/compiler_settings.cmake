a29669425d8fdc35ebe20b6567b9d3112bd79cc5
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2019-03-29 15:01:57 -0700

    Apply Spectre mitigations automatically in the CMake package
    This slightly duplicates code in our build, but it is necessary because
    the compiler used to generate the package can be different than the
    compiler used by the consumer of the package.
    
    This code checks if the given Spectre mitigation compiler flags are
    available, and if so, it enables them unconditionally for all code
    linking against `openenclave::oecore` (that is, all enclaves).

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -56,12 +56,13 @@ check_c_compiler_flag("${SPECTRE_MITIGATION_FLAGS}" SPECTRE_MITIGATION_C_FLAGS_S
 check_cxx_compiler_flag("${SPECTRE_MITIGATION_FLAGS}" SPECTRE_MITIGATION_CXX_FLAGS_SUPPORTED)
 if (SPECTRE_MITIGATION_C_FLAGS_SUPPORTED AND SPECTRE_MITIGATION_CXX_FLAGS_SUPPORTED)
   message(STATUS "Spectre 1 mitigations supported")
-  add_compile_options(${SPECTRE_MITIGATION_FLAGS})
-  # Allows reuse in cases where ExternalProject is used and global compile flags wouldn't propagate.
-  string(REPLACE ";" "  " SPECTRE_MITIGATION_FLAGS "${SPECTRE_MITIGATION_FLAGS}")
+  set(OE_SPECTRE_MITIGATION_FLAGS "${SPECTRE_MITIGATION_FLAGS}")
+  # TODO: We really should specify this only on the `oecore` target;
+  # however, the third-party mbed TLS build needs it set to, so we
+  # have to keep this here for now.
+  add_compile_options(${OE_SPECTRE_MITIGATION_FLAGS})
 else ()
   message(WARNING "Spectre 1 mitigations NOT supported")
-  set(SPECTRE_MITIGATION_FLAGS "")
 endif ()
 
 if (CMAKE_CXX_COMPILER_ID MATCHES GNU OR CMAKE_CXX_COMPILER_ID MATCHES Clang)
