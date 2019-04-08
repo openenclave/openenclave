19da82d11b3426b5468aa37ad1ef931a54536771
Hernan Gatta <hegatta@microsoft.com>
2019-04-08 16:00:38 -0700

    Add OP-TEE flags to AARCH64 cross toolchain.

--- cmake/arm-cross.cmake
+++ cmake/arm-cross.cmake
@@ -6,3 +6,43 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 set(CMAKE_C_COMPILER_ID GNU)
+
+# When using GCC to compile assembly files.
+set(OE_TRUSTZONE_TA_S_FLAGS
+    -DASM=1
+    -pipe)
+
+set(OE_TRUSTZONE_TA_DEFINES
+    -D_XOPEN_SOURCE=700
+    -DARM64=1
+    -D__LP64__=1)
+
+# When using GCC to compile C/CXX files.
+set(OE_TRUSTZONE_TA_C_FLAGS
+    -mstrict-align
+    -nostdinc
+    -nostdlib
+    -nodefaultlibs
+    -nostartfiles
+    -fno-builtin-memcpy
+    -fno-builtin-memset
+    -ffreestanding
+    -funwind-tables
+    -fpie
+    -fPIC
+    -gdwarf)
+
+string(REPLACE ";"
+    " "
+    OE_TRUSTZONE_TA_C_FLAGS_STRING
+    "${OE_TRUSTZONE_TA_C_FLAGS}")
+
+# When using GNU LD for linking.
+set(OE_TRUSTZONE_TA_LD_FLAGS "-nostdinc -nostdlib -nodefaultlibs -nostartfiles -pie --sort-section=alignment")
+
+# Path to required bits of the TA Dev Kit.
+set(OE_TRUSTZONE_TA_DEV_KIT_CONF                   ${TA_DEV_KIT_DIR}/host_include/conf.h)
+set(OE_TRUSTZONE_TA_DEV_KIT_LINKER_SCRIPT_TEMPLATE ${TA_DEV_KIT_DIR}/src/ta.ld.S)
+set(OE_TRUSTZONE_TA_DEV_KIT_HEADER_SOURCE          ${TA_DEV_KIT_DIR}/src/user_ta_header.c)
+set(OE_TRUSTZONE_TA_DEV_KIT_DEFAULT_SIGNING_KEY    ${TA_DEV_KIT_DIR}/keys/default_ta.pem)
+set(OE_TRUSTZONE_TA_DEV_KIT_SIGN_TOOL              ${TA_DEV_KIT_DIR}/scripts/sign.py)
