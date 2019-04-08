20386e9b1395857d4d1a891e7f8564aa0963fe8a
Dave Thaler <dthaler@ntdev.microsoft.com>
2019-04-08 16:45:19 -0700

    Pick ARM board from menu
    Paths are placeholders until we get the actual nuget names and paths to use
    
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

--- new_platforms/vsextension/ProjectWizard/WizardImplementation.cs
+++ new_platforms/vsextension/ProjectWizard/WizardImplementation.cs
@@ -4,6 +4,8 @@ using Microsoft.VisualStudio.TemplateWizard;
 using System.Windows.Forms;
 using EnvDTE;
 using System.IO;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace OpenEnclaveSDK
 {
@@ -33,30 +35,56 @@ namespace OpenEnclaveSDK
 
         private bool SetOETADevKitPath(Dictionary<string, string> replacementsDictionary)
         {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // First try picking the board from a menu.
+            BoardPickerPage picker = new BoardPickerPage();
+            DialogResult result = picker.ShowDialog();
+            if (result != DialogResult.OK || picker.Board == "None")
             {
-                openFileDialog.Title = "Select ta_dev_kit.mk in your ARM TA Dev Kit, or hit Cancel to skip ARM support";
-                openFileDialog.InitialDirectory = Directory.GetCurrentDirectory(); // Must be an absolute path.
-                openFileDialog.Filter = "ta_dev_kit.mk|ta_dev_kit.mk";
-                openFileDialog.RestoreDirectory = true;
+                // No ARM support requested.
+                return false;
+            }
 
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+            string folder = null;
+            string board = picker.Board;
+            if (board != "Other")
+            {
+                // User picked a specific board for which we have a TA Dev Kit in the nuget package.
+                string solutionDirectory;
+                replacementsDictionary.TryGetValue("$solutiondirectory$", out solutionDirectory);
+                // TODO: update the path below, and the actual values of board, once we know the path from the actual nuget package.
+                folder = Path.Combine(solutionDirectory, "packages\\openenclave.0.2.0-CI-20190129-011410\\boards\\" + board);
+            }
+            else
+            {
+                // Ok, the user picked "Other", so ask the user for their own TA Dev Kit location.
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
                 {
+                    openFileDialog.Title = "Select ta_dev_kit.mk in your ARM TA Dev Kit, or hit Cancel to skip ARM support";
+                    openFileDialog.InitialDirectory = Directory.GetCurrentDirectory(); // Must be an absolute path.
+                    openFileDialog.Filter = "ta_dev_kit.mk|ta_dev_kit.mk";
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() != DialogResult.OK)
+                    {
+                        return false;
+                    }
+
                     // Get the path of specified file.
-                    var filePath = openFileDialog.FileName;
-                    string folder = Path.GetFullPath(Path.Combine(filePath, "..\\.."));
-
-                    // We now have the full path in Windows format, but we need to convert it to Unix format.
-                    string root = Path.GetPathRoot(folder).ToLower();
-                    string relativeFolder = folder.Substring(root.Length).Replace('\\', '/');
-                    string drive = root.Substring(0, 1);
-                    string unixPath = "/mnt/" + drive + "/" + relativeFolder;
-
-                    replacementsDictionary.Add("$OETADevKitPath$", unixPath);
-                    return true;
+                    string filePath = openFileDialog.FileName;
+                    folder = Path.GetFullPath(Path.Combine(filePath, "..\\.."));
                 }
             }
-            return false;
+
+            // We now have the full path in Windows format, but we need to convert it to Unix format.
+            string root = Path.GetPathRoot(folder).ToLower();
+            string relativeFolder = folder.Substring(root.Length).Replace('\\', '/');
+            string drive = root.Substring(0, 1);
+            string unixPath = "/mnt/" + drive + "/" + relativeFolder;
+
+            replacementsDictionary.Add("$OETADevKitPath$", unixPath);
+            return true;
         }
 
         private bool SetEnclaveName(Dictionary<string, string> replacementsDictionary)
