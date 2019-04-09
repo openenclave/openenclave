58f417ee78a137aeb41c5902827282b0ef204c03
Hernan Gatta <hegatta@microsoft.com>
2019-04-08 18:05:08 -0700

    Prepare libunwind for ARM TrustZone.

---
+++ 3rdparty/libunwind/optee/libunwind-common.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef _OE_LIBUNWIND_COMMON_H
+#define _OE_LIBUNWIND_COMMON_H
+
+#include "libunwind-common.inc"
+
+#endif /* _OE_LIBUNWIND_COMMON_H */
