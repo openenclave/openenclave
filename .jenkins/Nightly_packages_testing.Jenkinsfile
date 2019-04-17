openenclave/openenclave

commit 6ecc1463f5d7b1a1b6919fb75c6194b74c72c955

Update CI Docker images
* Generate `oetools-deploy` Docker image and push it to private registry
* Have the Jenkins user created in the images pushed to Docker private registry

src/jenkins/common/Openenclave.groovy
@@ -29,20 +29,19 @@ def ContainerRun(String imageName, String compiler, String task, String runArgs=
 }
 
 def azureEnvironment(String task) {
-    String buildArgs = dockerBuildArgs("UID=\$(id -u)",
-                                       "GID=\$(id -g)",
-                                       "UNAME=\$(id -un)",
-                                       "GNAME=\$(id -gn)")
-
-    dockerImage("oetools-deploy", ".jenkins/Dockerfile.deploy", buildArgs).inside {
-        timeout(60) {
-            withCredentials([usernamePassword(credentialsId: 'SERVICE_PRINCIPAL_OSTCLAB',
-                                              passwordVariable: 'SERVICE_PRINCIPAL_PASSWORD',
-                                              usernameVariable: 'SERVICE_PRINCIPAL_ID'),
-                             string(credentialsId: 'OSCTLabSubID', variable: 'SUBSCRIPTION_ID'),
-                             string(credentialsId: 'TenantID', variable: 'TENANT_ID')]) {
-                dir('.jenkins/provision') {
-                    sh "${task}"
+    timeout(60) {
+        withCredentials([usernamePassword(credentialsId: 'SERVICE_PRINCIPAL_OSTCLAB',
+                                          passwordVariable: 'SERVICE_PRINCIPAL_PASSWORD',
+                                          usernameVariable: 'SERVICE_PRINCIPAL_ID'),
+                         string(credentialsId: 'OSCTLabSubID', variable: 'SUBSCRIPTION_ID'),
+                         string(credentialsId: 'TenantID', variable: 'TENANT_ID')]) {
+            dir("${WORKSPACE}/.jenkins/provision") {
+                docker.withRegistry("https://oejenkinscidockerregistry.azurecr.io", "oejenkinscidockerregistry") {
+                    image = docker.image("oetools-deploy:latest")
+                    image.pull()
+                    image.inside {
+                        sh "${task}"
+                    }
                 }
             }
         }

.jenkins/Dockerfile.full
@@ -44,6 +44,11 @@ ARG ubuntu_version=18.04
 
 FROM ubuntu:${ubuntu_version}
 
+ARG UNAME=jenkins
+ARG GNAME=jenkins
+ARG UID=1000
+ARG GID=1000
+
 ADD scripts/ansible /ansible
 
 RUN /ansible/install-ansible.sh && \
@@ -52,4 +57,6 @@ RUN /ansible/install-ansible.sh && \
     rm -rf /ansible && \
     # Configure Git in target image to enable merge/rebase actions.
     git config --global user.email "oeciteam@microsoft.com" && \
-    git config --global user.name "OE CI Team"
+    git config --global user.name "OE CI Team" && \
+    groupadd --gid ${GID} ${GNAME} && \
+    useradd --create-home --uid ${UID} --gid ${GID} --shell /bin/bash ${UNAME}

.jenkins/Dockerfile.minimal
@@ -1,8 +1,14 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+
 ARG ubuntu_version=18.04
 
 FROM ubuntu:${ubuntu_version}
+
+ARG UNAME=jenkins
+ARG GNAME=jenkins
+ARG UID=1000
+ARG GID=1000
 
 COPY scripts/ansible /ansible
 
@@ -10,4 +16,6 @@ RUN /ansible/install-ansible.sh && \
     ansible localhost --playbook-dir=/ansible -m import_role -a "name=linux/openenclave tasks_from=environment-setup.yml" -vvv && \
     /ansible/remove-ansible.sh && \
     rm -rf /ansible && \
-    apt-get install --no-install-recommends -y lsb-release wget ca-certificates git
+    apt-get install --no-install-recommends -y lsb-release wget ca-certificates git && \
+    groupadd --gid ${GID} ${GNAME} && \
+    useradd --create-home --uid ${UID} --gid ${GID} --shell /bin/bash ${UNAME}

.jenkins/build_docker_images.Jenkinsfile
@@ -9,17 +9,22 @@ def buildDockerImages() {
             cleanWs()
             checkout scm
         }
-        stage("Build Ubuntu1604 Docker Image") {
-            oefull1604 = oe.dockerImage("oetools-full-16.04:${DOCKER_TAG}", ".jenkins/Dockerfile.full", "--build-arg ubuntu_version=16.04")
+        String buildArgs = oe.dockerBuildArgs("UID=\$(id -u)", "UNAME=\$(id -un)",
+                                              "GID=\$(id -g)", "GNAME=\$(id -gn)")
+        stage("Build Ubuntu 16.04 Full Docker Image") {
+            oefull1604 = oe.dockerImage("oetools-full-16.04:${DOCKER_TAG}", ".jenkins/Dockerfile.full", "${buildArgs} --build-arg ubuntu_version=16.04")
         }
-        stage("Build Ubuntu1804 Docker Image") {
-            oefull1804 = oe.dockerImage("oetools-full-18.04:${DOCKER_TAG}", ".jenkins/Dockerfile.full", "--build-arg ubuntu_version=18.04")
+        stage("Build Ubuntu 18.04 Full Docker Image") {
+            oefull1804 = oe.dockerImage("oetools-full-18.04:${DOCKER_TAG}", ".jenkins/Dockerfile.full", "${buildArgs} --build-arg ubuntu_version=18.04")
         }
-         stage("Build Ubuntu1604 scripts Docker image") {
-            oeminimal1604 = oe.dockerImage("oetools-minimal-16.04:${DOCKER_TAG}", ".jenkins/Dockerfile.minimal", "--build-arg ubuntu_version=16.04")
+         stage("Build Ubuntu 16.04 Minimal Docker image") {
+            oeminimal1604 = oe.dockerImage("oetools-minimal-16.04:${DOCKER_TAG}", ".jenkins/Dockerfile.minimal", "${buildArgs} --build-arg ubuntu_version=16.04")
         }
-        stage("Build Ubuntu1804 scripts Docker image") {
-            oeminimal1804 = oe.dockerImage("oetools-minimal-18.04:${DOCKER_TAG}", ".jenkins/Dockerfile.minimal", "--build-arg ubuntu_version=18.04")
+        stage("Build Ubuntu 18.04 Minimal Docker image") {
+            oeminimal1804 = oe.dockerImage("oetools-minimal-18.04:${DOCKER_TAG}", ".jenkins/Dockerfile.minimal", "${buildArgs} --build-arg ubuntu_version=18.04")
+        }
+        stage("Build Ubuntu Deploy Docker image") {
+            oeDeploy = oe.dockerImage("oetools-deploy:${DOCKER_TAG}", ".jenkins/Dockerfile.deploy", buildArgs)
         }
         stage("Push to OE Docker Registry") {
             docker.withRegistry(OETOOLS_REPO, OETOOLS_REPO_CREDENTIAL_ID) {
@@ -27,10 +32,12 @@ def buildDockerImages() {
                 oefull1804.push()
                 oeminimal1604.push()
                 oeminimal1804.push()
+                oeDeploy.push()
                 oefull1604.push('latest')
                 oefull1804.push('latest')
                 oeminimal1604.push('latest')
                 oeminimal1804.push('latest')
+                oeDeploy.push('latest')
             }
         }
     }

commit 3950f9db901397e8c5c6e75f0fab9c680a3a2715

Fix libcxx Jenkins job regression
Since https://github.com/Microsoft/openenclave/commit/6ecc1463f5d7b1a1b6919fb75c6194b74c72c955, the Jenkins system user exists inside the testing
containers. Thus, when mounting the `provision` directory, the parent
directory is not mounted as well.

This caused a regression to the libcxx Jenkins job because the script
`register-agents.sh` needs access to the `scripts/ansible` directory.
Without mounting the entire parent directory (the OE repository root
directory), we get the following error:

```
./register-agents.sh
realpath: ./../../scripts/ansible: No such file or directory
script returned exit code 1
```

To solve the above problem, we moved the `dir` mounts from the
`azureEnvironment` shared library function. The mounts are used
in every place where `azureEnvironment` is called.

.jenkins/Nightly_packages_testing.Jenkinsfile
@@ -10,7 +10,9 @@ def ACCDeployVM(String agent_name, String agent_type, String region, String reso
             cleanWs()
             checkout scm
             withEnv(["REGION=${region}", "RESOURCE_GROUP=${resource_group}", "AGENT_NAME=${agent_name}", "AGENT_TYPE=${agent_type}", "VHD_URL=${vhd_url}"]) {
-                oe.azureEnvironment("./deploy-agent.sh")
+                dir("${WORKSPACE}/.jenkins/provision") {
+                    oe.azureEnvironment("./deploy-agent.sh")
+                }
             }
         }
     }

.jenkins/build_vhd.Jenkinsfile
@@ -13,12 +13,14 @@ def buildVHD(String version) {
                                       storageAccountKeyVariable: 'WESTEUROPE_STORAGE_ACCOUNT_KEY',
                                       storageAccountNameVariable: 'WESTEUROPE_STORAGE_ACCOUNT_NAME')]) {
             withEnv(["REGION=eastus", "DEST_VHD_NAME=${VHD_NAME_PREFIX}-${version}.vhd", "CONTAINER_NAME=disks"]) {
-                oe.azureEnvironment("""
-                                    packer build -var-file=templates/packer/ubuntu-${version}-variables.json templates/packer/packer.json 2>&1 | tee packer.log
-                                    export SOURCE_URI=\$(cat packer.log | grep OSDiskUri: | awk '{print \$2}')
-                                    az storage blob copy start --source-uri \$SOURCE_URI --destination-blob \$DEST_VHD_NAME --destination-container \$CONTAINER_NAME --account-key \$EASTUS_STORAGE_ACCOUNT_KEY --account-name \$EASTUS_STORAGE_ACCOUNT_NAME
-                                    az storage blob copy start --source-uri \$SOURCE_URI --destination-blob \$DEST_VHD_NAME --destination-container \$CONTAINER_NAME --account-key \$WESTEUROPE_STORAGE_ACCOUNT_KEY --account-name \$WESTEUROPE_STORAGE_ACCOUNT_NAME
-                                    """)
+                dir("${WORKSPACE}/.jenkins/provision") {
+                    oe.azureEnvironment("""
+                                        packer build -var-file=templates/packer/ubuntu-${version}-variables.json templates/packer/packer.json 2>&1 | tee packer.log
+                                        export SOURCE_URI=\$(cat packer.log | grep OSDiskUri: | awk '{print \$2}')
+                                        az storage blob copy start --source-uri \$SOURCE_URI --destination-blob \$DEST_VHD_NAME --destination-container \$CONTAINER_NAME --account-key \$EASTUS_STORAGE_ACCOUNT_KEY --account-name \$EASTUS_STORAGE_ACCOUNT_NAME
+                                        az storage blob copy start --source-uri \$SOURCE_URI --destination-blob \$DEST_VHD_NAME --destination-container \$CONTAINER_NAME --account-key \$WESTEUROPE_STORAGE_ACCOUNT_KEY --account-name \$WESTEUROPE_STORAGE_ACCOUNT_NAME
+                                        """)
+                }
             }
         }
     }

src/jenkins/common/Openenclave.groovy
@@ -35,13 +35,11 @@ def azureEnvironment(String task) {
                                           usernameVariable: 'SERVICE_PRINCIPAL_ID'),
                          string(credentialsId: 'OSCTLabSubID', variable: 'SUBSCRIPTION_ID'),
                          string(credentialsId: 'TenantID', variable: 'TENANT_ID')]) {
-            dir("${WORKSPACE}/.jenkins/provision") {
-                docker.withRegistry("https://oejenkinscidockerregistry.azurecr.io", "oejenkinscidockerregistry") {
-                    image = docker.image("oetools-deploy:latest")
-                    image.pull()
-                    image.inside {
-                        sh "${task}"
-                    }
+            docker.withRegistry("https://oejenkinscidockerregistry.azurecr.io", "oejenkinscidockerregistry") {
+                image = docker.image("oetools-deploy:latest")
+                image.pull()
+                image.inside {
+                    sh "${task}"
                 }
             }
         }
@@ -69,7 +67,9 @@ def deleteRG(List resourceGroups) {
     stage("Delete ${resourceGroups.toString()} resource groups") {
         resourceGroups.each { rg ->
             withEnv(["RESOURCE_GROUP=${rg}"]) {
-                azureEnvironment("./cleanup.sh")
+                dir("${WORKSPACE}/.jenkins/provision") {
+                    azureEnvironment("./cleanup.sh")
+                }
             }
         }
     }

.jenkins/libcxx_tests.Jenkinsfile
@@ -27,7 +27,9 @@ def ACCDeployVM(String agent_name, String agent_type, String region, String reso
             cleanWs()
             checkout scm
             withEnv(["REGION=${region}", "RESOURCE_GROUP=${resource_group}", "AGENT_NAME=${agent_name}", "AGENT_TYPE=${agent_type}", "VHD_URL=${vhd_url}"]) {
-                oe.azureEnvironment("./deploy-agent.sh")
+                dir("${WORKSPACE}/.jenkins/provision") {
+                    oe.azureEnvironment("./deploy-agent.sh")
+                }
             }
         }
     }
@@ -48,7 +50,9 @@ def registerJenkinsSlaves() {
                          "BIONIC_LABEL=${BIONIC_LABEL}",
                          "XENIAL_HOSTS=${hostsList(XENIAL_LABEL, 'eastus').join(',')}",
                          "BIONIC_HOSTS=${hostsList(BIONIC_LABEL, 'westeurope').join(',')}"]) {
-                    oe.azureEnvironment("./register-agents.sh")
+                    dir(WORKSPACE) {
+                        oe.azureEnvironment("${WORKSPACE}/.jenkins/provision/register-agents.sh")
+                    }
                 }
             }
         }