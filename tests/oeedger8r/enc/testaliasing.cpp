3e4e8fec2782b25b45e452dc528c58a90c4789c0
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2019-04-18 16:21:44 -0700

    Enable strict-aliasing for all edger8r tests
    Because we've done so, we can also combine the new tests into the
    existing enclave.

--- tests/oeedger8r/enc/testaliasing.cpp
+++ tests/oeedger8r/enc/testaliasing.cpp
@@ -4,16 +4,16 @@
 #include <cstddef>
 
 #include "../edltestutils.h"
-#include "aliasing_t.h"
+#include "all_t.h"
 
-int a(const char* d, size_t c)
+int aliasing_a(const char* d, size_t c)
 {
     OE_UNUSED(d);
     OE_UNUSED(c);
     return 0;
 }
 
-int b(char** e, size_t* g, size_t f, char* h, size_t i, size_t* j)
+int aliasing_b(char** e, size_t* g, size_t f, char* h, size_t i, size_t* j)
 {
     OE_UNUSED(e);
     OE_UNUSED(g);
@@ -24,7 +24,7 @@ int b(char** e, size_t* g, size_t f, char* h, size_t i, size_t* j)
     return 0;
 }
 
-int c(char* a)
+int aliasing_c(char* a)
 {
     OE_UNUSED(a);
     return 0;
