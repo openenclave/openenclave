5b843baf8fad0d495fc2bba27266998470d27ac5
Mike Brasher <mikbras@microsoft.com>
2019-04-18 18:40:37 -0500

    Removed oe-prefixed function reference from mbedtls.

--- 3rdparty/mbedtls/config.h
+++ 3rdparty/mbedtls/config.h
@@ -2779,19 +2779,8 @@
 
 /* Platform options */
 //#define MBEDTLS_PLATFORM_STD_MEM_HDR   <stdlib.h> /**< Header to include if MBEDTLS_PLATFORM_NO_STD_FUNCTIONS is defined. Don't define if no header is needed. */
-/* Open Enclave: Redirect the mbedtls_calloc/free calls to the corelibc methods
- * so that mbedtls does not need to depend on oelibc. Note that we do this
- * instead of defining the MBEDTLS_PLATFORM_CALLOC/FREE_MACRO as the STD
- * implementation defines them as function pointers instead of macros, which is
- * resiliant to compiling without the oe_calloc/free definition as happens in
- * tests/mbed where the return value is corrupted by being marshalled as an
- * int by default instead of void*.
- *
- * Note: do not depend on oe-prefixed functions because the mbed headers
- * will not compile against libc (only corelibc).
- */
-#define MBEDTLS_PLATFORM_STD_CALLOC        calloc /**< Default allocator to use, can be undefined */
-#define MBEDTLS_PLATFORM_STD_FREE            free /**< Default free to use, can be undefined */
+//#define MBEDTLS_PLATFORM_STD_CALLOC        calloc /**< Default allocator to use, can be undefined */
+//#define MBEDTLS_PLATFORM_STD_FREE            free /**< Default free to use, can be undefined */
 
 //#define MBEDTLS_PLATFORM_STD_EXIT            exit /**< Default exit to use, can be undefined */
 //#define MBEDTLS_PLATFORM_STD_TIME            time /**< Default time to use, can be undefined. MBEDTLS_HAVE_TIME must be enabled */
@@ -2814,7 +2803,7 @@
 //#define MBEDTLS_PLATFORM_TIME_TYPE_MACRO       time_t /**< Default time macro to use, can be undefined. MBEDTLS_HAVE_TIME must be enabled */
 //#define MBEDTLS_PLATFORM_FPRINTF_MACRO      fprintf /**< Default fprintf macro to use, can be undefined */
 /* Open Enclave: Redirect printf to corelibc version for tests */
-#define MBEDTLS_PLATFORM_PRINTF_MACRO        printf /**< Default printf macro to use, can be undefined */
+//#define MBEDTLS_PLATFORM_PRINTF_MACRO        printf /**< Default printf macro to use, can be undefined */
 /* Note: your snprintf must correclty zero-terminate the buffer! */
 //#define MBEDTLS_PLATFORM_SNPRINTF_MACRO    snprintf /**< Default snprintf macro to use, can be undefined */
 //#define MBEDTLS_PLATFORM_NV_SEED_READ_MACRO   mbedtls_platform_std_nv_seed_read /**< Default nv_seed_read function to use, can be undefined */
