0448ef12d2033671c006a164fa00d3725d64dbfa
Mike Brasher <mikbras@microsoft.com>
2019-04-22 16:01:19 -0500

    removed unimplemented optee files.

--- enclave/core/optee/errno.c
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-static int _errno = 0; // We do this because a TA has only one thread
-
-int* __oe_errno_location(void)
-{
-    return &_errno;
-}
