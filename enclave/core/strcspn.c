08f33084b81b09cb4ab48ed3a76e030623bfcf36
Mike Brasher <mikbras@microsoft.com>
2019-04-23 12:00:08 -0500

    reimplement oe_strchrnul()

--- enclave/core/strcspn.c
+++ enclave/core/strcspn.c
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-extern char* __oe_strchrnul(const char* s, int c);
+#include <openenclave/corelibc/string.h>
 
 #pragma GCC diagnostic ignored "-Wsign-conversion"
 #define strcspn oe_strcspn
-#define __strchrnul __oe_strchrnul
+#define __strchrnul oe_strchrnul
 #include "../../3rdparty/musl/musl/src/string/strcspn.c"
