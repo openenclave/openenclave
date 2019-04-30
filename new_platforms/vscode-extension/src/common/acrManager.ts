e501f32e730bbdcdea4ef6ccc8b43cbb0a41ef16
Brian Fjeldstad <bfjelds@microsoft.com>
2019-04-30 14:13:16 -0700

    remove commented code

--- new_platforms/vscode-extension/src/common/acrManager.ts
+++ new_platforms/vscode-extension/src/common/acrManager.ts
@@ -13,32 +13,10 @@ import { AcrRegistryQuickPickItem } from "./models/acrRegistryQuickPickItem";
 
 export class AcrManager {
     private readonly azureAccount: AzureAccount;
-    // private acrRefreshToken: string;
 
     constructor() {
         this.azureAccount = vscode.extensions.getExtension<AzureAccount>("ms-vscode.azure-account")!.exports;
     }
-
-    // public async selectAcrImage(): Promise<string> {
-    //     const acrRegistryItem: AcrRegistryQuickPickItem = await this.selectAcrRegistry();
-    //     if (acrRegistryItem === undefined) {
-    //         throw new UserCancelledError();
-    //     }
-
-    //     const registryUrl: string = acrRegistryItem.registry.loginServer;
-
-    //     const acrRepoItem: vscode.QuickPickItem = await this.selectAcrRepo(registryUrl, acrRegistryItem.azureSubscription.session);
-    //     if (acrRepoItem === undefined) {
-    //         throw new UserCancelledError();
-    //     }
-
-    //     const acrTagItem: vscode.QuickPickItem = await this.selectAcrTag(registryUrl, acrRepoItem.label);
-    //     if (acrTagItem === undefined) {
-    //         throw new UserCancelledError();
-    //     }
-
-    //     return acrTagItem.description;
-    // }
 
     public async getAcrRegistryCredential(address: string): Promise<{ username: string | undefined, password: string | undefined }> {
         let username: string | undefined;
@@ -96,105 +74,4 @@ export class AcrManager {
             throw error;
         }
     }
-
-    // private async selectAcrRepo(registryUrl: string, session: AzureSession): Promise<vscode.QuickPickItem> {
-    //     const acrRepoItem: vscode.QuickPickItem = await vscode.window.showQuickPick(this.loadAcrRepoItems(registryUrl, session), { placeHolder: "Select Repository", ignoreFocusOut: true });
-    //     return acrRepoItem;
-    // }
-
-    // private async loadAcrRepoItems(registryUrl: string, session: AzureSession): Promise<vscode.QuickPickItem[]> {
-    //     try {
-    //         const { aadAccessToken, aadRefreshToken } = await Utility.acquireAadToken(session);
-    //         this.acrRefreshToken = await this.acquireAcrRefreshToken(registryUrl, session.tenantId, aadRefreshToken, aadAccessToken);
-    //         const acrAccessToken = await this.acquireAcrAccessToken(registryUrl, "registry:catalog:*", this.acrRefreshToken);
-
-    //         const catalogResponse = await request.get(`https://${registryUrl}/v2/_catalog`, {
-    //             auth: {
-    //                 bearer: acrAccessToken,
-    //             },
-    //         });
-
-    //         const repoItems: vscode.QuickPickItem[] = [];
-    //         const repos = JSON.parse(catalogResponse).repositories;
-
-    //         if (!repos) {
-    //             const error: any = new Error("There is no repository in the registry.");
-    //             error.statusCode = 404;
-    //             throw error;
-    //         }
-
-    //         repos.map((repo) => {
-    //             repoItems.push({
-    //                 label: repo,
-    //                 description: `${registryUrl}/${repo}`,
-    //             });
-    //         });
-    //         repoItems.sort((a, b) => a.label.localeCompare(b.label));
-    //         return repoItems;
-    //     } catch (error) {
-    //         error.message = `Error fetching repository list: ${error.message}`;
-
-    //         if (error.statusCode === 404) {
-    //             error.message = `Please make sure that there is at least one repository in the registry. ${error.message}`;
-    //         }
-
-    //         throw error;
-    //     }
-    // }
-
-    // private async acquireAcrRefreshToken(registryUrl: string, tenantId: string, aadRefreshToken: string, aadAccessToken: string): Promise<string> {
-    //     const acrRefreshTokenResponse = await request.post(`https://${registryUrl}/oauth2/exchange`, {
-    //         form: {
-    //             grant_type: "access_token_refresh_token",
-    //             service: registryUrl,
-    //             tenant: tenantId,
-    //             refresh_token: aadRefreshToken,
-    //             access_token: aadAccessToken,
-    //         },
-    //     });
-    //     return JSON.parse(acrRefreshTokenResponse).refresh_token;
-    // }
-
-    // private async acquireAcrAccessToken(registryUrl: string, scope: string, acrRefreshToken: string) {
-    //     const acrAccessTokenResponse = await request.post(`https://${registryUrl}/oauth2/token`, {
-    //         form: {
-    //             grant_type: "refresh_token",
-    //             service: registryUrl,
-    //             scope,
-    //             refresh_token: acrRefreshToken,
-    //         },
-    //     });
-    //     return JSON.parse(acrAccessTokenResponse).access_token;
-    // }
-
-    // private async selectAcrTag(registryUrl: string, repo: string): Promise<vscode.QuickPickItem> {
-    //     const tag: vscode.QuickPickItem = await vscode.window.showQuickPick(this.loadAcrTagItems(registryUrl, repo), { placeHolder: "Select Tag", ignoreFocusOut: true });
-    //     return tag;
-    // }
-
-    // private async loadAcrTagItems(registryUrl: string, repo: string): Promise<vscode.QuickPickItem[]> {
-    //     try {
-    //         const acrAccessToken = await this.acquireAcrAccessToken(registryUrl, `repository:${repo}:pull`, this.acrRefreshToken);
-
-    //         const tagsResponse = await request.get(`https://${registryUrl}/v2/${repo}/tags/list`, {
-    //             auth: {
-    //                 bearer: acrAccessToken,
-    //             },
-    //         });
-
-    //         const tagItems: vscode.QuickPickItem[] = [];
-    //         const tags = JSON.parse(tagsResponse).tags;
-    //         tags.map((tag) => {
-    //             tagItems.push({
-    //                 label: tag,
-    //                 description: `${registryUrl}/${repo}:${tag}`,
-    //             });
-    //         });
-    //         tagItems.sort((a, b) => a.label.localeCompare(b.label));
-    //         return tagItems;
-    //     } catch (error) {
-    //         error.message = `Error fetching tag list: ${error.message}`;
-    //         throw error;
-    //     }
-    // }
 }
