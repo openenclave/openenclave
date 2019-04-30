f2184b3385aee5b537b2d01ba24cea3b75b75ef4
Brian Fjeldstad <bfjelds@microsoft.com>
2019-04-30 10:54:09 -0700

    add copyright

--- new_platforms/vscode-extension/src/test/extension.test.ts
+++ new_platforms/vscode-extension/src/test/extension.test.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 //
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
--- new_platforms/vscode-extension/src/test/index.ts
+++ new_platforms/vscode-extension/src/test/index.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 //
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
