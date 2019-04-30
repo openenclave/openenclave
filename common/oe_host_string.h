40b057ef6e33c3937e215f19251509922e27f176
Mike Brasher <mikbras@microsoft.com>
2019-04-30 14:56:31 -0500

    back out uneeded changes.

--- common/oe_host_string.h
+++ common/oe_host_string.h
@@ -38,6 +38,28 @@ int oe_strncmp(const char* s1, const char* s2, size_t n)
 
 /* host already has an oe_strlcat implementation */
 
+OE_INLINE
+char* oe_strerror(int errnum)
+{
+    return strerror(errnum);
+}
+
+OE_INLINE
+int oe_strerror_r(int errnum, char* buf, size_t buflen)
+{
+#if defined(__GNUC__)
+    /* GNUC version of strerror_r returns char* instead
+     * caller is responsible for validating the output buf.
+     * It should never return NULL.
+     */
+    if (!strerror_r(errnum, buf, buflen))
+        return -1;
+    return 0;
+#else
+    return strerror_r(errnum, buf, buflen);
+#endif
+}
+
 OE_EXTERNC_END
 
 #endif /* _OE_HOST_STRING_H */
