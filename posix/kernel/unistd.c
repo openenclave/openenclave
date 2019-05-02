131deae1ecd67e1b920ba2a0337c575d36ee1bac
Mike Brasher <mikbras@microsoft.com>
2019-05-02 13:59:31 -0500

    consolidate functions into header-name sources.

--- posix/kernel/unistd.c
+++ posix/kernel/unistd.c
@@ -11,6 +11,7 @@
 #include <openenclave/corelibc/unistd.h>
 #include <openenclave/internal/time.h>
 #include <openenclave/internal/trace.h>
+#include "posix_t.h"
 
 int oe_gethostname(char* name, size_t len)
 {
@@ -171,4 +172,95 @@ unsigned int oe_sleep(unsigned int seconds)
     const uint64_t msec = seconds * ONE_SECOND;
 
     return (oe_sleep_msec(msec) == 0) ? 0 : seconds;
+}
+
+oe_pid_t oe_getpid(void)
+{
+    oe_pid_t ret = 0;
+    oe_posix_getpid(&ret);
+    return ret;
+}
+
+oe_pid_t oe_getppid(void)
+{
+    oe_pid_t ret = 0;
+    oe_posix_getppid(&ret);
+    return ret;
+}
+
+oe_pid_t oe_getpgrp(void)
+{
+    oe_pid_t ret = 0;
+    oe_posix_getpgrp(&ret);
+    return ret;
+}
+
+oe_uid_t oe_getuid(void)
+{
+    oe_uid_t ret = 0;
+    oe_posix_getuid(&ret);
+    return ret;
+}
+
+oe_uid_t oe_geteuid(void)
+{
+    oe_uid_t ret = 0;
+    oe_posix_geteuid(&ret);
+    return ret;
+}
+
+oe_gid_t oe_getgid(void)
+{
+    oe_gid_t ret = 0;
+    oe_posix_getgid(&ret);
+    return ret;
+}
+
+oe_gid_t oe_getegid(void)
+{
+    oe_gid_t ret = 0;
+    oe_posix_getegid(&ret);
+    return ret;
+}
+
+oe_pid_t oe_getpgid(oe_pid_t pid)
+{
+    oe_pid_t ret = -1;
+    oe_pid_t retval = -1;
+
+    if (oe_posix_getpgid(&retval, pid) != OE_OK)
+    {
+        oe_errno = OE_EINVAL;
+        goto done;
+    }
+
+    if (retval == -1)
+        goto done;
+
+    ret = retval;
+
+done:
+    return ret;
+}
+
+int oe_getgroups(int size, oe_gid_t list[])
+{
+    int ret = -1;
+    int retval = -1;
+
+    if (oe_posix_getgroups(&retval, (size_t)size, list) != OE_OK)
+    {
+        oe_errno = OE_EINVAL;
+        goto done;
+    }
+
+    if (retval == -1)
+    {
+        goto done;
+    }
+
+    ret = retval;
+
+done:
+    return ret;
 }
