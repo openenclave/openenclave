cf882b07b76f4399347d1865787fb68509402a07
Mike Brasher <mikbras@microsoft.com>
2019-05-13 19:36:42 -0500

    use dynamic memory for device table.

--- enclave/core/sgx/once.c
+++ enclave/core/sgx/once.c
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <openenclave/corelibc/stdio.h>
 #include <openenclave/enclave.h>
 #include <openenclave/internal/thread.h>
 #include <openenclave/internal/utils.h>
