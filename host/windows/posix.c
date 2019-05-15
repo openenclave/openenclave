7e6a30571dfb2b1bdb4dc92abebde20b076fed9c
Bruce Campbell <brcampbe@microsoft.com>
2019-05-15 10:01:59 -0700

    Ids test works

--- host/windows/posix.c
+++ host/windows/posix.c
@@ -1959,49 +1959,93 @@ int oe_posix_shutdown_polling_device_ocall(oe_host_fd_t fd)
 **==============================================================================
 */
 
+/* You cna't make an automatic translation between windows SID and unix uid/gid. Eventually you need 
+ * to just return plausible dummy data. In this case, the default group memberships from ubuntu 18.04
+ */
+static const int32_t USER_ID = 1001;
+static const int32_t GROUP_ID = 1001;
+static const int32_t GROUPS[] = {4,20,24,25,27,29,30,44,46,109,110};
+
 int oe_posix_getpid(void)
 {
-    PANIC;
+    return GetCurrentProcessId();
 }
 
+// clang-format off
+#include <tlhelp32.h>
+// clang-format on
 int oe_posix_getppid(void)
 {
-    PANIC;
+    int pid = -1;
+    int ppid = -1;
+    HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32 pe = { 0 };
+    pe.dwSize = sizeof(PROCESSENTRY32);
+
+    pid = GetCurrentProcessId();
+    if(Process32First(h, &pe))
+    {
+        do 
+        {
+            if (pe.th32ProcessID == pid)
+            {
+                ppid = pe.th32ParentProcessID;
+                break;
+            }
+        } while( Process32Next(h, &pe));
+    }
+    CloseHandle(h);
+    return ppid;
 }
 
 int oe_posix_getpgrp(void)
 {
-    PANIC;
+    return 0; // Means the process group is identical to the process. Windows doens't really have process groups
 }
 
 unsigned int oe_posix_getuid(void)
 {
-    PANIC;
+    return USER_ID;
 }
 
 unsigned int oe_posix_geteuid(void)
 {
-    PANIC;
+    return USER_ID;
 }
 
 unsigned int oe_posix_getgid(void)
 {
-    PANIC;
+    return GROUP_ID;
 }
 
 unsigned int oe_posix_getegid(void)
 {
-    PANIC;
+    return GROUP_ID;
 }
 
 int oe_posix_getpgid(int pid)
 {
-    PANIC;
+    return 0;
 }
 
 int oe_posix_getgroups(size_t size, unsigned int* list)
 {
-    PANIC;
+    if (size == 0)
+    {
+        return (int32_t)(sizeof(GROUPS)/sizeof(GROUPS[0]));
+    }
+    if ( size < (sizeof(GROUPS)/sizeof(GROUPS[0])))
+    {
+        _set_errno(OE_EINVAL);
+        return -1;
+    }
+    else 
+    {
+        size = (sizeof(GROUPS)/sizeof(GROUPS[0]));
+    }
+    
+    memcpy(list, GROUPS, sizeof(GROUPS));
+    return (int32_t) size;
 }
 
 /*
--- tests/posix/ids/host/host.c
+++ tests/posix/ids/host/host.c
@@ -5,8 +5,43 @@
 #include <openenclave/host.h>
 #include <openenclave/internal/tests.h>
 #include <stdio.h>
+#if defined(_MSC_VER)
+#include <windows.h>
+#include <stdint.h>
+typedef int32_t gid_t;
+#else
 #include <unistd.h>
+#endif
 #include "test_ids_u.h"
+
+#if !defined(__linux__)
+// clang-format off
+#include <tlhelp32.h>
+// clang-format on
+static int _getppid(void)
+{
+    int pid = -1;
+    int ppid = -1;
+    HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32 pe = { 0 };
+    pe.dwSize = sizeof(PROCESSENTRY32);
+
+    pid = GetCurrentProcessId();
+    if(Process32First(h, &pe))
+    {
+        do
+        {
+            if (pe.th32ProcessID == pid)
+            {
+                ppid = pe.th32ParentProcessID;
+                break;
+            }
+        } while( Process32Next(h, &pe));
+    }
+    CloseHandle(h);
+    return ppid;
+}
+#endif
 
 int main(int argc, const char* argv[])
 {
@@ -29,6 +64,7 @@ int main(int argc, const char* argv[])
 
     /* Ask enclvae to run the test. */
     {
+#if defined(__linux__)
         gid_t list[OE_NGROUPS_MAX] = {0};
         int required_size;
         int size;
@@ -50,6 +86,21 @@ int main(int argc, const char* argv[])
             getpgrp(),
             list,
             (size_t)size);
+#else
+        gid_t list[] = {4,20,24,25,27,29,30,44,46,109,110};
+
+        r = test_ids(
+            enclave,
+            GetCurrentProcessId(),
+            _getppid(),
+            1001,
+            1001, // geteuid(),
+            1001, // getgid(),
+            1001, // getegid(),
+            0,    // getpgrp(),
+            list,
+            sizeof(list)/sizeof(list[0]));
+#endif
 
         // size_t num_groups,
         //[in, count=num_groups] const gid_t* groups);
