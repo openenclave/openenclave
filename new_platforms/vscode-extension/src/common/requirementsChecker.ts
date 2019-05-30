589f06920251d98de1bf72ace889173b15df7733
Brian Fjeldstad <bfjelds@microsoft.com>
2019-05-30 14:34:43 -0700

    Output progress to output view too (#1764)
    * add progress output to Output view
    
    * move to 1.0.5
    
    * do not cache all of stdout, add known issue around git
    
    * cmake is a standalone project requirement, which is only available on linux
    
    * move build/run from source lower, some requirements are linux only
    
    * fix grammar
    
    * test for longpaths without specifying global/system/local, suggest global
    
    * test for 3rdparty/openenclave, not 3rdparty ... and remove failed directories if clone fails
    
    * mark as preview
    
    * add documentation about cross-building containers
    
    * specify that cross-build solution was effective for ubuntu 19.04
    
    * update docs and info on setting up device
    
    * document cross build fix for ubuntu 16.04, 18.04, 18.10, 19.04
    
    * move to 1.0.9
    
    * add docker cross-build requirement check
    
    * once system requirements pass, only run when requirements change or user asks to validate
    
    * capitalize command
    
    * fix grammar
    
    * document requirements check command
    
    * remove redundant doc info
    
    * add docs to help users test their system manually for docker cross-build
    
    * add visual feedback when check requirements command finishes
    
    * move from feature.new_platforms to master
    
    * update to 1.1.0
    
    * vscode data directory can have spaces in them
    
    * fix typo and make SetupDevice=>SetUpDevice
    
    * remove file as part1 of fix for case of filename
    
    * add file as part2 of fix for case of filename

--- new_platforms/vscode-extension/src/common/requirementsChecker.ts
+++ new_platforms/vscode-extension/src/common/requirementsChecker.ts
@@ -6,10 +6,17 @@
 import { ChildProcess, execSync, ExecSyncOptions, spawn, SpawnOptions } from "child_process";
 import * as os from "os";
 import * as vscode from "vscode";
+import { Configuration } from "./configuration";
+import { Constants } from "./constants";
 
 export class RequirementsChecker {
 
-    public static async checkRequirements() {
+    public static async checkRequirements(force: boolean, showSuccess: boolean) {
+
+        const systemConfigurationPassed = Configuration.getConfiguration().get<string>("systemRequirementsPassed");
+        if (!force && systemConfigurationPassed && systemConfigurationPassed === Constants.requirementsVersion) {
+            return;
+        }
 
         const promises: Array<Promise<any>> = [];
         const warnings: string[] = [];
@@ -38,8 +45,25 @@ export class RequirementsChecker {
                 .catch(async (error) => {
                     warnings.push("Unable to locate PYTHON.");
                 }));
+            promises.push(this.validateTool("cmake", ["--version"])
+                .then(async (output) => {
+                    const versionLine = output.split("\n").filter((line) => line.indexOf("cmake version") !== -1);
+                    if (versionLine && versionLine.length > 0) {
+                        const versionMatches = versionLine[0].match(/^cmake version ([0-9]+)\.([0-9]+)\.([0-9]+)$/);
+                        if (versionMatches && versionMatches.length === 4) {
+                            const major = parseInt(versionMatches[1], 10);
+                            const minor = parseInt(versionMatches[2], 10);
+                            if (major < 3 || (major === 3 && minor < 12)) {
+                                warnings.push(`Incorrect CMAKE found (${major}.${minor}).  Version 3.12.0 or higher is required.`);
+                            }
+                        }
+                    }
+                })
+                .catch(async (error) => {
+                    warnings.push("Unable to locate CMAKE 3.12 or higher.");
+                }));
         } else if (os.platform() === "win32") {
-            promises.push(this.validateTool("git", ["config", "--get", "--system", "core.longpaths"])
+            promises.push(this.validateTool("git", ["config", "--get", "core.longpaths"])
                 .then(async (output) => {
                     if (!output || !(/^true/.test(output.trim().toLowerCase()))) {
                         warnings.push(`Enable long paths for GIT.`);
@@ -49,35 +73,24 @@ export class RequirementsChecker {
                     warnings.push(`Enable long paths for GIT.`);
                 }));
         }
-        promises.push(this.validateTool("docker", ["--version"])
-            .catch(async (error) => {
-                warnings.push("Unable to locate DOCKER.");
+        promises.push(this.checkDocker()
+            .then(async (results) => {
+                results.forEach((result) => warnings.push(result));
             }));
         promises.push(this.validateTool("git", ["--version"])
             .catch(async (error) => {
                 warnings.push("Unable to locate GIT.");
             }));
-        promises.push(this.validateTool("cmake", ["--version"])
-            .then(async (output) => {
-                const versionLine = output.split("\n").filter((line) => line.indexOf("cmake version") !== -1);
-                if (versionLine && versionLine.length > 0) {
-                    const versionMatches = versionLine[0].match(/^cmake version ([0-9]+)\.([0-9]+)\.([0-9]+)$/);
-                    if (versionMatches && versionMatches.length === 4) {
-                        const major = parseInt(versionMatches[1], 10);
-                        const minor = parseInt(versionMatches[2], 10);
-                        if (major < 3 || (major === 3 && minor < 12)) {
-                            warnings.push(`Incorrect CMAKE found (${major}.${minor}).  Version 3.12.0 or higher is required.`);
-                        }
-                    }
-                }
-            })
-            .catch(async (error) => {
-                warnings.push("Unable to locate CMAKE 3.12 or higher.");
-            }));
         await Promise.all(promises)
             .then(async () => {
                 if (warnings.length !== 0) {
                     await this.showWarning(warnings.join("  "));
+                } else {
+                    // Update global settings to reflect that this system meets this version's requirements
+                    Configuration.setGlobalConfigurationProperty("systemRequirementsPassed", Constants.requirementsVersion);
+                    if (showSuccess) {
+                        vscode.window.showInformationMessage("System meets requirements.");
+                    }
                 }
             });
     }
@@ -89,6 +102,38 @@ export class RequirementsChecker {
         if (await vscode.window.showWarningMessage(`Some requirements are not found.  ${message}  Click Learn more button to see requirements.` , ...[learnMore]) === learnMore) {
             await vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(requirementsLink));
         }
+    }
+
+    private static async checkDocker(): Promise<string[]> {
+        const warnings: string[] = [];
+
+        // Check for docker installation
+        await Promise.all([
+            this.validateTool("docker", ["--version"])
+                .catch(async (error) => {
+                    warnings.push("Unable to locate DOCKER.");
+                })
+        ]);
+
+        // If docker is found, check for cross-build capability on linux
+        if (warnings.length === 0 && os.platform() === "linux") {
+            const promises: Array<Promise<any>> = [];
+            promises.push(this.validateTool("docker", ["run amd64/ubuntu:xenial"])
+                .catch(async (error) => {
+                    warnings.push("Unable to run amd64 container, enable docker cross-building.");
+                }));
+            promises.push(this.validateTool("docker", ["run arm32v7/ubuntu:xenial"])
+                .catch(async (error) => {
+                    warnings.push("Unable to run arm32v7 container, enable docker cross-building.");
+                }));
+            promises.push(this.validateTool("docker", ["run aarch64/ubuntu:xenial"])
+                .catch(async (error) => {
+                    warnings.push("Unable to run aarch64 container, enable docker cross-building.");
+                }));
+            await Promise.all(promises);
+        }
+
+        return warnings;
     }
 
     private static validateTool(command: string, args: string[]): Promise<string> {
