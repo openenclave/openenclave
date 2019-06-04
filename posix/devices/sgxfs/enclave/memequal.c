fe1f2051b2d15f1bc7c736ac4468bece65b2bbb8
Mike Brasher <mikbras@microsoft.com>
2019-06-03 22:36:39 -0500

    new posix.devices branch.

--- posix/devices/sgxfs/enclave/memequal.c
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-// clang-format off
-#include "common.h"
-#include "../3rdparty/linux-sgx/linux-sgx/sdk/tlibc/string/consttime_memequal.c"
