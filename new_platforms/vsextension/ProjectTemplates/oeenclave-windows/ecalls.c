def4ed78dfd36b77e727b81b825641c11ddf760c
Dave Thaler <dthaler@microsoft.com>
2019-06-14 22:05:14 -0700

    Address more bug bash issues in VS extension (#1942)
    * Add printf to generated sample ecall implementation.  This helps reinforce the point that it's easy to port host code into an enclave.
    * Bump VSIX version to 0.6
    * Work around library path issue in VS Linux feature.  Jirka observed that if mbedtls is installed on Linux, VS uses the wrong path. This works around the issue for now by explicitly using the full
    path to the OE mbedtls libraries.
    
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

--- new_platforms/vsextension/ProjectTemplates/oeenclave-windows/ecalls.c
+++ new_platforms/vsextension/ProjectTemplates/oeenclave-windows/ecalls.c
@@ -1,9 +1,11 @@
 #include <openenclave/enclave.h>
+#include <stdio.h>
 #include "$projectname$_t.h"
 
 int ecall_DoWorkInEnclave(void)
 {
     /* Implement your ECALL here. */
+    printf("Hello from within ecall_DoWorkInEnclave\n");
     oe_result_t result = ocall_DoWorkInHost();
     return (result != OE_OK);
 }
