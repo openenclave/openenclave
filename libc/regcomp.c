5fb9104ababaddb29dd466583affc6e85db901b2
Mike Brasher <mikbras@microsoft.com>
2019-06-17 17:30:25 -0500

    workaround for NDEBUG error in tree.h

---
+++ libc/regcomp.c
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
+#include "../3rdparty/musl/musl/src/regex/regcomp.c"
