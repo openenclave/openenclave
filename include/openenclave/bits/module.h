4971f8f0301e173f01f0912069ed3da8866a7661
radhikaj <radhikaj@microsoft.com>
2019-06-18 22:59:10 +0000

    Ad doxygen tags for bits/module.h and document APIs

--- include/openenclave/bits/module.h
+++ include/openenclave/bits/module.h
@@ -1,6 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+/**
+ * @file module.h
+ *
+ * This file defines functions to load the optional modules available.
+ *
+ */
 #ifndef _OE_BITS_MODULE_H
 #define _OE_BITS_MODULE_H
 
@@ -19,19 +25,65 @@
 
 OE_EXTERNC_BEGIN
 
-/** Load the host file system module. */
+/**
+ * Load the host file system module.
+ *
+ * This function loads the host file system module
+ * which is needed for an enclave application to perform operations
+ * such as mount, fopen, fread and fwrite on files located on the host.
+ *
+ * @retval OE_OK The module was successfully loaded.
+ * @retval OE_FAILURE Module failed to load.
+ *
+ */
 oe_result_t oe_load_module_host_file_system(void);
 
-/** Load the host socket interface module. */
+/**
+ * Load the host socket interface module.
+ *
+ * This function loads the host socket interface module
+ * which is needed for an enclave application to be able to call socket APIs
+ * which are routed through the host.
+ *
+ * @retval OE_OK The module was successfully loaded.
+ * @retval OE_FAILURE Module failed to load.
+ *
+ */
 oe_result_t oe_load_module_host_socket_interface(void);
 
-/** Load the Intel protected file system module. */
+/**
+ * Load the sgx file sytstem module.
+ *
+ * This function enables the use of the SGX file system module.
+ *
+ * @retval OE_OK The module was successfully loaded.
+ * @retval OE_FAILURE Module failed to load.
+ *
+ */
 oe_result_t oe_load_module_sgx_file_system(void);
 
-/** Load the host resolver module (getaddrinfo, getnameinfo). */
+/**
+ * Load the host resolver module.
+ *
+ * This function loads the host resolver module which is needed
+ * for an enclave application to be able to call
+ * getaddrinfo and getnameinfo.
+ *
+ * @retval OE_OK The module was successfully loaded.
+ * @retval OE_FAILURE Module failed to load.
+ */
 oe_result_t oe_load_module_host_resolver(void);
 
-/** Load the event polling module (epoll, poll, and select). */
+/**
+ * Load the event polling module.
+ *
+ * This function loads the event polling module which is needed
+ * for an enclave application to be able to call
+ * epoll, poll, and select.
+ *
+ * @retval OE_OK The module was successfully loaded.
+ * @retval OE_FAILURE Module failed to load.
+ */
 oe_result_t oe_load_module_host_epoll(void);
 
 OE_EXTERNC_END
