90e150df70e4c0535e89e669638167d90c206c76
Anand Krishnamoorthi <anakrish@microsoft.com>
2019-07-30 19:19:32 -0700

    Create oedebugrt.dll on Windows
    Housing debugger metadata in a separate DLL is best practice on windows.
    
    On Linux:
      oedebugrt is built as a static library. Therefore no symbols are
      exported. OE_DEBUGRT_EXPORT is null. Debugrt is subsumed into
      the oehost library which is expected to have debug symbols for
      debugging to work.
    
    On Windows:
       oedebugrt is built as a separate DLL. oehost does not directly link
       against oedebugrt.dll. Instead it uses a bridge that dynamically
       load oedebugrt.dll via LoadLibraryEx and then calls into it via
       the bridge.
       This has the following benefits:
         a) Enclave applications can run even when oedebugrt.dll is not
            present in the system
         b) Release mode applications are completely decoupled from
            oedebugrt.dll
       The installation/packaging/signing/loading story for oedebugrt.dll
       will be similar to other DLLs that oehost will use.
    
    Also change the order of fields for oe_debug_thread_binding_t.
    The next field is grouped together with magic and version fields.
    There are the common fields that all structures share.

---
+++ host/sgx/windows/debugrtbridge.c
@@ -0,0 +1,132 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <Windows.h>
+#include <openenclave/internal/debugrt/host.h>
+#include <openenclave/internal/trace.h>
+#include "../../hostthread.h"
+
+static struct
+{
+    oe_once_type once;
+    HMODULE hmodule;
+    oe_result_t (*notify_enclave_created)(oe_debug_enclave_t* enclave);
+    oe_result_t (*notify_enclave_terminated)(oe_debug_enclave_t* enclave);
+    oe_result_t (*push_thread_binding)(
+        oe_debug_enclave_t* enclave,
+        struct _sgx_tcs* tcs);
+    oe_result_t (*pop_thread_binding)(void);
+} _oedebugrt;
+
+static void get_debugrt_function(const char* name, FARPROC* out)
+{
+    *out = GetProcAddress(_oedebugrt.hmodule, name);
+    if (*out == NULL)
+    {
+        OE_TRACE_FATAL("Could not find function %s in oedebugrt.dll", name);
+    }
+}
+
+static void load_oedebugrt(void)
+{
+    if (_oedebugrt.hmodule != NULL)
+    {
+        OE_TRACE_FATAL("oedebugrt.dll has already been loaded. Multiple load "
+                       "attempts detected.");
+    }
+
+#ifndef NDEBUG
+    /**
+     *  In debug mode, give preference to OE_DEBUGRT_PATH.
+     *  This is mainly used for OE SDK development.
+     */
+    {
+        char* debugrtpath = getenv("OE_DEBUGRT_PATH");
+        _oedebugrt.hmodule = LoadLibraryExA(
+            debugrtpath,
+            NULL, /* reserved */
+            /* Search only specified path. */
+            LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+    }
+
+#endif
+
+    /* Search for oedebugrt.dll only in the application folder. */
+    if (_oedebugrt.hmodule == NULL)
+    {
+        _oedebugrt.hmodule = LoadLibraryExA(
+            "oedebugrt.dll",
+            NULL, /* reserved */
+            LOAD_LIBRARY_SEARCH_APPLICATION_DIR);
+    }
+
+    if (_oedebugrt.hmodule != NULL)
+    {
+        get_debugrt_function(
+            "oe_debug_notify_enclave_created",
+            (FARPROC*)&_oedebugrt.notify_enclave_created);
+        get_debugrt_function(
+            "oe_debug_notify_enclave_terminated",
+            (FARPROC*)&_oedebugrt.notify_enclave_terminated);
+        get_debugrt_function(
+            "oe_debug_push_thread_binding",
+            (FARPROC*)&_oedebugrt.push_thread_binding);
+        get_debugrt_function(
+            "oe_debug_pop_thread_binding",
+            (FARPROC*)&_oedebugrt.pop_thread_binding);
+
+        OE_TRACE_INFO(
+            "oedebugrtbridge: Loaded oedebugrt.dll. Debugging is available.\n");
+    }
+    else
+    {
+        DWORD error = GetLastError();
+        OE_TRACE_INFO(
+            "oedebugrtbridge: LoadLibraryEx on oedebugrt.dll error"
+            "= %#x. Debugging is unavailable.\n",
+            error);
+    }
+}
+
+static void initialize()
+{
+    oe_once(&_oedebugrt.once, &load_oedebugrt);
+}
+
+oe_result_t oe_debug_notify_enclave_created(oe_debug_enclave_t* enclave)
+{
+    initialize();
+    if (_oedebugrt.notify_enclave_created)
+        return _oedebugrt.notify_enclave_created(enclave);
+
+    return OE_OK;
+}
+
+oe_result_t oe_debug_notify_enclave_terminated(oe_debug_enclave_t* enclave)
+{
+    initialize();
+    if (_oedebugrt.notify_enclave_terminated)
+        return _oedebugrt.notify_enclave_terminated(enclave);
+
+    return OE_OK;
+}
+
+oe_result_t oe_debug_push_thread_binding(
+    oe_debug_enclave_t* enclave,
+    struct _sgx_tcs* tcs)
+{
+    initialize();
+    if (_oedebugrt.push_thread_binding)
+        return _oedebugrt.push_thread_binding(enclave, tcs);
+
+    return OE_OK;
+}
+
+oe_result_t oe_debug_pop_thread_binding()
+{
+    initialize();
+    if (_oedebugrt.pop_thread_binding)
+        return _oedebugrt.pop_thread_binding();
+
+    return OE_OK;
+}
