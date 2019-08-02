ce3f5b92af77b7bf0045c14352dd30bb34752507
Hernan Gatta <hegatta@microsoft.com>
2019-08-01 22:29:53 -0700

    Actually call oe_calloc_ocall from TrustZone.

--- enclave/core/optee/hostcalls.c
+++ enclave/core/optee/hostcalls.c
@@ -1,16 +1,31 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <openenclave/bits/safemath.h>
 #include <openenclave/corelibc/stdlib.h>
 #include <openenclave/edger8r/enclave.h>
 #include <openenclave/enclave.h>
 
+#include "optee_t.h"
+
 void* oe_host_calloc(size_t nmemb, size_t size)
 {
-    OE_UNUSED(nmemb);
-    OE_UNUSED(size);
+    void* retval = NULL;
 
-    return NULL;
+    size_t total_size;
+    if (oe_safe_mul_sizet(nmemb, size, &total_size) != OE_OK)
+        return NULL;
+
+    if (oe_calloc_ocall(&retval, nmemb, size) != OE_OK)
+        return NULL;
+
+    if (retval && !oe_is_outside_enclave(retval, total_size))
+    {
+        oe_assert("oe_calloc_ocall() returned non-host memory" == NULL);
+        oe_abort();
+    }
+
+    return retval;
 }
 
 // Function used by oeedger8r for allocating ocall buffers. This function can be
