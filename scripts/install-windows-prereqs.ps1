697156fc2b4935937e8a1fcdbf9f54e55123dc23
John Kordich <johnkord@microsoft.com>
2019-08-21 17:46:46 +0000

    Include reference in install-windows-prereqs.ps1 script to Intel doc.

--- scripts/install-windows-prereqs.ps1
+++ scripts/install-windows-prereqs.ps1
@@ -537,6 +537,7 @@ function Install-DCAPDrivers {
         Throw "Failed to install nuget EnclaveCommonAPI"
     }
 
+    # Please refer to Intel's Windows DCAP documentation for this registry setting: https://download.01.org/intel-sgx/dcap-1.2/windows/docs/Intel_SGX_DCAP_Windows_SW_Installation_Guide.pdf
     New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\sgx_lc_msr\Parameters" -Name "SGX_Launch_Config_Optin" -Value 1 -PropertyType DWORD -Force
 }
 
