b0763fd80e04acea236bf23a05d6dffb8b31e324
Brett McLaren <brmclare@microsoft.com>
2019-08-22 20:56:28 +0000

    Remove pkg-config dependency from debian package
    Fix #2107

--- cmake/cpack_settings.cmake
+++ cmake/cpack_settings.cmake
@@ -10,7 +10,7 @@ set(CPACK_PACKAGE_DESCRIPTION_FILE "${PROJECT_SOURCE_DIR}/README.md")
 set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE")
 set(CPACK_PACKAGE_VERSION ${OE_VERSION})
 set(CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libsgx-enclave-common (>=2.3.100.46354-1), libsgx-enclave-common-dev (>=2.3.100.0-1), libsgx-dcap-ql (>=1.0.100.46460-1.0), libsgx-dcap-ql-dev (>=1.0.100.46460-1.0), pkg-config")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libsgx-enclave-common (>=2.3.100.46354-1), libsgx-enclave-common-dev (>=2.3.100.0-1), libsgx-dcap-ql (>=1.0.100.46460-1.0), libsgx-dcap-ql-dev (>=1.0.100.46460-1.0)")
 
 # CPack variables for the non-enclave host verification package.
 # We match the naming convention of the OE SDK package by setting the
@@ -18,5 +18,4 @@ set(CPACK_DEBIAN_PACKAGE_DEPENDS "libsgx-enclave-common (>=2.3.100.46354-1), lib
 set(CPACK_DEBIAN_OEHOSTVERIFY_PACKAGE_NAME "open-enclave-hostverify")
 set(CPACK_DEBIAN_OEHOSTVERIFY_FILE_NAME "${CPACK_DEBIAN_OEHOSTVERIFY_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-Linux.deb")
 set(CPACK_COMPONENT_OEHOSTVERIFY_DESCRIPTION "Open Enclave Report Verification Host Library")
-set(CPACK_DEBIAN_OEHOSTVERIFY_PACKAGE_DEPENDS "pkg-config")
 include(CPack)
