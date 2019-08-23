8e7e57b98d37930dd6f987fe415dd219ce150451
Brett McLaren <brmclare@microsoft.com>
2019-08-23 01:15:55 +0000

    Add 'pkg-config' to debian package reccomendations
    Fix #2107

--- cmake/cpack_settings.cmake
+++ cmake/cpack_settings.cmake
@@ -11,12 +11,14 @@ set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE")
 set(CPACK_PACKAGE_VERSION ${OE_VERSION})
 set(CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libsgx-enclave-common (>=2.3.100.46354-1), libsgx-enclave-common-dev (>=2.3.100.0-1), libsgx-dcap-ql (>=1.0.100.46460-1.0), libsgx-dcap-ql-dev (>=1.0.100.46460-1.0)")
+set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "pkg-config")
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 
 # CPack variables for the non-enclave host verification package.
 # We match the naming convention of the OE SDK package by setting the
 # CPACK_DEBIAN_OEHOSTVERIFY_FILE_NAME field.
 set(CPACK_DEBIAN_OEHOSTVERIFY_PACKAGE_NAME "open-enclave-hostverify")
+set(CPACK_DEBIAN_OEHOSTVERIFY_PACKAGE_RECOMMENDS "pkg-config")
 set(CPACK_DEBIAN_OEHOSTVERIFY_FILE_NAME DEB-DEFAULT)
 set(CPACK_COMPONENT_OEHOSTVERIFY_DESCRIPTION "Open Enclave Report Verification Host Library")
 include(CPack)
