a892d305650f3fe8d996a3f3a82ec232062dce94
Anand Krishnamoorthi <anakrish@microsoft.com>
2019-10-01 00:12:54 +0000

    Fix missed out optee file

--- host/optee/linux/enclave.c
+++ host/optee/linux/enclave.c
@@ -399,8 +399,8 @@ oe_result_t oe_create_enclave(
     const char* enclave_path,
     oe_enclave_type_t enclave_type,
     uint32_t flags,
-    const oe_enclave_config_t* configs,
-    uint32_t config_count,
+    const oe_enclave_settings_t* settings,
+    uint32_t setting_count,
     const oe_ocall_func_t* ocall_table,
     uint32_t ocall_count,
     oe_enclave_t** enclave_out)
@@ -425,8 +425,8 @@ oe_result_t oe_create_enclave(
     if (!enclave_path || !enclave_out ||
         ((enclave_type != OE_ENCLAVE_TYPE_OPTEE) &&
          (enclave_type != OE_ENCLAVE_TYPE_AUTO)) ||
-        (config_count > 0 && configs == NULL) ||
-        (config_count == 0 && configs != NULL) ||
+        (setting_count > 0 && configs == NULL) ||
+        (setting_count == 0 && settings != NULL) ||
         (flags & OE_ENCLAVE_FLAG_RESERVED) ||
         (!(flags & OE_ENCLAVE_FLAG_SIMULATE) &&
          (flags & OE_ENCLAVE_FLAG_DEBUG)))
