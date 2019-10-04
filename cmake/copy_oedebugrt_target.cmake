64282a95270c135e1f5254c50c4dc64b06302269
Radhika Jandhyala <radhikaj@microsoft.com>
2019-10-04 16:23:14 -0700

    Fix cmake module

--- cmake/copy_oedebugrt_target.cmake
+++ cmake/copy_oedebugrt_target.cmake
@@ -1,8 +1,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-## This function adds a CMake target for oedebugrt.dll. This allows the caller to add a dependency on oedebugrt.dll so
+## This function adds a CMake target for oedebugrt.dll. This allows the caller to :add a dependency on oedebugrt.dll so
 ## that it will be copied to the output folder along with the target taking the dependency.
+## When the host executable is launched under windbg, if edebugrt.dll is present in the same path as the host executable,
+## it gets automatically loaded into the debugger. This enables windbg to debug enclave applications.
 ##
 ## TARGET_NAME: Name of the target to add for oe_debugrt. This should be unique for each caller.
 ##
@@ -13,20 +15,16 @@ function(copy_oedebugrt_target TARGET_NAME)
         message(WARNING "copy_oedebugrt_target is only intended for WIN32 build environments. Check if this invocation is needed.")
     endif ()
 
-    # Initialize the null list of dependencies for the target
-    set(DEPENDENCIES "")
-
+    get_property(oedebugrtlocation TARGET openenclave::oedebugrt PROPERTY LOCATION)
     # Add copy actions for the dependencies
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/oedebugrt.dll
-        DEPENDS oe:oedebugrt
-        COMMAND ${CMAKE_COMMAND} -E copy oe:oedebugrt ${CMAKE_CURRENT_BINARY_DIR})
+        DEPENDS ${oedebugrtlocation}
+        COMMENT FOOBARBAZ
+        COMMAND ${CMAKE_COMMAND} -E copy ${oedebugrtlocation} ${CMAKE_CURRENT_BINARY_DIR})
 
-    # Add the dependencies to the custom target list of dependencies
-    list(APPEND DEPENDENCIES
-        ${CMAKE_CURRENT_BINARY_DIR}/oedebugrt.dll)
 
     # Always create the requested target, which may have an empty dependency list
-    add_custom_target(${TARGET_NAME} DEPENDS ${DEPENDENCIES})
+    add_custom_target(${TARGET_NAME} DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/oedebugrt.dll)
 
 endfunction( copy_oedebugrt_target )
