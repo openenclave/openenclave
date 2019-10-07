11a74694a7d28ebf9435dcccdb224ff7681e7ea8
Olga Vrousgou <olgavrou@gmail.com>
2019-10-07 13:00:22 +0100

    only test character escaping in test

--- tests/logging/main.c
+++ tests/logging/main.c
@@ -8,22 +8,7 @@
     }
 
 #include <openenclave/internal/tests.h>
-#include <openenclave/internal/trace.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "../host/traceh.c"
-
-void test_line_format(const char* line)
-{
-    OE_TEST(strstr(line, "e_ts") != NULL);
-    OE_TEST(strstr(line, "level") != NULL);
-    OE_TEST(strstr(line, "tid") != NULL);
-    OE_TEST(strstr(line, "msg") != NULL);
-    OE_TEST(strstr(line, "file") != NULL);
-    OE_TEST(strstr(line, "func") != NULL);
-    OE_TEST(strstr(line, "number") != NULL);
-}
 
 void test_escaped_msg(const char* msg, const char* expected, bool expect_ok)
 {
@@ -39,39 +24,6 @@ void test_escaped_msg(const char* msg, const char* expected, bool expect_ok)
     }
     OE_TEST(strcmp(msg_escaped, expected) == 0);
     free(msg_escaped);
-}
-
-int TestLoggingFormat(const char* path)
-{
-    OE_TRACE_ERROR("Hey");
-    OE_TRACE_ERROR("Hello 'world'!");
-    OE_TRACE_ERROR("Hello \"world\"!");
-    OE_TRACE_ERROR("/ \\/ \' \a \\a \b \\b \e \\e \f \\f \n \\n \r \\r \t \\t "
-                   "\v \\v \? \\?");
-    OE_TRACE_ERROR("\\u005C \u0024 \u0040 @");
-    OE_TRACE_ERROR("\\\\\\\\");
-    OE_TRACE_ERROR("\u2605");
-    OE_TRACE_ERROR("\01");
-    OE_TRACE_ERROR("\037");
-    OE_TRACE_ERROR("\024");
-    OE_TRACE_ERROR("\200");
-    OE_TRACE_ERROR("\u2605\u0024");
-    FILE* log_file = fopen(path, "r");
-    char* line = NULL;
-    size_t len = 0;
-    if (log_file == NULL)
-    {
-        fprintf(stderr, "Failed to OPEN logfile %s\n", "out.log");
-        return 1;
-    }
-
-    while (getline(&line, &len, log_file) != -1)
-    {
-        test_line_format(line);
-    }
-    fclose(log_file);
-    printf("=== passed TestLoggingFormat()\n");
-    return 0;
 }
 
 int TestEscapedCharacters()
@@ -107,25 +59,8 @@ int TestEscapedCharacters()
     return 0;
 }
 
-int main(int argc, const char* argv[])
+int main()
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s LOG_FILE_PATH\n", argv[0]);
-        return 1;
-    }
-
-    const char* path = argv[1];
-    OE_TEST(setenv("OE_LOG_DEVICE", path, true) == 0);
-    OE_TEST(
-        setenv(
-            "OE_LOG_FORMAT",
-            "{\"e_ts\":\"%s.%06ldZ\",\"level\":\"(%s)%s\",\"tid\":\"tid(0x%lx)"
-            "\",\"msg\":\"%s\",\"file\":\"%s\",\"func\":\"%s\",\"number\":\"%"
-            "s\"}\n",
-            true) == 0);
-    OE_TEST(setenv("OE_LOG_JSON_ESCAPE", "true", true) == 0);
     TestEscapedCharacters();
-    TestLoggingFormat(path);
     return 0;
 }
