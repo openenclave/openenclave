87280435cc2fa300b36876212226f14d5cd18fae
Jordan Hand <jorhand@microsoft.com>
2019-12-03 16:17:49 -0800

    Don't build socket test enclave when BUILD_ENCLAVES is set to FALSE
    Signed-off-by: Jordan Hand <jorhand@microsoft.com>

--- samples/attested_tls/scripts/gen_mrenclave_header.sh
+++ samples/attested_tls/scripts/gen_mrenclave_header.sh
@@ -6,18 +6,22 @@
 destfile="$1"
 input_file="$2"
 
-cat > "$destfile" << EOF
+# Store text in variables and then write file all at once at the end. This
+# will keep build tools from consuming an incomplete header file.
+
+SECTION1="$(cat <<-EOF
 // Copyright (c) Open Enclave SDK contributors.
 // Licensed under the MIT License.
 
 #ifndef SAMPLES_ATTESTED_TLS_SERVER_UNIQUE_ID_H
 #define SAMPLES_ATTESTED_TLS_SERVER_UNIQUE_ID_H
 
+static const unsigned char SERVER_ENCLAVE_MRENCLAVE[] =
+{
 EOF
+)"
 
-printf 'static const unsigned char SERVER_ENCLAVE_MRENCLAVE[] =' >> "$destfile"
-printf '\n{' >> "$destfile"
-
+SECTION2=""
 while read -r line; do
   if [[ $line == mrenclave=* ]]; then
     read -r mrenclave < <(echo "$line" | cut -d'=' --fields=2)
@@ -26,18 +30,23 @@ while read -r line; do
     fi
 
     for (( i=0; i<${#mrenclave}; i=i+2)); do
-      echo -n "0x${mrenclave:$i:2}" >> "$destfile"
+      echo -n "" >> "$destfile"
+      SECTION2=$SECTION2"0x${mrenclave:$i:2}"
       index=$((${#mrenclave} - 2))
       if [ $i -lt $index ]; then
-        printf ",\n" >> "$destfile"
+        SECTION2=$SECTION2",\n"
       fi
     done
   fi
 done < "$input_file"
 
-printf '};\n' >> "$destfile"
 
-cat >> "$destfile" << EOF
+SECTION3="$(cat <<-EOF
+};
 
 #endif /* SAMPLES_ATTESTED_TLS_SERVER_UNIQUE_ID_H */
 EOF
+)"
+
+echo -ne "$SECTION1$SECTION2$SECTION3" > $destfile
+
