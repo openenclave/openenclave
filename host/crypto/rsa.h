2179e5a603c2e1bd05e832a3ba1c5f63cabe64df
Ubuntu <oeuser@OeSignIntegrationEnvironment.0zp2phv2g3he1f4qf3smv33lka.bx.internal.cloudapp.net>
2019-12-18 20:41:35 +0000

    Using engine to sign enclave

--- host/crypto/rsa.h
+++ host/crypto/rsa.h
@@ -11,4 +11,10 @@ oe_result_t oe_rsa_get_public_key_from_private(
     const oe_rsa_private_key_t* private_key,
     oe_rsa_public_key_t* public_key);
 
+/* Caller is responsible for freeing public key. */
+oe_result_t oe_rsa_get_public_key_from_engine(
+    const char *engine_id,
+    const uint64_t key_id,
+    oe_rsa_public_key_t* public_key);
+
 #endif /* _OE_HOST_CRYPTO_RSA_H */
