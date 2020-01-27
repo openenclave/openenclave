c6e462bbb5b3082b62c0519db8a1f6a07557255c
Anand Krishnamoorthi <anakrish@microsoft.com>
2020-01-27 00:55:57 +0000

    Support clang-9 and clang-8
    clang-8 and clang-9 have added improvements to x86-speculative-load-hardening.
    Visual Studio's clang support now bundles clang-8.
    
    clang-8 can be supported without any changes to master.
    
    clang-9 generates stack checking for functions in sgx/calls.c as well as memset/memcpy.
    Th former functions are called before stack checking is setup for an enclave thread,
    whereas the latter are called during the setup/cleanup of stack checking.
    Therefore, these files must be compiled without stack protectors.
    
    Additional fixes:
    - memset, memcpy and other C files were being compiled in oecore with different flags
    and then in oelibc with different flags. Now they are compiled in oelibc with correct
    flags
    - Whole of oelibc was being compiled with no builtin memset or memcpy. Only those two
    functions are now being compiled in that manner. This allows the compiler to use
    the builtins as needed when compiling oelibc.
    
    Signed-off-by: Anand Krishnamoorthi <anakrish@microsoft.com>

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -4,8 +4,8 @@
 # Check Clang version.
 if (CMAKE_C_COMPILER_ID MATCHES Clang)
   if (CMAKE_C_COMPILER_VERSION VERSION_LESS 7 OR
-      CMAKE_C_COMPILER_VERSION VERSION_GREATER 7.99)
-    message(WARNING "Open Enclave officially supports Clang 7 only, "
+      CMAKE_C_COMPILER_VERSION VERSION_GREATER 9.99)
+    message(WARNING "Open Enclave officially supports Clang 7, Clang 8 and Clang 9 only, "
       "but your Clang version (${CMAKE_C_COMPILER_VERSION}) "
       "is older or newer than that. Build problems may occur.")
   endif ()
