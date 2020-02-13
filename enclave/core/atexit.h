5dca2017462da60e6e8ad3a0ec95111cbb5af3ba
Jordan Hand <jorhand@microsoft.com>
2020-02-13 14:45:12 -0800

    Rename __cxa_atexit in oecore to oe_cxa_atexit
    Symbols in oecore should not conflict with symbols defined in libc to
    allow alternate libc implementations to be used without issue.
    
    Retain oe_cxa_atexit implementation so that it can be used by oecore.
    Modify oelibc to simply call oe_cxa_atexit from __cxa_atexit.
    
    Signed-off-by: Jordan Hand <jorhand@microsoft.com>

--- enclave/core/atexit.h
+++ enclave/core/atexit.h
@@ -12,7 +12,7 @@ int oe_atexit(void (*function)(void));
 
 void oe_call_atexit_functions(void);
 
-int __cxa_atexit(void (*func)(void*), void* arg, void* dso_handle);
+int oe_cxa_atexit(void (*func)(void*), void* arg, void* dso_handle);
 
 OE_EXTERNC_END
 
