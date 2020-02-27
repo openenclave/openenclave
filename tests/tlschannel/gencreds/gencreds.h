99d4eca71f170a0ed197d27ba42a95bf3e14bdce
Mike Brasher <mikbras@microsoft.com>
2020-02-27 23:16:07 +0000

    cleanup

---
+++ tests/tlschannel/gencreds/gencreds.h
@@ -0,0 +1,17 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#ifndef _OE_GENCREDS_H
+#define _OE_GENCREDS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+int oe_generated_attested_credentials(
+    const char* common_name,
+    uint8_t** cert_out,
+    size_t* cert_size_out,
+    uint8_t** private_key_out,
+    size_t* private_key_size_out);
+
+#endif /* _OE_GENCREDS_H */
