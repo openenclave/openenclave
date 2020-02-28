4c0bd68b51f43db05d25d7cb627ef597ba935d10
Mike Brasher <mikbras@microsoft.com>
2020-02-28 17:16:27 +0000

    add verify_identity function

--- tests/tlschannel/srv/tlssrv.h
+++ tests/tlschannel/srv/tlssrv.h
@@ -8,6 +8,19 @@
 #include <mbedtls/ssl.h>
 #include <mbedtls/ssl_cache.h>
 
+#define TSLSRV_MRENCLAVE_SIZE 32
+#define TSLSRV_MRSIGNER_SIZE 32
+#define TSLSRV_PRODUCT_ID_SIZE 16
+
+typedef oe_result_t (*verify_identity_function_t)(
+    void* arg,
+    const uint8_t* mrenclave,
+    size_t mrenclave_size,
+    const uint8_t* mrsigner,
+    size_t mrsigner_size,
+    const uint8_t* product_id,
+    size_t product_id_size);
+
 typedef struct _tlssrv_err
 {
     char buf[1024];
@@ -23,6 +36,8 @@ typedef struct _tlssrv
     mbedtls_x509_crt crt;
     mbedtls_pk_context pk;
     mbedtls_ssl_cache_context cache;
+    verify_identity_function_t verify_identity;
+    void* verify_identity_arg;
 } tlssrv_t;
 
 int tlssrv_startup(tlssrv_err_t* err);
@@ -32,6 +47,8 @@ int tlssrv_shutdown(tlssrv_err_t* err);
 int tlssrv_create(
     const char* host,
     const char* port,
+    verify_identity_function_t verify_identity,
+    void* verify_identity_arg,
     tlssrv_t** srv_out,
     tlssrv_err_t* err);
 
