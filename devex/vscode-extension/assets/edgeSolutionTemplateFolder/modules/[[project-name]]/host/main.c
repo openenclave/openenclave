47a072f93559801df62a7acc5bbddd549add2473
Hernan Gatta <hegatta@microsoft.com>
2020-02-28 16:01:32 -0800

    Clean up the sample code.
    Signed-off-by: Hernan Gatta <hegatta@microsoft.com>

--- devex/vscode-extension/assets/edgeSolutionTemplateFolder/modules/[[project-name]]/host/main.c
+++ devex/vscode-extension/assets/edgeSolutionTemplateFolder/modules/[[project-name]]/host/main.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include <openenclave/host.h>
 
@@ -15,15 +16,14 @@
 #include <azure_c_shared_utility/shared_util_options.h>
 #include <iothubtransportmqtt.h>
 #include <iothub.h>
-#include <time.h>
+
+#include "host.h"
 
 // Set to true to enable tracing.
-bool traceOn = false;
+bool g_traceOn = false;
 
-// Forward declarations.
-int create_enclave(int argc, const char* argv[]);
-int terminate_enclave();
-int call_enclave(char *input_msg, char *enclave_msg, unsigned int enclave_msg_size);
+// Message counter.
+size_t g_messagesReceivedByInput1Queue = 0;
 
 typedef struct MESSAGE_INSTANCE_TAG
 {
@@ -31,10 +31,7 @@ typedef struct MESSAGE_INSTANCE_TAG
 
     // For tracking the messages within the user callback.
     size_t messageTrackingId;
-}
-MESSAGE_INSTANCE;
-
-size_t messagesReceivedByInput1Queue = 0;
+} MESSAGE_INSTANCE;
 
 // SendConfirmationCallback is invoked when the message that was forwarded on
 // from 'InputQueue1Callback' pipeline function is confirmed.
@@ -44,7 +41,8 @@ static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result,
     // The context corresponds to which message# we were at when we sent.
     MESSAGE_INSTANCE* messageInstance = (MESSAGE_INSTANCE*)userContextCallback;
 
-    printf("Confirmation[%zu] received for message with result = %d\r\n", messageInstance->messageTrackingId, result);
+    printf("Confirmation[%zu] received for message with result = %d.\n",
+        messageInstance->messageTrackingId, result);
 
     IoTHubMessage_Destroy(messageInstance->messageHandle);
     free(messageInstance);
@@ -59,7 +57,7 @@ static MESSAGE_INSTANCE* CreateMessageInstance(char* messageBody)
     MESSAGE_INSTANCE* messageInstance = (MESSAGE_INSTANCE*)malloc(sizeof(MESSAGE_INSTANCE));
     if (NULL == messageInstance)
     {
-        printf("Failed to allocate 'MESSAGE_INSTANCE' for pipelined message\r\n");
+        fprintf(stderr, "Failed to allocate 'MESSAGE_INSTANCE' for pipelined message.\n");
     }
     else
     {
@@ -75,7 +73,7 @@ static MESSAGE_INSTANCE* CreateMessageInstance(char* messageBody)
             IoTHubMessage_SetContentTypeSystemProperty(messageInstance->messageHandle, "application%2fjson");
             IoTHubMessage_SetContentEncodingSystemProperty(messageInstance->messageHandle, "utf-8");
 
-            messageInstance->messageTrackingId = messagesReceivedByInput1Queue;
+            messageInstance->messageTrackingId = g_messagesReceivedByInput1Queue;
         }
     }
 
@@ -87,17 +85,15 @@ static IOTHUBMESSAGE_DISPOSITION_RESULT SendEnclaveResponse(IOTHUB_MODULE_CLIENT
     IOTHUBMESSAGE_DISPOSITION_RESULT result;
     IOTHUB_CLIENT_RESULT clientResult;
 
-    const size_t enclaveMessageSize = 512;
-
-    char* enclaveMessage = (char*)malloc(enclaveMessageSize * sizeof(char));
+    char* enclaveMessage = (char*)malloc(ENCLAVE_MESSAGE_SIZE * sizeof(char));
     if (NULL == enclaveMessage)
     {
-        printf("Failed to allocate enclave message\r\n");
+        fprintf(stderr, "Failed to allocate enclave message.\n");
         result = IOTHUBMESSAGE_ABANDONED;
     }
     else
     {
-        int enclaveResult = call_enclave(messageBodyStr, enclaveMessage, enclaveMessageSize);
+        int enclaveResult = call_enclave(messageBodyStr, enclaveMessage, ENCLAVE_MESSAGE_SIZE);
         if (enclaveResult != 0)
         {
             result = IOTHUBMESSAGE_ABANDONED;
@@ -114,7 +110,8 @@ static IOTHUBMESSAGE_DISPOSITION_RESULT SendEnclaveResponse(IOTHUB_MODULE_CLIENT
             }
             else
             {
-                printf("Sending message (%zu) to the next stage in pipeline\n", messagesReceivedByInput1Queue);
+                printf("Sending message (%zu) to the next stage in pipeline.\n",
+                    g_messagesReceivedByInput1Queue);
 
                 clientResult = IoTHubModuleClient_LL_SendEventToOutputAsync(
                     iotHubModuleClientHandle, messageInstance->messageHandle,
@@ -124,8 +121,8 @@ static IOTHUBMESSAGE_DISPOSITION_RESULT SendEnclaveResponse(IOTHUB_MODULE_CLIENT
                     IoTHubMessage_Destroy(messageInstance->messageHandle);
                     free(messageInstance);
 
-                    printf("IoTHubModuleClient_LL_SendEventToOutputAsync failed on sending msg#=%zu, err=%d\n",
-                        messagesReceivedByInput1Queue, clientResult);
+                    fprintf(stderr, "IoTHubModuleClient_LL_SendEventToOutputAsync failed on sending msg#=%zu, err=%d.\n",
+                        g_messagesReceivedByInput1Queue, clientResult);
 
                     result = IOTHUBMESSAGE_ABANDONED;
                 }
@@ -158,7 +155,7 @@ static IOTHUBMESSAGE_DISPOSITION_RESULT InputQueue1Callback(IOTHUB_MESSAGE_HANDL
     char* messageBodyStr = (char*)malloc(contentSize + 1);
     if (NULL == messageBodyStr)
     {
-        printf("Failed to allocate message body string\r\n");
+        fprintf(stderr, "Failed to allocate message body string.\n");
         result = IOTHUBMESSAGE_ABANDONED;
     }
     else
@@ -166,13 +163,13 @@ static IOTHUBMESSAGE_DISPOSITION_RESULT InputQueue1Callback(IOTHUB_MESSAGE_HANDL
         memcpy(messageBodyStr, messageBody, contentSize);
         messageBodyStr[contentSize] = '\0';
 
-        printf("Received Message [%zu]\r\n Data: [%s]\r\n",
-                messagesReceivedByInput1Queue, messageBodyStr);
+        printf("Received Message [%zu]\n Data: [%s].\n",
+                g_messagesReceivedByInput1Queue, messageBodyStr);
 
         result = SendEnclaveResponse(iotHubModuleClientHandle, messageBodyStr);
 
         free(messageBodyStr);
-        messagesReceivedByInput1Queue++;
+        g_messagesReceivedByInput1Queue++;
     }
 
     return result;
@@ -184,16 +181,16 @@ static IOTHUB_MODULE_CLIENT_LL_HANDLE InitializeConnection()
 
     if (IoTHub_Init() != 0)
     {
-        printf("Failed to initialize the platform.\r\n");
+        fprintf(stderr, "Failed to initialize the platform.\n");
         iotHubModuleClientHandle = NULL;
     }
     else if ((iotHubModuleClientHandle = IoTHubModuleClient_LL_CreateFromEnvironment(MQTT_Protocol)) == NULL)
     {
-        printf("ERROR: IoTHubModuleClient_LL_CreateFromEnvironment failed\r\n");
+        fprintf(stderr, "Failed to create IoT Hub module client from environment.\n");
     }
     else
     {
-        IoTHubModuleClient_LL_SetOption(iotHubModuleClientHandle, OPTION_LOG_TRACE, &traceOn);
+        IoTHubModuleClient_LL_SetOption(iotHubModuleClientHandle, OPTION_LOG_TRACE, &g_traceOn);
     }
 
     return iotHubModuleClientHandle;
@@ -215,7 +212,7 @@ static int SetupCallbacksForModule(IOTHUB_MODULE_CLIENT_LL_HANDLE iotHubModuleCl
     if (IoTHubModuleClient_LL_SetInputMessageCallback(iotHubModuleClientHandle, "input1",
         InputQueue1Callback, (void*)iotHubModuleClientHandle) != IOTHUB_CLIENT_OK)
     {
-        printf("ERROR: IoTHubModuleClient_LL_SetInputMessageCallback(\"input1\")..........FAILED!\r\n");
+        fprintf(stderr, "Failed to set input message callback.\n");
         ret = -1;
     }
     else
@@ -247,7 +244,7 @@ void iothub_module()
         SetupCallbacksForModule(iotHubModuleClientHandle) == 0)
     {
         // The receiver just loops constantly waiting for messages.
-        printf("Waiting for incoming messages.\r\n");
+        printf("Waiting for incoming messages.\n");
         while (true)
         {
             IoTHubModuleClient_LL_DoWork(iotHubModuleClientHandle);
