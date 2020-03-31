60b3dcb1db39baa68f3fdb85549d3d83aa1cca8e
Jordan Hand <jorhand@microsoft.com>
2020-03-31 13:33:59 -0700

    Add oe_wcslen implementation to corelibc
    In an effort to allow edger8r-generated code to not depend on stdc,
    oecore needs an implementation of wcslen. Add such an implementation.
    
    Signed-off-by: Jordan Hand <jorhand@microsoft.com>

--- include/openenclave/corelibc/wchar.h
+++ include/openenclave/corelibc/wchar.h
@@ -1,2 +1,11 @@
 // Copyright (c) Open Enclave SDK contributors.
 // Licensed under the MIT License.
+
+#ifndef _OE_WCHAR_H
+#define _OE_WCHAR_H
+
+#include <openenclave/bits/types.h>
+
+size_t oe_wcslen(const wchar_t* s);
+
+#endif
