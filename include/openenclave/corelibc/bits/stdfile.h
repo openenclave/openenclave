b2757c4a481bc270998276ed9dc8242fdbee87a8
Jordan Hand <jorhand@microsoft.com>
2020-04-01 16:36:48 -0700

    Refactor stdio.h and stdfile.h in corelibc to properly use stdc names
    Remove a circular dependency in stdio.h and stdfile.h in corelibc which
    cause build failures when using stdc names depending on the order
    headers were included.
    
    Signed-off-by: Jordan Hand <jorhand@microsoft.com>

--- include/openenclave/corelibc/bits/stdfile.h
+++ include/openenclave/corelibc/bits/stdfile.h
@@ -4,9 +4,19 @@
 #ifndef _OE_BITS_STDFILE_H
 #define _OE_BITS_STDFILE_H
 
+#define OE_BUFSIZ 8192
+#define OE_EOF (-1)
+
+typedef struct _OE_IO_FILE OE_FILE;
+extern OE_FILE* const oe_stdin;
+extern OE_FILE* const oe_stdout;
+extern OE_FILE* const oe_stderr;
+
+#if defined(OE_NEED_STDC_NAMES)
 typedef OE_FILE FILE;
 #define stdin oe_stdin
 #define stdout oe_stdout
 #define stderr oe_stderr
+#endif
 
 #endif /* _OE_BITS_STDFILE_H */
