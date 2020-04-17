203c3a80d173835ce1f2243285a9e20866a809b1
Yufei Jiang <yufjiang@microsoft.com>
2020-04-17 02:16:57 +0000

    not an error but a warning
    Signed-off-by: Yufei Jiang <yufjiang@microsoft.com>

--- host/sgx/windows/sgxquoteproviderloader.c
+++ host/sgx/windows/sgxquoteproviderloader.c
@@ -40,8 +40,9 @@ void oe_load_quote_provider()
             }
             else
             {
-                OE_TRACE_ERROR("sgxquoteprovider: sgx_ql_set_logging_function "
-                               "not found\n");
+                OE_TRACE_WARNING(
+                    "sgxquoteprovider: sgx_ql_set_logging_function "
+                    "not found\n");
             }
 
             provider.get_sgx_quote_verification_collateral =
