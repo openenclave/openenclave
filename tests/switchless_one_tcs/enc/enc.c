e8eb4223ada74e2a0dd0f5f70473daef1516a502
yan xue <yan.xue@intel.com>
2020-04-17 13:46:54 +0800

    add switchless test for one TCS
    Signed-off-by: yan xue <yan.xue@intel.com>

---
+++ tests/switchless_one_tcs/enc/enc.c
@@ -0,0 +1,30 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <openenclave/corelibc/string.h>
+#include <openenclave/enclave.h>
+#include <openenclave/internal/print.h>
+#include "switchless_one_tcs_t.h"
+
+void enc_empty_regular(void)
+{
+}
+
+void enc_empty_switchless(status_e* status)
+{
+    int num_waiting = 0;
+    *status = FUNC_WORKING;
+    while (*status != FUNC_EXIT)
+    {
+        num_waiting++;
+    }
+    oe_host_printf("enc_empty_switchless exit\n");
+}
+
+OE_SET_ENCLAVE_SGX(
+    1,        /* ProductID */
+    1,        /* SecurityVersion */
+    true,     /* AllowDebug */
+    64,       /* HeapPageCount */
+    64,       /* StackPageCount */
+    NUM_TCS); /* TCSCount */
