3d6df0043e54c3f1b4b24138f9ef407be551a1be
xiaonan-INTC <xiaonan.zhao@intel.com>
2020-06-12 15:33:37 +0800

    add oe stress test of ecall ocall
    Signed-off-by: xiaonan-INTC <xiaonan.zhao@intel.com>

--- tests/stress/enc/enc.cpp
+++ tests/stress/enc/enc.cpp
@@ -4,11 +4,12 @@
 #include <openenclave/internal/print.h>
 #include "stress_t.h"
 
+static int rcv = 0;
+
 void do_ecall(int arg)
 {
-    // almost do nothing, just assign value
-    int rcv = 0;
-    rcv = arg;
+    // almost do nothing
+    rcv = arg + 1;
 }
 
 OE_SET_ENCLAVE_SGX(
