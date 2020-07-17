10ac71446f876a22389d59aee8b066aae9c185df
Mike Brasher <mikbras@microsoft.com>
2020-07-17 16:48:55 +0000

    use colon separated enclave path

--- tests/split/host/host.c
+++ tests/split/host/host.c
@@ -15,19 +15,14 @@ int main(int argc, const char* argv[])
     const oe_enclave_type_t type = OE_ENCLAVE_TYPE_SGX;
     const uint32_t flags = oe_get_create_flags();
     int retval;
-    char path[1024];
 
-    if (argc != 3)
+    if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s <enclave> <image>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <enclave>\n", argv[0]);
         return 1;
     }
 
-    strcpy(path, argv[1]);
-    strcat(path, ":");
-    strcat(path, argv[2]);
-
-    r = oe_create_split_enclave(path, type, flags, NULL, 0, &enclave);
+    r = oe_create_split_enclave(argv[1], type, flags, NULL, 0, &enclave);
     OE_TEST(r == OE_OK);
 
     r = split_ecall(enclave, &retval);
