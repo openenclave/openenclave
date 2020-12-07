91fe135287ab9a2579c8c383e401c785f2e023db
Anand Krishnamoorthi <anakrish@microsoft.com>
2020-12-07 14:05:14 -0800

    Remove unused stubs
    New syscall dispatching no longer requires these stubs to be present.
    The linker is already eliminating these stubs in the enclave binary.
    Remove these stubs in code.
    
    Signed-off-by: Anand Krishnamoorthi <anakrish@microsoft.com>

--- tests/edl_opt_out/enc/enc.c
+++ tests/edl_opt_out/enc/enc.c
@@ -12,89 +12,8 @@ void enc_edl_opt_out()
     /* logging.edl */
     OE_TEST(oe_log_ocall(0, NULL) == OE_UNSUPPORTED);
 
-    /* epoll.edl */
-    OE_TEST(oe_syscall_epoll_create1_ocall(NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_epoll_wait_ocall(NULL, 0, NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_epoll_wake_ocall(NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_epoll_ctl_ocall(NULL, 0, 0, 0, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_epoll_close_ocall(NULL, 0) == OE_UNSUPPORTED);
-
-    /* fcntl.edl */
-    OE_TEST(oe_syscall_read_ocall(NULL, 0, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_write_ocall(NULL, 0, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_pread_ocall(NULL, 0, NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_pwrite_ocall(NULL, 0, NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_fsync_ocall(NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_fdatasync_ocall(NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_opendir_ocall(NULL, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_readdir_ocall(NULL, 0, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_rewinddir_ocall(0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_closedir_ocall(NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_stat_ocall(NULL, NULL, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_fstat_ocall(NULL, 0, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_access_ocall(NULL, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_link_ocall(NULL, NULL, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_unlink_ocall(NULL, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_rename_ocall(NULL, NULL, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_truncate_ocall(NULL, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_mkdir_ocall(NULL, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_rmdir_ocall(NULL, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_fcntl_ocall(NULL, 0, 0, 0, 0, NULL) == OE_UNSUPPORTED);
-
     /* ioctl.edl */
     OE_TEST(oe_syscall_ioctl_ocall(NULL, 0, 0, 0, 0, NULL) == OE_UNSUPPORTED);
-
-    /* signal.edl */
-    OE_TEST(oe_syscall_kill_ocall(NULL, 0, 0) == OE_UNSUPPORTED);
-
-    /* socket.edl */
-    OE_TEST(oe_syscall_close_socket_ocall(NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_socket_ocall(NULL, 0, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_shutdown_sockets_device_ocall(NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_socketpair_ocall(NULL, 0, 0, 0, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_connect_ocall(NULL, 0, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_accept_ocall(NULL, 0, NULL, 0, NULL) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_bind_ocall(NULL, 0, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_listen_ocall(NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_recvmsg_ocall(
-            NULL, 0, NULL, 0, NULL, NULL, 0, 0, NULL, 0, NULL, 0) ==
-        OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_sendmsg_ocall(NULL, 0, NULL, 0, NULL, 0, 0, NULL, 0, 0) ==
-        OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_recv_ocall(NULL, 0, NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_recvfrom_ocall(NULL, 0, NULL, 0, 0, NULL, 0, NULL) ==
-        OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_send_ocall(NULL, 0, NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_sendto_ocall(NULL, 0, NULL, 0, 0, NULL, 0) ==
-        OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_recvv_ocall(NULL, 0, NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_sendv_ocall(NULL, 0, NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_shutdown_ocall(NULL, 0, 0) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_setsockopt_ocall(NULL, 0, 0, 0, NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_getsockname_ocall(NULL, 0, NULL, 0, NULL) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_getpeername_ocall(NULL, 0, NULL, 0, NULL) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_getaddrinfo_open_ocall(NULL, NULL, NULL, NULL, NULL) ==
-        OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_getaddrinfo_read_ocall(
-            NULL, 0, NULL, NULL, NULL, NULL, 0, NULL, NULL, 0, NULL, NULL) ==
-        OE_UNSUPPORTED);
-    OE_TEST(oe_syscall_getaddrinfo_close_ocall(NULL, 0) == OE_UNSUPPORTED);
-    OE_TEST(
-        oe_syscall_getnameinfo_ocall(NULL, NULL, 0, NULL, 0, NULL, 0, 0) ==
-        OE_UNSUPPORTED);
-
-    /* poll.edl */
-    OE_TEST(oe_syscall_poll_ocall(NULL, NULL, 0, 0) == OE_UNSUPPORTED);
 
     /* time.edl */
     OE_TEST(oe_syscall_nanosleep_ocall(NULL, NULL, NULL) == OE_UNSUPPORTED);
@@ -109,9 +28,6 @@ void enc_edl_opt_out()
     OE_TEST(oe_syscall_getegid_ocall(NULL) == OE_UNSUPPORTED);
     OE_TEST(oe_syscall_getpgid_ocall(NULL, 0) == OE_UNSUPPORTED);
     OE_TEST(oe_syscall_getgroups_ocall(NULL, 0, NULL) == OE_UNSUPPORTED);
-
-    /* utsname.edl */
-    OE_TEST(oe_syscall_uname_ocall(NULL, NULL) == OE_UNSUPPORTED);
 
 #if __x86_64__ || _M_X64
     /* debug.edl */
