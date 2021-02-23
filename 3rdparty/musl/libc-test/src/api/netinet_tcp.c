6aab17db77e901fceb11ec1e3ccd79e39a8ac7d9
Ming-Wei Shih <mishih@microsoft.com>
2021-02-23 22:58:45 +0000

    Use musl and libc-test submodules
    Signed-off-by: Ming-Wei Shih <mishih@microsoft.com>

--- 3rdparty/musl/libc-test/src/api/netinet_tcp.c
+++
@@ -1,6 +0,0 @@
-#include <netinet/tcp.h>
-#define C(n) switch(n){case n:;}
-static void f()
-{
-C(TCP_NODELAY)
-}
--- 3rdparty/musl/libc-test/src/math/special/acoshf.h
+++
@@ -1,8 +0,0 @@
-T(RN,             nan,             nan,          0x0p+0, 0)
-T(RN,             inf,             inf,          0x0p+0, 0)
-T(RN,          0x1p+0,          0x0p+0,          0x0p+0, 0)
-T(RN,     0x1.ffffp-1,             nan,          0x0p+0, INVALID)
-T(RN,          0x0p+0,             nan,          0x0p+0, INVALID)
-T(RN,         -0x0p+0,             nan,          0x0p+0, INVALID)
-T(RN,            -inf,             nan,          0x0p+0, INVALID)
-T(RN,        -0x1p+50,             nan,          0x0p+0, INVALID)
