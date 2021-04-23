45823c8ec5889a39b2c47d598c02586122e5d892
Yen Lee <yenlee@microsoft.com>
2021-04-22 21:35:28 -0700

    attestation perf

---
+++ tests/tools/attestation_perf/enc/enc.cpp
@@ -0,0 +1,88 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <openenclave/attestation/attester.h>
+#include <openenclave/attestation/sgx/evidence.h>
+#include <openenclave/attestation/verifier.h>
+#include <openenclave/edger8r/enclave.h>
+#include <openenclave/enclave.h>
+#include <openenclave/internal/raise.h>
+#include <openenclave/internal/report.h>
+#include <openenclave/internal/safecrt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "attestation_perf_t.h"
+
+oe_result_t get_evidence(
+    uint8_t* evidence_out,
+    size_t evidence_size,
+    size_t* evidence_out_size,
+    bool verify)
+{
+    oe_result_t result = OE_UNEXPECTED;
+    uint8_t* local_evidence = NULL;
+    size_t local_evidence_size = 0;
+
+    {
+        static const oe_uuid_t _ecdsa_uuid = {OE_FORMAT_UUID_SGX_ECDSA};
+        OE_CHECK(oe_attester_initialize());
+        OE_CHECK(oe_get_evidence(
+            &_ecdsa_uuid,
+            OE_EVIDENCE_FLAGS_EMBED_FORMAT_ID,
+            NULL,
+            0,
+            NULL,
+            0,
+            &local_evidence,
+            &local_evidence_size,
+            NULL,
+            0));
+        OE_CHECK(oe_attester_shutdown());
+
+        if (evidence_out)   // Caller wants to get evidence buffer
+        {
+            if (local_evidence_size > evidence_size)
+                return OE_BUFFER_TOO_SMALL;
+
+            oe_memcpy_s(
+                evidence_out,
+                evidence_size,
+                local_evidence,
+                local_evidence_size);
+            *evidence_out_size = local_evidence_size;
+        }
+        else if (evidence_out_size)
+        {
+            *evidence_out_size = 0;
+        }
+    }
+
+    if (verify)
+    {
+        oe_claim_t* claims = NULL;
+        size_t claims_length = 0;
+
+        OE_CHECK(oe_verifier_initialize());
+        OE_CHECK(
+            oe_verify_evidence(
+                NULL,
+                local_evidence,
+                local_evidence_size,
+                NULL,
+                0,
+                NULL,
+                0,
+                &claims,
+                &claims_length));
+        oe_free_claims(claims, claims_length);
+        OE_CHECK(oe_verifier_shutdown());
+    }
+
+    result = OE_OK;
+
+done:
+    oe_free_evidence(local_evidence);
+
+    return result;
+}
---
+++ tests/tools/attestation_perf/host/host.cpp
@@ -0,0 +1,103 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <openenclave/attestation/attester.h>
+#include <openenclave/attestation/verifier.h>
+#include <openenclave/host.h>
+#include <openenclave/internal/raise.h>
+#include <openenclave/internal/report.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#if defined(__linux__)
+#include <unistd.h>
+#elif defined(_WIN32)
+#include <windows.h>
+#endif
+#include "attestation_perf_u.h"
+
+long get_tick()
+{
+    return clock() * 1000 / CLOCKS_PER_SEC;
+}
+
+int main(int argc, const char* argv[])
+{
+    int ret = 0;
+    oe_result_t result = OE_UNEXPECTED;
+    oe_enclave_t* enclave = nullptr;
+    const char* enclave_filename = argv[1];
+    oe_claim_t* claims = nullptr;
+    size_t claims_length = 0;
+    uint8_t evidence[8096] = {};
+    size_t evidence_size = 0;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Invalid parameter.\nSyntax: %s <enclave_file>\n", argv[0]);
+        return -1;
+    }
+
+    OE_CHECK_MSG(
+        oe_create_attestation_perf_enclave(
+            enclave_filename,
+            OE_ENCLAVE_TYPE_AUTO,
+            OE_ENCLAVE_FLAG_DEBUG,
+            nullptr,
+            0,
+            &enclave),
+        "Failed to create enclave. result=%u (%s)\n",
+        oe_result_str(result));
+
+    // TODO - Remove cache
+
+    printf(
+        "1. Verifying evidence with no collateral cache (%ld)\n", get_tick());
+    OE_CHECK_MSG(
+        get_evidence(enclave, &result, nullptr, 0, nullptr, true),
+        "Failed to create OE evidence. Error: %s\n",
+        oe_result_str(result));
+
+    printf(
+        "2. Verifying evidence with collateral cache (%ld)\n", get_tick());
+    OE_CHECK_MSG(
+        get_evidence(enclave, &result, nullptr, 0, nullptr, true),
+        "Failed to create OE evidence. Error: %s\n",
+        oe_result_str(result));
+
+    printf("3. Generating evidence without verifying (%ld)\n", get_tick());
+    OE_CHECK_MSG(
+        get_evidence(
+            enclave, &result, evidence, sizeof(evidence), &evidence_size, false),
+        "Failed to create OE evidence. Error: %s\n",
+        oe_result_str(result));
+
+    printf("4. Verifying evidence in host (%ld)\n", get_tick());
+    OE_CHECK(oe_verifier_initialize());
+    OE_CHECK_MSG(
+        oe_verify_evidence(
+            nullptr,
+            evidence,
+            evidence_size,
+            nullptr,
+            0,
+            nullptr,
+            0,
+            &claims,
+            &claims_length),
+        "Failed to verify evidence. Error: %s\n",
+        oe_result_str(result));
+
+    printf("5. Done verifying evidence in host (%ld)\n", get_tick());
+    oe_free_claims(claims, claims_length);
+    OE_CHECK(oe_verifier_shutdown());
+
+    result = OE_OK;
+
+done:
+    if (enclave)
+        oe_terminate_enclave(enclave);
+
+    return ret;
+}
