ad8494be9fcbc2b3826aeb7d57a2e95be002eabb
Ming-Wei Shih <mishih@microsoft.com>
2021-06-16 17:20:46 +0000

    Expose necessary libc symbols for the SymCrypt FIPS module
    Signed-off-by: Ming-Wei Shih <mishih@microsoft.com>

--- tests/module_loading/module/module.c
+++ tests/module_loading/module/module.c
@@ -1,6 +1,10 @@
 // Copyright (c) Open Enclave SDK contributors.
 // Licensed under the MIT License.
 
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
 extern int debugger_test;
 extern int is_module_init;
 
@@ -36,4 +40,32 @@ int add_with_constant(volatile int a, volatile int b)
     else
         t = -1;
     return t;
+}
+
+#define TEST_SYMBOL(name)                 \
+    do                                    \
+    {                                     \
+        volatile void* ptr = (void*)name; \
+        (void)ptr;                        \
+    } while (0)
+
+int test_libc_symbols()
+{
+    TEST_SYMBOL(memcmp);
+    TEST_SYMBOL(memcpy);
+    TEST_SYMBOL(memmove);
+    TEST_SYMBOL(memset);
+    TEST_SYMBOL(pthread_mutex_destroy);
+    TEST_SYMBOL(pthread_mutex_init);
+    TEST_SYMBOL(pthread_mutex_lock);
+    TEST_SYMBOL(pthread_mutex_unlock);
+    TEST_SYMBOL(posix_memalign);
+    TEST_SYMBOL(free);
+    TEST_SYMBOL(qsort);
+
+    /* The size of pthread_mutex_t is required to be 40 to be
+     * compatible with musl and glibc. */
+    if (sizeof(pthread_mutex_t) == 40)
+        return 1;
+    return 0;
 }
