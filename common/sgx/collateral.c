47060ae19d4081da665bc6c888c7bfc350166d8e
Daniel Weiße <dw@edgeless.systems>
2021-10-06 16:41:36 +0200

    Support for Intel PCCS API v3
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- common/sgx/collateral.c
+++ common/sgx/collateral.c
@@ -3,6 +3,7 @@
 
 #include "collateral.h"
 #include <openenclave/bits/attestation.h>
+#include <openenclave/corelibc/ctype.h>
 #include <openenclave/internal/calls.h>
 #include <openenclave/internal/crypto/cert.h>
 #include <openenclave/internal/crypto/crl.h>
@@ -290,17 +291,44 @@ oe_result_t oe_validate_revocation_list(
                 "Failed to read CRL. %s",
                 oe_result_str(result));
         }
-        // Otherwise, CRL should have v3 structure which is DER encoded
+        // Otherwise, CRL should have v3 or v3.1 structure
+        //    v3 is hex encoded DER
+        //    v3.1 is DER
         else
         {
             size_t der_data_size = sgx_endorsements->items[i].size;
             if (der_data_size == 0)
                 OE_RAISE(OE_INVALID_PARAMETER);
 
-            // If DER CRL buffer has null terminator, need to remove it before
+            // If CRL buffer has null terminator, need to remove it before
             // send the DER data to crypto API for reading.
             if (sgx_endorsements->items[i].data[der_data_size - 1] == 0)
                 der_data_size -= 1;
+
+            // If the CRL is only composed of hex digits we need to manually
+            // convert to DER
+            bool ishex = true;
+            for (size_t l = 0; l < der_data_size; ++l)
+            {
+                const uint8_t c = sgx_endorsements->items[i].data[l];
+                if (!oe_isxdigit(c))
+                {
+                    ishex = false;
+                    break;
+                }
+            }
+            if (ishex)
+            {
+                OE_CHECK_MSG(
+                    oe_hex_to_buffer(
+                        (const char*)sgx_endorsements->items[i].data,
+                        der_data_size,
+                        sgx_endorsements->items[i].data,
+                        der_data_size),
+                    "Failed to convert to DER. %s",
+                    oe_result_str(result));
+                der_data_size /= 2;
+            }
 
             OE_CHECK_MSG(
                 oe_crl_read_der(
