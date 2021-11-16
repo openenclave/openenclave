e0d710e66efdf309a9c8f9f8ce55f9a1c9bdaa25
Amaury Chamayou <amchamay@microsoft.com>
2021-11-16 17:01:32 +0000

    Test snmalloc upgrade
    Signed-off-by: Amaury Chamayou <amchamay@microsoft.com>

--- 3rdparty/snmalloc/allocator.cpp
+++ 3rdparty/snmalloc/allocator.cpp
@@ -4,32 +4,38 @@
 #include <openenclave/advanced/allocator.h>
 #include <openenclave/advanced/mallinfo.h>
 
-namespace snmalloc
-{
-__thread void* allocator_local;
-
-class ThreadAllocUntyped
-{
-  public:
-    static void* get();
-};
-
-void* ThreadAllocUntyped::get()
-{
-    return allocator_local;
-}
-} // namespace snmalloc
-
+//#define SNMALLOC_CHECK_CLIENT
+#define SNMALLOC_PROVIDE_OWN_CONFIG
+#define SNMALLOC_USE_CXX17
 #define OPEN_ENCLAVE
 #define SNMALLOC_SGX
 #define SNMALLOC_USE_SMALL_CHUNKS
 #define SNMALLOC_EXTERNAL_THREAD_ALLOC
 #define SNMALLOC_NAME_MANGLE(a) oe_allocator_##a
 
+#include "./snmalloc/src/snmalloc/snmalloc_core.h"
+#include "./snmalloc/src/snmalloc/backend/fixedglobalconfig.h"
+
+namespace snmalloc
+{
+  using Globals = snmalloc::FixedGlobals<snmalloc::PALOpenEnclave>;
+  using Alloc = snmalloc::LocalAllocator<Globals>;
+
+class ThreadAllocExternal
+{
+public:
+  static Alloc& get()
+  {
+    static __thread Alloc alloc;
+    return alloc;
+  }
+};
+}
+
 // Enable the Open Enclave PAL(Platform Abstraction Layer) for Open Enclave,
 // see pal_open_enclave.h for details
-#include "./snmalloc/src/override/malloc-extensions.cc"
-#include "./snmalloc/src/override/malloc.cc"
+#include "./snmalloc/src/snmalloc/override/malloc-extensions.cc"
+#include "./snmalloc/src/snmalloc/override/malloc.cc"
 
 static size_t _max_heap_size;
 static malloc_info_v1 _info;
@@ -40,8 +46,7 @@ void oe_allocator_init(void* heap_start_address, void* heap_end_address)
         static_cast<uint8_t*>(heap_end_address) -
         static_cast<uint8_t*>(heap_start_address));
 
-    snmalloc::PALOpenEnclave::setup_initial_range(
-        heap_start_address, heap_end_address);
+    Globals::init(nullptr, heap_start_address, _max_heap_size);
 }
 
 void oe_allocator_cleanup(void)
@@ -50,13 +55,13 @@ void oe_allocator_cleanup(void)
 
 void oe_allocator_thread_init(void)
 {
-    allocator_local = current_alloc_pool()->acquire();
+    auto allocator = new (&ThreadAllocExternal::get()) snmalloc::Alloc();
+    allocator->init();
 }
 
 void oe_allocator_thread_cleanup(void)
 {
-    current_alloc_pool()->release(ThreadAlloc::get());
-    allocator_local = nullptr;
+    ThreadAllocExternal::get().teardown();
 }
 
 oe_result_t oe_allocator_mallinfo(oe_mallinfo_t* info)
