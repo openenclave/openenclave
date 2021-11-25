3dc32f37b68b85912586fe93fc2176a9656d242a
Ming-Wei Shih <mishih@microsoft.com>
2021-11-25 00:53:28 +0000

    [Mystikos] Optimize is_enclave_debug_allowed
    Signed-off-by: Ming-Wei Shih <mishih@microsoft.com>

--- enclave/core/sgx/tracee.c
+++ enclave/core/sgx/tracee.c
@@ -9,28 +9,40 @@
 #include "../tracee.h"
 #include "report.h"
 
+static volatile int _is_enclave_debug_allowed = -1;
+
 // Read an enclave's identity attribute to see to if it was signed as an debug
 // enclave
 bool is_enclave_debug_allowed()
 {
-    bool ret = false;
-    oe_sgx_td_t* td = oe_sgx_get_td();
+    oe_sgx_td_t* td = NULL;
 
-    if (td->simulate)
+    if (_is_enclave_debug_allowed != -1)
+        goto done;
+
+    td = oe_sgx_get_td();
+
+    if (td && td->simulate)
     {
         // enclave in simulate mode is treated as debug_allowed
-        ret = true;
+        _is_enclave_debug_allowed = 1;
     }
     else
     {
         // get a report on the enclave itself for enclave identity information
         sgx_report_t sgx_report;
         oe_result_t result = sgx_create_report(NULL, 0, NULL, 0, &sgx_report);
-        if (result != OE_OK)
-            goto done;
 
-        ret = (sgx_report.body.attributes.flags & SGX_FLAGS_DEBUG) != 0;
+        if (result != OE_OK)
+        {
+            _is_enclave_debug_allowed = 0;
+            goto done;
+        }
+
+        _is_enclave_debug_allowed =
+            (sgx_report.body.attributes.flags & SGX_FLAGS_DEBUG) ? 1 : 0;
     }
+
 done:
-    return ret;
+    return _is_enclave_debug_allowed == 1 ? true : false;
 }
