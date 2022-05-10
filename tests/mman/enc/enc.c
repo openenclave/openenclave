7fa549f5e822c1f1009b3d4fdabf92ca5d88f6ee
Amaury Chamayou <amchamay@microsoft.com>
2022-05-10 19:43:25 +0000

    Adjust memory limits
    Signed-off-by: Amaury Chamayou <amchamay@microsoft.com>

--- tests/mman/enc/enc.c
+++ tests/mman/enc/enc.c
@@ -352,6 +352,6 @@ OE_SET_ENCLAVE_SGX(
     1,    /* ProductID */
     1,    /* SecurityVersion */
     true, /* Debug */
-    1024, /* NumHeapPages */
-    1024, /* NumStackPages */
+    2048, /* NumHeapPages */
+    2048, /* NumStackPages */
     2);   /* NumTCS */
