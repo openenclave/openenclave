f5dbab92262f044e58200fc7e129e3443ff6d88f
Chris Yan <chrisyan@microsoft.com>
2022-08-02 09:59:03 -0700

    Update Git to 2.37.1
    Signed-off-by: Chris Yan <chrisyan@microsoft.com>

--- scripts/install-windows-prereqs.ps1
+++ scripts/install-windows-prereqs.ps1
@@ -3,8 +3,8 @@
 
 # The Hash parameter defaults below are calculated using Get-FileHash with the default SHA256 hashing algorithm
 Param(
-    [string]$GitURL = 'https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe',
-    [string]$GitHash = 'C43611EB73AD1F17F5C8CC82AE51C3041A2E7279E0197CCF5F739E9129CE426E',
+    [string]$GitURL = 'https://github.com/git-for-windows/git/releases/download/v2.37.1.windows.1/Git-2.37.1-64-bit.exe',
+    [string]$GitHash = '1966761AD2C9E4CBD38F9E583B1125949B011A5A250A99D65E9BB21958E6EF8B',
     [string]$OpenSSLURL = 'https://oejenkins.blob.core.windows.net/oejenkins/openssl.1.1.1579.74.nupkg',
     [string]$OpenSSLHash = '82678FC7C71CBACA420BC869A1938BC80F2876148A5870D46B4C9A746F6BCEB6',
     [string]$SevenZipURL = 'https://www.7-zip.org/a/7z1806-x64.msi',
