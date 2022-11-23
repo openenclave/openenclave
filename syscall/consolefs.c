7ead05be612b0d5badecd1bbde23fb6800c19150
Thomas Tendyck <tt@edgeless.systems>
2022-11-23 17:04:47 +0100

    Remove superfluous memcpy in fcntl
    Signed-off-by: Thomas Tendyck <tt@edgeless.systems>

--- syscall/consolefs.c
+++ syscall/consolefs.c
@@ -139,6 +139,7 @@ static int _consolefs_fcntl(oe_fd_t* file_, int cmd, uint64_t arg)
 {
     int ret = -1;
     file_t* file = _cast_file(file_);
+    uint64_t argin = 0;
     void* argout = NULL;
     uint64_t argsize = 0;
 
@@ -147,37 +148,27 @@ static int _consolefs_fcntl(oe_fd_t* file_, int cmd, uint64_t arg)
 
     switch (cmd)
     {
+        // arg: void
         case OE_F_GETFD:
-        case OE_F_SETFD:
         case OE_F_GETFL:
-        case OE_F_SETFL:
             break;
 
+        // arg: int
+        case OE_F_SETFD:
+        case OE_F_SETFL:
+            argin = arg;
+            break;
+
+        // arg: struct flock*
         case OE_F_GETLK:
         case OE_F_OFD_GETLK:
+        case OE_F_SETLKW:
+        case OE_F_SETLK:
+        case OE_F_OFD_SETLK:
+        case OE_F_OFD_SETLKW:
             argsize = sizeof(struct oe_flock);
             argout = (void*)arg;
             break;
-
-        case OE_F_SETLKW:
-        case OE_F_SETLK:
-        {
-            void* srcp = (void*)arg;
-            argsize = sizeof(struct oe_flock64);
-            argout = (void*)arg;
-            memcpy(argout, srcp, argsize);
-            break;
-        }
-
-        case OE_F_OFD_SETLK:
-        case OE_F_OFD_SETLKW:
-        {
-            void* srcp = (void*)arg;
-            argsize = sizeof(struct oe_flock64);
-            argout = (void*)arg;
-            memcpy(argout, srcp, argsize);
-            break;
-        }
 
         // for sockets
         default:
@@ -193,7 +184,7 @@ static int _consolefs_fcntl(oe_fd_t* file_, int cmd, uint64_t arg)
     }
 
     if (oe_syscall_fcntl_ocall(
-            &ret, file->host_fd, cmd, arg, argsize, argout) != OE_OK)
+            &ret, file->host_fd, cmd, argin, argsize, argout) != OE_OK)
         OE_RAISE_ERRNO(OE_EINVAL);
 
 done:
