a5b6a8d6c47bb0f71c67f0ee1e316e9c9fcb69e2
Minh Nguyen <luonguyen@microsoft.com>
2022-11-30 16:34:05 +0000

    Added sanity check for key pair in oe_ec_generate_key_pair_from_private()

---
+++ build/tests/crypto/host/CMakeFiles/hostcrypto.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/luongminh97/openenclave/common/sgx/rand.S" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/__/__/common/sgx/rand.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "OE_API_VERSION=2"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../include"
+  "output/include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/luongminh97/openenclave/tests/crypto/asn1_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/asn1_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/cpu_entropy_test.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/cpu_entropy_test.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/crl_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/crl_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/ec_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/ec_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/hash.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/hash.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/hmac_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/hmac_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/kdf_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/kdf_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/random_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/random_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/read_file.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/read_file.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/rsa_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/rsa_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/sha_tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/sha_tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/tests.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/tests.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/utils.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/__/utils.c.o"
+  "/home/luongminh97/openenclave/tests/crypto/host/main.c" "/home/luongminh97/openenclave/build/tests/crypto/host/CMakeFiles/hostcrypto.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "OE_API_VERSION=2"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "output/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/luongminh97/openenclave/build/host/CMakeFiles/oehost.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/tests/crypto/host/CMakeFiles/hostcrypto.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hostcrypto.dir/__/__/__/common/sgx/rand.S.o"
+  "CMakeFiles/hostcrypto.dir/__/asn1_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/cpu_entropy_test.c.o"
+  "CMakeFiles/hostcrypto.dir/main.c.o"
+  "CMakeFiles/hostcrypto.dir/__/crl_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/ec_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/hash.c.o"
+  "CMakeFiles/hostcrypto.dir/__/hmac_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/kdf_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/random_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/read_file.c.o"
+  "CMakeFiles/hostcrypto.dir/__/rsa_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/sha_tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/tests.c.o"
+  "CMakeFiles/hostcrypto.dir/__/utils.c.o"
+  "hostcrypto.pdb"
+  "hostcrypto"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/hostcrypto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
