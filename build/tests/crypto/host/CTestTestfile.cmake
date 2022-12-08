bb7f77eaf15b233ab27c8a7763cccdd96e9e8de2
Minh Nguyen <luonguyen@microsoft.com>
2022-12-08 21:37:32 +0000

    Removed build directory
    Signed-off-by: Minh Nguyen <luonguyen@microsoft.com>

--- build/tests/crypto/host/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luongminh97/openenclave/tests/crypto/host
-# Build directory: /home/luongminh97/openenclave/build/tests/crypto/host
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests/crypto/host "hostcrypto")
