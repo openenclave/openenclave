a54603dd1f4f856f30fa69ffbe1b94b48aa08caf
Chris Yan <chrisyan@microsoft.com>
2023-08-28 10:26:21 -0700

    Update Intel DCAP to 1.18.100.1
    Also update Azure DCAP version on Windows Ansible roles
    
    Signed-off-by: Chris Yan <chrisyan@microsoft.com>

--- scripts/install-windows-prereqs.ps1
+++ scripts/install-windows-prereqs.ps1
@@ -20,9 +20,9 @@ Param(
     [string]$NugetHash = '2D4D38666E5C7D27EE487C60C9637BD9DD63795A117F0E0EDC68C55EE6DFB71F',
     [string]$DevconURL = 'https://download.microsoft.com/download/7/D/D/7DD48DE6-8BDA-47C0-854A-539A800FAA90/wdk/Installers/787bee96dbd26371076b37b13c405890.cab',
     [string]$DevconHash = 'A38E409617FC89D0BA1224C31E42AF4344013FEA046D2248E4B9E03F67D5908A',
-    # Intel PSW 2.18.100.2 is included in DCAP 1.16.100.2
-    [string]$IntelDCAPURL = 'https://registrationcenter-download.intel.com/akdlm/irc_nas/19233/Intel%20SGX%20DCAP%20for%20Windows%20v1.16.100.2.zip',
-    [string]$IntelDCAPHash = 'F72827C7C56DE61254EFFB25E9E1BB9AC0472934F9322A943B2AAA108DBF1589',
+    # Intel PSW 2.20.100.1 is included in DCAP 1.18.100.1
+    [string]$IntelDCAPURL = 'https://registrationcenter-download.intel.com/akdlm/IRC_NAS/c8fc28e4-bfa9-4e1d-8dc5-502f5e6895a5/Intel_SGX_DCAP_for_Windows_v1.18.100.1.zip',
+    [string]$IntelDCAPHash = 'CDB796B5D97744CE64E8B2B1DEC4EF95ABB2AAE00C74ACD38E70355A5235D06F',
     [string]$VCRuntime2012URL = 'https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe',
     [string]$VCRuntime2012Hash = '681BE3E5BA9FD3DA02C09D7E565ADFA078640ED66A0D58583EFAD2C1E3CC4064',
     [string]$AzureDCAPNupkgURL = 'https://www.nuget.org/api/v2/package/Microsoft.Azure.DCAP/1.12.1',
