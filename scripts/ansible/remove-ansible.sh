d845c7b33f8b68b41dea341861d9c60a2988f4ea
Chris Yan <chrisyan@microsoft.com>
2024-02-28 12:47:05 -0800

    Update to Ansible 8
    Signed-off-by: Chris Yan <chrisyan@microsoft.com>

--- scripts/ansible/remove-ansible.sh
+++ scripts/ansible/remove-ansible.sh
@@ -7,14 +7,28 @@ set -o errexit
 
 DIR=$(dirname "$0")
 
-if command -v yum > /dev/null; then
-    yum install python3-pip -y
-elif command -v apt-get > /dev/null; then
-    apt-get update
-    apt-get install python3-pip -y
-else
-    echo "ERROR: Only these package managers are supported: yum, apt-get"
-    exit 1
+if [[ -d ~/.ansible/collections/ansible_collections/community/general ]]; then
+    echo "Removing community.general from Ansible collections..."
+    rm -rf ~/.ansible/collections/ansible_collections/community/general
+fi
+if [[ -d ~/.ansible/collections/ansible_collections/ansible/windows ]]; then
+    echo "Removing ansiblle.windows from Ansible collections..."
+    rm -rf ~/.ansible/collections/ansible_collections/ansible/windows
+fi
+if [[ -d ~/.ansible/collections/ansible_collections/community/windows ]]; then
+    echo "Removing community.windows from Ansible collections..."
+    rm -rf ~/.ansible/collections/ansible_collections/community/windows
+fi
+if ! command -v ansible; then
+    echo "No Ansible installation found!"
 fi
 
-pip3 uninstall -r "$DIR/requirements.txt" -y
+# Get Python version from Ansible in PATH
+PYTHON_VERSION=$(ansible --version | grep "python version" | awk '{print $4}' | cut -d "." -f 1,2)
+PYTHON_EXECUTABLE=python${PYTHON_VERSION}
+
+if [[ -z ${PYTHON_VERSION+x} ]]; then
+    echo "ERROR: The Python version from Ansible could not be found! Found version: ${PYTHON_VERSION}"
+fi
+
+${PYTHON_EXECUTABLE} -m pip uninstall -r "$DIR/requirements.txt" -y
