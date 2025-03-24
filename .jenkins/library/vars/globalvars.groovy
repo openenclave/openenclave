cd88657de16b5e9d0826faa9d990ae43b73c3247
Chris Yan <chrisyan@microsoft.com>
2025-03-24 15:04:29 -0700

    Separate Ubuntu 22.04 non-sgx agent label
    Signed-off-by: Chris Yan <chrisyan@microsoft.com>

--- .jenkins/library/vars/globalvars.groovy
+++ .jenkins/library/vars/globalvars.groovy
@@ -13,17 +13,18 @@ import groovy.transform.Field
 @Field AGENTS_LABELS = [
     // ACC VMs
     "acc-ubuntu-20.04":            env.UBUNTU_2004_CUSTOM_LABEL ?: "ACC-2004",
-    "acc-ubuntu-22.04":            "ACC-2204",
+    "acc-ubuntu-22.04":            env.UBUNTU_2204_CUSTOM_LABEL ?: "ACC-2204",
     "acc-v2-ubuntu-20.04":         env.UBUNTU_2004_CUSTOM_LABEL ?: "ACC-v2-2004-DC4",
-    "acc-v2-ubuntu-22.04":         "ACC-v2-2204-DC4",
-    "acc-v3-ubuntu-20.04":         env.UBUNTU_2004_ICX_CUSTOM_LABEL ?: "ACC-v3-2004",
-    "acc-v3-ubuntu-22.04":         "ACC-v3-2204-DC4",
+    "acc-v2-ubuntu-22.04":         env.UBUNTU_2204_CUSTOM_LABEL ?: "ACC-v2-2204-DC4",
+    "acc-v3-ubuntu-20.04":         env.UBUNTU_2004_ICX_CUSTOM_LABEL ?: "ACC-v3-2004-DC4",
+    "acc-v3-ubuntu-22.04":         env.UBUNTU_2204_ICX_CUSTOM_LABEL ?: "ACC-v3-2204-DC4",
     "acc-win2022-dcap":            env.WS2022_DCAP_CUSTOM_LABEL ?: "SGXFLC-Windows-2022-DCAP-v2",
     "acc-v3-win2022-dcap":         env.WS2022_DCAP_ICX_CUSTOM_LABEL ?: "SGXFLC-Windows-2022-DCAP-v3",
     // Non SGX VMs
+    // ubuntu-nonsgx will be deprecated in favor of ubuntu-nonsgx-[version]
     "ubuntu-nonsgx":               env.UBUNTU_NONSGX_CUSTOM_LABEL ?: "nonSGX-ubuntu-2004",
-    "ubuntu-nonsgx-20.04":         env.UBUNTU_NONSGX_CUSTOM_LABEL ?: "nonSGX-ubuntu-2004",
-    "ubuntu-nonsgx-22.04":         env.UBUNTU_NONSGX_CUSTOM_LABEL ?: "nonSGX-ubuntu-2204",
+    "ubuntu-nonsgx-20.04":         env.UBUNTU_2004_NONSGX_LABEL ?: "nonSGX-ubuntu-2004",
+    "ubuntu-nonsgx-22.04":         env.UBUNTU_2204_NONSGX_LABELL ?: "nonSGX-ubuntu-2204",
     "ws2022-nonsgx":               env.WS2022_NONSGX_CUSTOM_LABEL ?: "nonSGX-Windows-2022",
     // Plain VMs
     "acc-ubuntu-22.04-vanilla":    "ACC-marketplace-ubuntu-2204",
